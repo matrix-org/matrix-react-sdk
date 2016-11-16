01c120379be27529edd8efb99bf5f8b0d4768e10
Mark Haines <mjark@negativecurvature.net>
2016-11-16 13:05:05 +0000

    Render attachments inside iframes.

--- src/components/views/messages/MFileBody.js
+++ src/components/views/messages/MFileBody.js
@@ -23,12 +23,86 @@ import sdk from '../../../index';
 import {decryptFile} from '../../../utils/DecryptFile';
 
 
+// User supplied content can contain scripts, we have to be careful that
+// we don't accidentally run those script within the same origin as the
+// client. Otherwise those scripts written by remote users can read
+// the access token and end-to-end keys that are in local storage.
+//
+// For attachments downloaded directly from the homeserver we can use
+// Content-Security-Policy headers to disable script execution.
+//
+// But attachments with end-to-end ecryption are more difficult to handle.
+// We need to decrypt the attachment on the client and then display it.
+// To display the attachment we need to turn the decrypted bytes into a URL.
+//
+// There are two ways to turn bytes into URLs, data URL and blob URLs.
+// Data URLs aren't suitable for downloading a file because Chrome has a
+// 2MB limit on the size of URLs that can be viewed in the browser or
+// downloaded. This limit does not seem to apply when the url is used as
+// the source attribute of an image tag.
+//
+// Blob URLs are generated using window.URL.createObjectURL and unforuntately
+// for our purposes they inherit the origin of the page that created them.
+// This means that any scripts that run when the URL is viewed will be able
+// to access local storage.
+//
+// The easiest solution is to host the code that generates the blob URL on
+// a different domain to the client.
+// Another possibility is to generate the blob URL within a sandboxed iframe.
+// The downside of using a second domain is that it complicates hosting,
+// the downside of using a sandboxed iframe is that the browers are overly
+// restrictive in what you are allowed to do with the generated URL.
+//
+// So as a compomise we'll do both, that is use a second domain if one is
+// configured, otherwise fall back to using a sandbox.
+// To simplify deploying the client we'll try to keep as much of the rendering
+// logic within the client domain as possible.
+//
+// To that end the first version of the blob generation will be the following
+// html:
+//
+//      <html><head><script>
+//      window.onmessage=function(e){eval("("+e.data.code+")")(e)}
+//      </script></head><body></body></html>
+//
+// This waits to receive a message event sent using the window.postMessage API.
+// When it receives the event it evals a javascript function in data.code and
+// runs the function passing the event as an argument.
+//
+// In particular it means that the rendering function can be written as a
+// ordinary javascript function which then is turned into a string using
+// toString().
+//
+const DEFAULT_CROSS_ORIGIN_RENDERER = "data:text/html;base64," + window.btoa(`
+<html><head><script>
+window.onmessage=function(e){eval("("+e.data.code+")")(e)}
+\u003c/script></head><body></body></html>
+`);
+
+/**
+ * Render the attachment inside the iframe.
+ * We can't use imported libraries here so this has to be vanilla JS.
+ */
+function renderAttachment(event) {
+    var a = document.createElement("a");
+    a.rel = event.data.rel;
+    a.target = event.data.target;
+    a.download = event.data.download;
+    a.href = event.data.url;
+    a.textContent = event.data.textContent;
+    if (event.data.blob) {
+        a.href = window.URL.createObjectURL(event.data.blob);
+    }
+    document.body.appendChild(a);
+}
+
+
 module.exports = React.createClass({
     displayName: 'MFileBody',
 
     getInitialState: function() {
         return {
-            decryptedUrl: (this.props.decryptedUrl ? this.props.decryptedUrl : null),
+            decryptedBlob: (this.props.decryptedBlob ? this.props.decryptedBlob : null),
         };
     },
 
@@ -63,7 +137,7 @@ module.exports = React.createClass({
     _getContentUrl: function() {
         const content = this.props.mxEvent.getContent();
         if (content.file !== undefined) {
-            return this.state.decryptedUrl;
+            return null;
         } else {
             return MatrixClientPeg.get().mxcUrlToHttp(content.url);
         }
@@ -71,10 +145,10 @@ module.exports = React.createClass({
 
     componentDidMount: function() {
         const content = this.props.mxEvent.getContent();
-        if (content.file !== undefined && this.state.decryptedUrl === null) {
-            decryptFile(content.file).done((url) => {
+        if (content.file !== undefined && this.state.decryptedBlob === null) {
+            decryptFile(content.file).done((blob) => {
                 this.setState({
-                    decryptedUrl: url,
+                    decryptedBlob: blob,
                 });
             }, (err) => {
                 console.warn("Unable to decrypt attachment: ", err)
@@ -90,7 +164,7 @@ module.exports = React.createClass({
         const text = this.presentableTextForFile(content);
 
         var TintableSvg = sdk.getComponent("elements.TintableSvg");
-        if (content.file !== undefined && this.state.decryptedUrl === null) {
+        if (content.file !== undefined && this.state.decryptedBlob === null) {
 
             // Need to decrypt the attachment
             // The attachment is decrypted in componentDidMount.
@@ -108,34 +182,28 @@ module.exports = React.createClass({
         const fileName = content.body && content.body.length > 0 ? content.body : "Attachment";
 
         var downloadAttr = undefined;
-        if (this.state.decryptedUrl) {
-            // If the file is encrypted then we MUST download it rather than displaying it
-            // because Firefox is vunerable to XSS attacks in data:// URLs
-            // and all browsers are vunerable to XSS attacks in blob: URLs
-            // created with window.URL.createObjectURL
-            // See https://bugzilla.mozilla.org/show_bug.cgi?id=255107
-            // See https://w3c.github.io/FileAPI/#originOfBlobURL
-            //
-            // This is not a problem for unencrypted links because they are
-            // either fetched from a different domain so are safe because of
-            // the same-origin policy or they are fetch from the same domain,
-            // in which case we trust that the homeserver will set a
-            // Content-Security-Policy that disables script execution.
-            // It is reasonable to trust the homeserver in that case since
-            // it is the same domain that controls this javascript.
-            //
-            // We can't apply the same workaround for encrypted files because
-            // we can't supply HTTP headers when the user clicks on a blob:
-            // or data:// uri.
-            //
-            // We should probably provide a download attribute anyway so that
-            // the file will have the correct name when the user tries to
-            // download it. We can't provide a Content-Disposition header
-            // like we would for HTTP.
+        var blobAttr = undefined;
+        if (this.state.decryptedBlob) {
+            // Set a download attribute for encrypted files so that the file
+            // will have the correct name when the user tries to download it.
+            // We can't provide a Content-Disposition header like we would for HTTP.
             downloadAttr = fileName;
+            blobAttr = this.state.decryptedBlob;
         }
 
-        if (contentUrl) {
+        function onIframeLoad(ev) {
+            ev.target.contentWindow.postMessage({
+                code: renderAttachment.toString(),
+                url: contentUrl,
+                blob: blobAttr,
+                download: downloadAttr,
+                target: "_blank",
+                textContent: "Download " + text,
+            }, "*");
+        }
+
+
+        if (contentUrl || blobAttr) {
             if (this.props.tileShape === "file_grid") {
                 return (
                     <span className="mx_MFileBody">
@@ -154,10 +222,7 @@ module.exports = React.createClass({
                 return (
                     <span className="mx_MFileBody">
                         <div className="mx_MImageBody_download">
-                            <a href={contentUrl} target="_blank" rel="noopener" download={downloadAttr}>
-                                <TintableSvg src="img/download.svg" width="12" height="14"/>
-                                Download {text}
-                            </a>
+                            <iframe src={DEFAULT_CROSS_ORIGIN_RENDERER} onLoad={onIframeLoad}/>
                         </div>
                     </span>
                 );
