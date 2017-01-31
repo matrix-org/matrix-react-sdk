467b9dc4f68feee8f3d0dd288a992814698ab05c
Luke Barnard <lukeb@openmarket.com>
2017-01-31 10:44:45 +0000

    Doc trackReferral

--- src/RtsClient.js
+++ src/RtsClient.js
@@ -13,6 +13,17 @@ export default class RtsClient {
         });
     }
 
+    /**
+     * Track a referral with the Riot Team Server. This should be called once a referred
+     * user has been successfully registered.
+     * @param {string} referrer the user ID of one who referred the user to Riot.
+     * @param {string} user_id the user ID of the user being referred.
+     * @param {string} user_email the email address linked to `user_id`.
+     * @returns {Promise} a promise that resolves to [$response, $body], where $response
+     * is the response object created by the request lib and $body is the object parsed
+     * from the JSON response body. $body should be { team_token: 'sometoken' } upon
+     * success.
+     */
     trackReferral(referrer, user_id, user_email) {
         return request({
             url: this._url + '/register',
