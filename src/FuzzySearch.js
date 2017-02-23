8a2ac1876c3dc70dd41f32e9eaa3cdedd2d155b0
Luke Barnard <lukeb@openmarket.com>
2017-02-23 16:09:53 +0000

    Use FuzzySearch based on aviraldg:rte-fixes
    This replaces fuse, which was likewise done by @aviraldg, in order to speed up search. Whilst it is certainly faster, the scoring seems slightly lower quality than the prev commit.

---
+++ src/FuzzySearch.js
@@ -0,0 +1,65 @@
+import Levenshtein from 'liblevenshtein';
+import _at from 'lodash/at';
+import _flatMap from 'lodash/flatMap';
+import _sortBy from 'lodash/sortBy';
+import _sortedUniq from 'lodash/sortedUniq';
+import _keys from 'lodash/keys';
+
+class KeyMap {
+    keys: Array<String>;
+    objectMap: {[String]: Array<Object>};
+    priorityMap: {[String]: number}
+}
+
+const DEFAULT_RESULT_COUNT = 10;
+const DEFAULT_DISTANCE = 5;
+
+export default class FuzzySearch {
+
+    static valuesToKeyMap(objects: Array<Object>, keys: Array<String>): KeyMap {
+        const keyMap = new KeyMap();
+        const map = {};
+        const priorities = {};
+
+        objects.forEach((object, i) => {
+            const keyValues = _at(object, keys);
+            // console.log(object, keyValues, keys);
+            for (const keyValue of keyValues) {
+                if (!map.hasOwnProperty(keyValue)) {
+                   map[keyValue] = [];
+                }
+                map[keyValue].push(object);
+            }
+            priorities[object] = i;
+        });
+
+        keyMap.objectMap = map;
+        keyMap.priorityMap = priorities;
+        keyMap.keys = _sortBy(_keys(map), [value => priorities[value]]);
+        return keyMap;
+    }
+
+    constructor(objects: Array<Object>, options: {[Object]: Object} = {}) {
+        this.options = options;
+        this.keys = options.keys;
+        this.setObjects(objects);
+    }
+
+    setObjects(objects: Array<Object>) {
+        this.keyMap = FuzzySearch.valuesToKeyMap(objects, this.keys);
+
+        this.matcher = new Levenshtein.Builder()
+            .dictionary(this.keyMap.keys, false)
+            .algorithm('transposition')
+            .sort_candidates(true)
+            .include_distance(false)
+            .maximum_candidates(this.options.resultCount || DEFAULT_RESULT_COUNT) // result count 0 doesn't make much sense
+            .build();
+    }
+
+    search(query: String): Array<Object> {
+        let d = this.options.distance || DEFAULT_DISTANCE;
+        let candidates = this.matcher.transduce(query.toLowerCase(), d);
+        return _flatMap(candidates, candidate => this.keyMap.objectMap[candidate]);
+    }
+}
--- src/components/views/dialogs/ChatInviteDialog.js
+++ src/components/views/dialogs/ChatInviteDialog.js
@@ -26,7 +26,7 @@ import dis from '../../../dispatcher';
 import Modal from '../../../Modal';
 import AccessibleButton from '../elements/AccessibleButton';
 import q from 'q';
-import Fuse from 'fuse.js';
+import FuzzySearch from '../../../FuzzySearch.js';
 
 const TRUNCATE_QUERY_LIST = 40;
 
@@ -88,17 +88,10 @@ module.exports = React.createClass({
         }
         // Create a Fuse instance for fuzzy searching this._userList
         if (!this._fuse) {
-            this._fuse = new Fuse(
+            this._fuse = new FuzzySearch(
                 // Use an empty list at first that will later be populated
                 // (see this._updateUserList)
-                [],
-                {
-                    shouldSort: true,
-                    location: 0, // The index of the query in the test string
-                    distance: 5, // The distance away from location the query can be
-                    // 0.0 = exact match, 1.0 = match anything
-                    threshold: 0.3,
-                }
+                [], {keys: ["displayName", "userId"], distance: 4, resultCount: 5}
             );
         }
         this._updateUserList();
@@ -187,20 +180,20 @@ module.exports = React.createClass({
             return;
         }
 
-        if (this.queryChangedDebouncer) {
-            clearTimeout(this.queryChangedDebouncer);
-        }
-        this.queryChangedDebouncer = setTimeout(() => {
+        // if (this.queryChangedDebouncer) {
+        //     clearTimeout(this.queryChangedDebouncer);
+        // }
+        // this.queryChangedDebouncer = setTimeout(() => {
             // Only do search if there is something to search
             if (query.length > 0 && query != '@') {
                 // Weighted keys prefer to match userIds when first char is @
-                this._fuse.options.keys = [{
-                    name: 'displayName',
-                    weight: query[0] === '@' ? 0.1 : 0.9,
-                },{
-                    name: 'userId',
-                    weight: query[0] === '@' ? 0.9 : 0.1,
-                }];
+                // this._fuse.options.keys = [{
+                //     name: 'displayName',
+                //     weight: query[0] === '@' ? 0.1 : 0.9,
+                // },{
+                //     name: 'userId',
+                //     weight: query[0] === '@' ? 0.9 : 0.1,
+                // }];
                 queryList = this._fuse.search(query).map((user) => {
                     // Return objects, structure of which is defined
                     // by InviteAddressType
@@ -236,7 +229,7 @@ module.exports = React.createClass({
             }, () => {
                 this.addressSelector.moveSelectionTop();
             });
-        }, 200);
+        // }, 200);
     },
 
     onDismissed: function(index) {
@@ -368,7 +361,8 @@ module.exports = React.createClass({
         });
         this._userList.splice(meIx, 1);
 
-        this._fuse.set(this._userList);
+        // this._fuse.set(this._userList);
+        this._fuse.setObjects(this._userList);
     }, 500),
 
     _isOnInviteList: function(uid) {
