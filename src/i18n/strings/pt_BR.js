4c38aecc8f867055478f0b792b25c52b58c270e6
Alan <alantygel@gmail.com>
2017-04-22 20:39:56 -0300

    fix error

--- src/i18n/strings/pt_BR.js
+++ src/i18n/strings/pt_BR.js
@@ -93,7 +93,7 @@ module.exports={
     '💬 Rooms' : ' 💬  Salas ',
     'Rooms' : 'Salas',
     'Searches DuckDuckGo for results' : 'Buscar por resultados no buscador DuckDuckGo',
-    "Send a message (unencrypted)…" : "Enviar uma mensagem (sem criptografia)",
+    'Send a message (unencrypted)…' : 'Enviar uma mensagem (sem criptografia)',
     "Send an encrypted message…" : "Enviar uma mensagem criptografada",
     'Sender device information' : 'Informação do dispositivo emissor',
     "Send Invites" : "Enviar convites",
