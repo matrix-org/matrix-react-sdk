139dffb223fcb94bc1422babb37e03dd07e20094
MTRNord <mtrnord1@gmail.com>
2017-05-05 19:35:46 +0200

    fix variable not defined

--- src/components/structures/MatrixChat.js
+++ src/components/structures/MatrixChat.js
@@ -786,7 +786,7 @@ module.exports = React.createClass({
                         }
                         const resp = {err: err, response: resp_raw};
                         err = resp['err'];
-                        response_cb = resp['response'];
+                        const response_cb = resp['response'];
                         callback(err, response_cb, langCode);
                         return;
                     }
