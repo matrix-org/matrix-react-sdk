fc7f3cd4206479fca44abcc35190a95a2fd70a06
daniel tygel <dtygel@eita.org.br>
2017-05-16 00:32:51 -0300

    fixed https://github.com/matrix-org/matrix-react-sdk/pull/801#discussion_r116629719

--- src/languageHandler.js
+++ src/languageHandler.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2015, 2016 OpenMarket Ltd
+Copyright 2017 Nordgedanken and Cooperative EITA
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -23,12 +23,12 @@ var q = require('q');
 const i18nFolder = 'i18n/';
 
 module.exports.setLanguage = function(languages, extCounterpart=null) {
-	if (!languages || !Array.isArray(languages)) {
+    if (!languages || !Array.isArray(languages)) {
     const languages = this.getNormalizedLanguageKeys(this.getLanguageFromBrowser());
     console.log("no language found. Got from browser: " + JSON.stringify(languages));
-	}
+    }
 
-	request(i18nFolder + 'languages.json', function(err, response, body) {
+    request(i18nFolder + 'languages.json', function(err, response, body) {
     function getLanguage(langPath, langCode, callback) {
         let response_return = {};
         let resp_raw = {};
@@ -57,63 +57,63 @@ module.exports.setLanguage = function(languages, extCounterpart=null) {
     }
 
     function registerTranslations(err, langJson, langCode){
-		if (err !== null) {
-			var ErrorDialog = sdk.getComponent("dialogs.ErrorDialog");
-			Modal.createDialog(ErrorDialog, {
-				title: counterpart.translate('Error changing language'),
-				description: counterpart.translate('Riot was unable to find the correct Data for the selected Language.'),
-				button: counterpart.translate("OK"),
-			});
-			return;
-		} else {
-			if (extCounterpart) {
-				extCounterpart.registerTranslations(langCode, langJson);
-			}
-  			counterpart.registerTranslations(langCode, langJson);
-  		}
+        if (err !== null) {
+            var ErrorDialog = sdk.getComponent("dialogs.ErrorDialog");
+            Modal.createDialog(ErrorDialog, {
+                title: counterpart.translate('Error changing language'),
+                description: counterpart.translate('Riot was unable to find the correct Data for the selected Language.'),
+                button: counterpart.translate("OK"),
+            });
+            return;
+        } else {
+            if (extCounterpart) {
+                extCounterpart.registerTranslations(langCode, langJson);
+            }
+            counterpart.registerTranslations(langCode, langJson);
+        }
     }
 
     let languageFiles = {};
     if(err){
-    	console.error(err);
-    	return;
+        console.error(err);
+        return;
     } else {
-      if (body) {
-        languageFiles = JSON.parse(body);
-      } else {
-        languageFiles = JSON.parse('{"en": "en_EN.json"}');
-      }
+        if (body) {
+            languageFiles = JSON.parse(body);
+        } else {
+            languageFiles = JSON.parse('{"en": "en_EN.json"}');
+        }
     }
 
 
     const isValidFirstLanguage = (languageFiles.hasOwnProperty(languages[0]));
     var validLanguageKey = "";
     if ((isValidFirstLanguage) || (languages.length==2 && languageFiles.hasOwnProperty(languages[1]))) {
-    	validLanguageKey = (isValidFirstLanguage) ? languages[0] : languages[1];
-   		getLanguage(i18nFolder + languageFiles[validLanguageKey], validLanguageKey, registerTranslations);
-   		if (extCounterpart) {
-   			extCounterpart.setLocale(validLanguageKey);
-   		}
-   		counterpart.setLocale(validLanguageKey);
-      UserSettingsStore.setLocalSetting('language', validLanguageKey);
-      console.log("set language to "+validLanguageKey);
+        validLanguageKey = (isValidFirstLanguage) ? languages[0] : languages[1];
+        getLanguage(i18nFolder + languageFiles[validLanguageKey], validLanguageKey, registerTranslations);
+        if (extCounterpart) {
+            extCounterpart.setLocale(validLanguageKey);
+        }
+        counterpart.setLocale(validLanguageKey);
+        UserSettingsStore.setLocalSetting('language', validLanguageKey);
+        console.log("set language to "+validLanguageKey);
     } else {
-    	console.log("didnt find any language file");
+        console.log("didnt find any language file");
     }
 
     //Set 'en' as fallback language:
     if (validLanguageKey!="en") {
-    	getLanguage(i18nFolder + languageFiles['en'], 'en', registerTranslations);
+        getLanguage(i18nFolder + languageFiles['en'], 'en', registerTranslations);
     }
     if (extCounterpart) {
-   		extCounterpart.setFallbackLocale('en');
-   	}
- 		counterpart.setFallbackLocale('en');
-	});
+        extCounterpart.setFallbackLocale('en');
+    }
+    counterpart.setFallbackLocale('en');
+    });
 };
 
 module.exports.getAllLanguageKeysFromJson = function() {
-	let deferred = q.defer();
+    let deferred = q.defer();
 
     request(
         { method: "GET", url: i18nFolder + 'languages.json' },
@@ -130,7 +130,7 @@ module.exports.getAllLanguageKeysFromJson = function() {
             var languages = JSON.parse(body);
             // If no language is found, fallback to 'en':
             if (!languages) {
-            	languages = [{"en": "en_EN.json"}];
+                languages = [{"en": "en_EN.json"}];
             }
             const languageKeys = Object.keys(languages);
             deferred.resolve(languageKeys);
@@ -140,27 +140,27 @@ module.exports.getAllLanguageKeysFromJson = function() {
 }
 
 module.exports.getLanguageFromBrowser = function() {
-	return navigator.languages[0] || navigator.language || navigator.userLanguage;
+    return navigator.languages[0] || navigator.language || navigator.userLanguage;
 };
 
 module.exports.getNormalizedLanguageKeys = function(language) {
-	if (!language) {
-		return;
-	}
-	const languageKeys = [];
-	const normalizedLanguage = this.normalizeLanguageKey(language);
-	const languageParts = normalizedLanguage.split('-');
-	if (languageParts.length==2 && languageParts[0]==languageParts[1]) {
-		languageKeys.push(languageParts[0]);
-	} else {
-		languageKeys.push(normalizedLanguage);
-		if (languageParts.length==2) {
-			languageKeys.push(languageParts[0]);
-		}
-	}
-	return languageKeys;
+    if (!language) {
+        return;
+    }
+    const languageKeys = [];
+    const normalizedLanguage = this.normalizeLanguageKey(language);
+    const languageParts = normalizedLanguage.split('-');
+    if (languageParts.length==2 && languageParts[0]==languageParts[1]) {
+        languageKeys.push(languageParts[0]);
+    } else {
+        languageKeys.push(normalizedLanguage);
+        if (languageParts.length==2) {
+            languageKeys.push(languageParts[0]);
+        }
+    }
+    return languageKeys;
 };
 
 module.exports.normalizeLanguageKey = function(language) {
-	return language.toLowerCase().replace("_","-");
+    return language.toLowerCase().replace("_","-");
 };
