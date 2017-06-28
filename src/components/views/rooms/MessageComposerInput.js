7ef16b154abf6ddf7e69130e480cdf8e7c263e74
MTRNord <mtrnord1@gmail.com>
2017-06-28 21:00:31 +0200

    remove superfluous space

--- src/components/views/rooms/MessageComposerInput.js
+++ src/components/views/rooms/MessageComposerInput.js
@@ -522,7 +522,7 @@ export default class MessageComposerInput extends React.Component {
         let sendHtmlFn = this.client.sendHtmlMessage;
         let sendTextFn = this.client.sendTextMessage;
 
-        if (contentText.startsWith('/me') ) {
+        if (contentText.startsWith('/me')) {
             contentText = contentText.startsWith("/me ") ? contentText.substring(4) : contentText.substring(3);
             // bit of a hack, but the alternative would be quite complicated
             if (contentHTML) contentHTML = contentHTML.replace(/\/me ?/, '');
