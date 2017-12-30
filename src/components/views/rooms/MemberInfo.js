489bbd4d9bb7a6055b60dc740c638a1bd94f2389
Michael Telatynski <7t3chguy@gmail.com>
2017-12-30 00:16:44 +0000

    update MemberInfo on PL change, it doesn't care about other state events
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/views/rooms/MemberInfo.js
+++ src/components/views/rooms/MemberInfo.js
@@ -80,9 +80,9 @@ module.exports = withMatrixClient(React.createClass({
         cli.on("Room.timeline", this.onRoomTimeline);
         cli.on("Room.name", this.onRoomName);
         cli.on("Room.receipt", this.onRoomReceipt);
-        cli.on("RoomState.events", this.onRoomStateEvents);
         cli.on("RoomMember.name", this.onRoomMemberName);
         cli.on("RoomMember.membership", this.onRoomMemberMembership);
+        cli.on("RoomMember.powerLevel", this.onRoomMemberPowerlevel);
         cli.on("accountData", this.onAccountData);
 
         this._checkIgnoreState();
@@ -107,9 +107,9 @@ module.exports = withMatrixClient(React.createClass({
             client.removeListener("Room.timeline", this.onRoomTimeline);
             client.removeListener("Room.name", this.onRoomName);
             client.removeListener("Room.receipt", this.onRoomReceipt);
-            client.removeListener("RoomState.events", this.onRoomStateEvents);
             client.removeListener("RoomMember.name", this.onRoomMemberName);
             client.removeListener("RoomMember.membership", this.onRoomMemberMembership);
+            client.removeListener("RoomMember.powerLevel", this.onRoomMemberPowerlevel);
             client.removeListener("accountData", this.onAccountData);
         }
         if (this._cancelDeviceList) {
@@ -181,8 +181,8 @@ module.exports = withMatrixClient(React.createClass({
         }
     },
 
-    onRoomStateEvents: function(ev, state) {
-        this.forceUpdate();
+    onRoomMemberPowerlevel: function(ev, member) {
+        if (this.props.member.userId === member.userId) this.setState(this._calculateOpsPermissions(member));
     },
 
     onRoomMemberName: function(ev, member) {
