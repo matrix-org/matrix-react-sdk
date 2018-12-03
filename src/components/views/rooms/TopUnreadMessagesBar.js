509ce3dedc86ef193f13f04b1af0339ca8157669
Bruno Windels <brunow@matrix.org>
2018-12-03 14:55:24 +0100

    apply redesign to topunreadmessagebar
    with placeholder for message count

--- src/components/views/rooms/TopUnreadMessagesBar.js
+++ src/components/views/rooms/TopUnreadMessagesBar.js
@@ -35,21 +35,12 @@ module.exports = React.createClass({
         return (
             <div className="mx_TopUnreadMessagesBar">
                 <AccessibleButton className="mx_TopUnreadMessagesBar_scrollUp"
-                        onClick={this.props.onScrollUpClick}>
-                    <img src="img/scrollto.svg" width="24" height="24"
-                        // No point on setting up non empty alt on this image
-                        // as it only complements the text which follows it.
-                        alt=""
-                        title={_t('Scroll to unread messages')}
-                        // In order not to use this title attribute for accessible name
-                        // calculation of the parent button set the role presentation
-                        role="presentation" />
-                    { _t("Jump to first unread message.") }
+                    title={_t('Jump to first unread message.')}
+                    onClick={this.props.onScrollUpClick}>
+                    <div className="mx_TopUnreadMessagesBar_badgeContainer">
+                        <div className="mx_TopUnreadMessagesBar_badge">x</div>
+                    </div>
                 </AccessibleButton>
-                <AccessibleButton element='img' className="mx_TopUnreadMessagesBar_close mx_filterFlipColor"
-                    src="img/cancel.svg" width="18" height="18"
-                    alt={_t("Close")} title={_t("Close")}
-                    onClick={this.props.onCloseClick} />
             </div>
         );
     },
