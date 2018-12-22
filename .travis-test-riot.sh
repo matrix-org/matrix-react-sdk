b9e456b2f579dc5896a2d1b51a7ba23750ea1c60
Travis Ralston <travpc@gmail.com>
2018-12-21 18:24:10 -0700

    Fetch matching e2e-test branch

--- .travis-test-riot.sh
+++ .travis-test-riot.sh
@@ -30,13 +30,12 @@ popd
 if [ "$TRAVIS_BRANCH" = "develop" ]
 then
     # run end to end tests
-    git clone https://github.com/matrix-org/matrix-react-end-to-end-tests.git --branch master
+    scripts/fetchdep.sh matrix-org matrix-react-end-to-end-tests
     pushd matrix-react-end-to-end-tests
     ln -s $REACT_SDK_DIR/$RIOT_WEB_DIR riot/riot-web
     # PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true ./install.sh
     # CHROME_PATH=$(which google-chrome-stable) ./run.sh
     ./install.sh
-    cat installations/consent/homeserver.yaml
     ./run.sh --travis
     popd
 fi
