b08cbe0479749cdb63497776a7ede3a774d7f3eb
Erik Johnston <erik@matrix.org>
2019-01-11 10:08:41 +0000

    fixup

--- scripts/travis/build-riot-web.sh
+++ scripts/travis/build-riot-web.sh
@@ -12,9 +12,6 @@ REACT_SDK_DIR=`pwd`
 scripts/fetchdep.sh vector-im riot-web
 pushd "$RIOT_WEB_DIR"
 
-npm install "$REACT_SDK_DIR/matrix-js-sdk"
-npm install "$REACT_SDK_DIR"
-
 pushd "$REACT_SDK_DIR/matrix-js-sdk"
 npm install
 popd
@@ -24,6 +21,9 @@ npm install "$REACT_SDK_DIR/matrix-js-sdk"
 npm install
 popd
 
+
+npm install "$REACT_SDK_DIR/matrix-js-sdk"
+npm install "$REACT_SDK_DIR"
 npm install
 
 popd
