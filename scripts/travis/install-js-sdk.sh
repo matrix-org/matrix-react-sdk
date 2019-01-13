5c59bafee3065ed59ef667b2db91c65f774eb41f
Erik Johnston <erik@matrix.org>
2019-01-13 15:33:55 +0000

    foo

--- scripts/travis/install-js-sdk.sh
+++ scripts/travis/install-js-sdk.sh
@@ -5,7 +5,18 @@ set -ev
 reactsdk=$(pwd)
 
 pushd "$HOME/build"
+
 $reactsdk/scripts/fetchdep.sh matrix-org matrix-js-sdk
 cd matrix-js-sdk
+
+# If we've cached matrix-js-sdk/node_modules
+if [ -d "$HOME/nm-cache/js-sdk" ]; then
+    rsync "$HOME/nm-cache/js-sdk" node_modules
+fi
+
 npm install
+
+mkdir -p "$HOME/nm-cache"
+rsync node_modules "$HOME/nm-cache/js-sdk"
+
 popd
--- scripts/travis/install-riot-web.sh
+++ scripts/travis/install-riot-web.sh
@@ -7,7 +7,19 @@ reactsdk=$(pwd)
 pushd "$HOME/build"
 $reactsdk/scripts/fetchdep.sh vector-im riot-web
 cd riot-web
+rm -rf "$HOME/build/matrix-js-sdk"
+rm -rf "$HOME/build/matrix-react-sdk"
+
+# If we've cached matrix-js-sdk/node_modules
+if [ -d "$HOME/nm-cache/riot-web" ]; then
+    rsync "$HOME/nm-cache/riot-web" node_modules
+fi
+
 npm install "$HOME/build/matrix-js-sdk"
 npm install "$HOME/build/matrix-react-sdk"
 npm install
+
+mkdir -p "$HOME/nm-cache"
+rsync node_modules "$HOME/nm-cache/riot-web"
+
 popd
