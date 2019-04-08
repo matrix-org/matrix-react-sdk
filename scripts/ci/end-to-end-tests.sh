64cc16b3ce09236d2538fb6552547b5ce2a59972
Bruno Windels <brunow@matrix.org>
2019-04-08 17:34:55 +0200

    dont bail out when tests fail, first upload logs

--- scripts/ci/end-to-end-tests.sh
+++ scripts/ci/end-to-end-tests.sh
@@ -3,22 +3,33 @@
 # script which is run by the CI build (after `yarn test`).
 #
 # clones riot-web develop and runs the tests against our version of react-sdk.
+$(
+    # running this in a subshell because
+    # only want -ev for installation part
+    set -ev
 
-set -ev
+    RIOT_WEB_DIR=riot-web
+    REACT_SDK_DIR=`pwd`
 
-RIOT_WEB_DIR=riot-web
-REACT_SDK_DIR=`pwd`
-
-scripts/ci/build.sh
-# run end to end tests
-scripts/fetchdep.sh matrix-org matrix-react-end-to-end-tests master
-pushd matrix-react-end-to-end-tests
-ln -s $REACT_SDK_DIR/$RIOT_WEB_DIR riot/riot-web
-# PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true ./install.sh
-# CHROME_PATH=$(which google-chrome-stable) ./run.sh
-./install.sh
+    scripts/ci/build.sh
+    # run end to end tests
+    scripts/fetchdep.sh matrix-org matrix-react-end-to-end-tests master
+    pushd matrix-react-end-to-end-tests
+    ln -s $REACT_SDK_DIR/$RIOT_WEB_DIR riot/riot-web
+    # PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true ./install.sh
+    # CHROME_PATH=$(which google-chrome-stable) ./run.sh
+    ./install.sh
+)
+installResult=$?
+if [ $installResult -ne 0 ]; then
+    echo "installation failed, bailing out"
+    exit $installResult
+fi
+# running tests now
 ./run.sh --no-sandbox --error-log e2e-tests-error.log
-if [ $? -ne 0 ]; then
+testResult=$?
+if [ $testResult -ne 0 ]; then
     buildkite-agent artifact upload e2e-tests-error.log
 fi
 popd
+exit $testResult
