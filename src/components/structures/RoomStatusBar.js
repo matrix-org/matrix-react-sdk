9012bfa9253083fc45d40f92c26c3fe34692e55f
Léo Mora <yoshin@live.fr>
2019-04-17 15:23:50 +0200

    Update Share Keys features

--- src/components/structures/RoomStatusBar.js
+++ src/components/structures/RoomStatusBar.js
@@ -220,16 +220,10 @@ module.exports = React.createClass({
         });
 
         if (hasUDE) {
-            title = _t("Message not sent due to unknown devices being present");
-            content = _t(
-                "<showDevicesText>Show devices</showDevicesText>, <sendAnywayText>send anyway</sendAnywayText> or <cancelText>cancel</cancelText>.",
-                {},
-                {
-                    'showDevicesText': (sub) => <a className="mx_RoomStatusBar_resend_link" key="resend" onClick={this._onShowDevicesClick}>{ sub }</a>,
-                    'sendAnywayText': (sub) => <a className="mx_RoomStatusBar_resend_link" key="sendAnyway" onClick={this._onSendWithoutVerifyingClick}>{ sub }</a>,
-                    'cancelText': (sub) => <a className="mx_RoomStatusBar_resend_link" key="cancel" onClick={this._onCancelAllClick}>{ sub }</a>,
-                },
-            );
+            // The dialog box asking about what to do if there is an unknown device
+            // in the room have been disabled for the moment.
+            // We consider "Send Anyway" as the default choice.
+            this._onSendWithoutVerifyingClick();
         } else {
             let consentError = null;
             let resourceLimitError = null;
