c7776830659460f3b30355adcff29df815bca39b
Léo Mora <yoshin@live.fr>
2019-07-17 10:37:27 +0200

    Updated the way to retrieve members inside the room

--- src/Tchap.js
+++ src/Tchap.js
@@ -165,7 +165,7 @@ class Tchap {
 
     static isUserLastAdmin(room) {
         const userId = MatrixClientPeg.get().getUserId();
-        const members = room.currentState.getMembers();
+        const members = room.getJoinedMembers();
         let adminNumber = 0;
         let isUserAdmin = false;
         members.forEach(m => {
