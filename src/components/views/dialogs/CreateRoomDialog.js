d1d38454a6e0d10f88dbd1941ad76359fa09026a
Bruno Windels <brunow@matrix.org>
2019-09-17 14:03:51 +0200

    add public toggle, and alias field if public

--- src/components/views/dialogs/CreateRoomDialog.js
+++ src/components/views/dialogs/CreateRoomDialog.js
@@ -20,6 +20,7 @@ import PropTypes from 'prop-types';
 import sdk from '../../../index';
 import SdkConfig from '../../../SdkConfig';
 import { _t } from '../../../languageHandler';
+import MatrixClientPeg from '../../../MatrixClientPeg';
 
 export default createReactClass({
     displayName: 'CreateRoomDialog',
@@ -29,8 +30,10 @@ export default createReactClass({
 
     getInitialState() {
         return {
+            isPublic: false,
             name: "",
             topic: "",
+            alias: "",
         };
     },
 
@@ -56,18 +59,45 @@ export default createReactClass({
         this.setState({topic: ev.target.value});
     },
 
+    onPublicChange(isPublic) {
+        this.setState({isPublic});
+    },
+
+    onAliasChange(ev) {
+        this.setState({alias: ev.target.value});
+    },
+
     render: function() {
         const BaseDialog = sdk.getComponent('views.dialogs.BaseDialog');
         const DialogButtons = sdk.getComponent('views.elements.DialogButtons');
         const Field = sdk.getComponent('views.elements.Field');
+        const LabelledToggleSwitch = sdk.getComponent('views.elements.LabelledToggleSwitch');
+        const RoomAliasField = sdk.getComponent('views.elements.RoomAliasField');
+
+        let privateLabel;
+        let publicLabel;
+        let aliasField;
+        if (this.state.isPublic) {
+            publicLabel = (<p>{_t("Set a room address to easily share your room with other people.")}</p>);
+            const domain = MatrixClientPeg.get().getDomain();
+            aliasField = (<RoomAliasField onChange={this.onAliasChange} domain={domain} />);
+        } else {
+            privateLabel = (<p>{_t("This room is private, and can only be joined by invitation.")}</p>);
+        }
+
+        const title = this.state.isPublic ? _t('Create a public room') : _t('Create a private room');
         return (
             <BaseDialog className="mx_CreateRoomDialog" onFinished={this.props.onFinished}
-                title={_t('Create Room')}
+                title={title}
             >
                 <form onSubmit={this.onOk}>
                     <div className="mx_Dialog_content">
                         <Field className="mx_CreateRoomDialog_input" label={ _t('Name') } onChange={this.onNameChange} />
                         <Field label={ _t('Topic (optional)') } onChange={this.onTopicChange} />
+                        <LabelledToggleSwitch label={ _t("Make this room public")} onChange={this.onPublicChange} value={this.state.isPublic} />
+                        { privateLabel }
+                        { publicLabel }
+                        { aliasField }
                             <div>
                                 <input type="checkbox" id="checkbox" ref="checkbox" defaultChecked={this.defaultNoFederate} />
                                 <label htmlFor="checkbox">
