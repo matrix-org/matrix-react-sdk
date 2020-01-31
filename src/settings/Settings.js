a10a3c2721e6bb8b939b742587d22736d4b75dcb
Damir Jelić <poljar@termina.org.uk>
2020-01-31 17:27:08 +0100

    ManageEventIndexDialog: Add back a control for the crawler sleep time.

--- src/settings/Settings.js
+++ src/settings/Settings.js
@@ -144,9 +144,10 @@ export const SETTINGS = {
     },
     "feature_cross_signing": {
         isFeature: true,
-        displayName: _td("Enable cross-signing to verify per-user instead of per-session (in development)"),
+        displayName: _td("Enable cross-signing to verify per-user instead of per-device (in development)"),
         supportedLevels: LEVELS_FEATURE,
         default: false,
+        controller: new ReloadOnChangeController(),
     },
     "feature_event_indexing": {
         isFeature: true,
@@ -159,12 +160,6 @@ export const SETTINGS = {
         supportedLevels: LEVELS_FEATURE,
         displayName: _td("Show info about bridges in room settings"),
         default: false,
-    },
-    "feature_invite_only_padlocks": {
-        isFeature: true,
-        supportedLevels: LEVELS_FEATURE,
-        displayName: _td("Show padlocks on invite only rooms"),
-        default: true,
     },
     "MessageComposerInput.suggestEmoji": {
         supportedLevels: LEVELS_ACCOUNT_SETTINGS,
@@ -354,8 +349,8 @@ export const SETTINGS = {
         supportedLevels: ['room-device', 'device'],
         supportedLevelsAreOrdered: true,
         displayName: {
-            "default": _td('Never send encrypted messages to unverified sessions from this session'),
-            "room-device": _td('Never send encrypted messages to unverified sessions in this room from this session'),
+            "default": _td('Never send encrypted messages to unverified devices from this device'),
+            "room-device": _td('Never send encrypted messages to unverified devices in this room from this device'),
         },
         default: false,
     },
@@ -491,9 +486,9 @@ export const SETTINGS = {
         displayName: _td("Enable message search in encrypted rooms"),
         default: true,
     },
-    "keepSecretStoragePassphraseForSession": {
+    "crawlerSleepTime": {
         supportedLevels: LEVELS_DEVICE_ONLY_SETTINGS,
-        displayName: _td("Keep secret storage passphrase in memory for this session"),
-        default: false,
+        displayName: _td("How fast should messages be downloaded."),
+        default: 3000,
     },
 };
