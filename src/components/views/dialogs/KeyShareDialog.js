662f0dbf77e791eb6d151f86a13482679ec0c3f7
Bruno Windels <brunow@matrix.org>
2020-04-14 15:21:20 +0200

    fix lint

--- src/components/views/dialogs/KeyShareDialog.js
+++ src/components/views/dialogs/KeyShareDialog.js
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import Modal from '../../../Modal';
 import {verifyDevice} from '../../../verification';
 import React from 'react';
 import createReactClass from 'create-react-class';
