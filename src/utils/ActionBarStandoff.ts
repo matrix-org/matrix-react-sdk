86a2f7066977cf601ae055e92bc4b4a09fbd1bf5
Resynth <resynth1943@tutanota.com>
2020-10-25 01:48:13 +0000

    Check events for standoff requirements
    Signed-off-by: Resynth <resynth1943@tutanota.com>

---
+++ src/utils/ActionBarStandoff.ts
@@ -0,0 +1,18 @@
+import { func } from "prop-types";
+import { MatrixEvent } from "../indexing/BaseEventIndexManager";
+
+/** A list of event types that require standoff in MessageActionBar.
+ * Standoff stops the action bar intersecting with the MessageEvent.
+ * See: https://github.com/matrix-org/matrix-react-sdk/pull/5329
+ */
+const eventTypesWithActionBarStandoff = [
+    'm.sticker',
+    'm.video',
+    'm.key.verification.request',
+    'm.room.encryption'
+];
+
+export function eventRequiresActionBarStandoff (event: MatrixEvent) {
+    return eventTypesWithActionBarStandoff.includes(event.type)
+        || event.type === 'm.room.message' && (event.content as any)?.msgtype === 'm.image';
+}
