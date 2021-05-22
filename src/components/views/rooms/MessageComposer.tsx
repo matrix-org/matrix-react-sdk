fc4babada0721b0c3b4202a53d48b5cdd581305a
Šimon Brandner <simon.bra.ag@gmail.com>
2021-05-22 17:12:53 +0200

    Remove unused import
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/components/views/rooms/MessageComposer.tsx
+++ src/components/views/rooms/MessageComposer.tsx
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import React, {createRef, useCallback} from 'react';
+import React, {useCallback} from 'react';
 import classNames from 'classnames';
 import { _t } from '../../../languageHandler';
 import {MatrixClientPeg} from '../../../MatrixClientPeg';
