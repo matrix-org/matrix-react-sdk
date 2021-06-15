43db20ad4aa12e6773e596fc7f91e915cd8228e7
github-action-benchmark <github@users.noreply.github.com>
2021-06-15 13:23:47 +0000

    add Benchmark (jsperformanceentry) benchmark result for bef20dde48faa80a2bedeb29db84be6cfe09b323

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623749637064,
+  "lastUpdate": 1623763427772,
   "repoUrl": "https://github.com/matrix-org/matrix-react-sdk",
   "entries": {
     "Benchmark": [
@@ -50,6 +50,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "mx_Register",
             "value": 6046.299999999975,
+            "unit": "ms",
+            "extra": "type: measure"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "germain@souquet.com",
+            "name": "Germain",
+            "username": "gsouquet"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bef20dde48faa80a2bedeb29db84be6cfe09b323",
+          "message": "Merge pull request #6177 from matrix-org/gsouquet/benchmark-tracking",
+          "timestamp": "2021-06-15T14:17:59+01:00",
+          "tree_id": "f52875a04c63ff4e2ce6903affd9f9113ff5b926",
+          "url": "https://github.com/matrix-org/matrix-react-sdk/commit/bef20dde48faa80a2bedeb29db84be6cfe09b323"
+        },
+        "date": 1623763426137,
+        "tool": "jsperformanceentry",
+        "benches": [
+          {
+            "name": "mx_Register",
+            "value": 4508.595000000014,
             "unit": "ms",
             "extra": "type: measure"
           }
