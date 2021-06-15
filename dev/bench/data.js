8129351cb79638b8cb90c9daefd93f326dfabd0b
github-action-benchmark <github@users.noreply.github.com>
2021-06-15 09:16:26 +0000

    add Benchmark (jsperformanceentry) benchmark result for 2b2e83b9e755f98de9287ed6d19c2bff098f09b9

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1623748586473,
+  "repoUrl": "https://github.com/matrix-org/matrix-react-sdk",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "matrix-org",
+            "username": "matrix-org"
+          },
+          "committer": {
+            "name": "matrix-org",
+            "username": "matrix-org"
+          },
+          "id": "2b2e83b9e755f98de9287ed6d19c2bff098f09b9",
+          "message": "Add JS benchmark tracking in CI",
+          "timestamp": "2021-06-15T07:10:43Z",
+          "url": "https://github.com/matrix-org/matrix-react-sdk/pull/6177/commits/2b2e83b9e755f98de9287ed6d19c2bff098f09b9"
+        },
+        "date": 1623748584925,
+        "tool": "jsperformanceentry",
+        "benches": [
+          {
+            "name": "mx_Register",
+            "value": 4661.519999999996,
+            "unit": "ms",
+            "extra": "type: measure"
+          }
+        ]
+      }
+    ]
+  }
+}
