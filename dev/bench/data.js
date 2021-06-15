a2fa65ba07c4dff8e1b73f64d5ef7b898f5374ff
github-action-benchmark <github@users.noreply.github.com>
2021-06-15 09:33:57 +0000

    add Benchmark (jsperformanceentry) benchmark result for d5f7f524e13b580dc5ed0384f4b1930f9a65ea60

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623748586473,
+  "lastUpdate": 1623749637064,
   "repoUrl": "https://github.com/matrix-org/matrix-react-sdk",
   "entries": {
     "Benchmark": [
@@ -24,6 +24,32 @@ window.BENCHMARK_DATA = {
           {
             "name": "mx_Register",
             "value": 4661.519999999996,
+            "unit": "ms",
+            "extra": "type: measure"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "matrix-org",
+            "username": "matrix-org"
+          },
+          "committer": {
+            "name": "matrix-org",
+            "username": "matrix-org"
+          },
+          "id": "d5f7f524e13b580dc5ed0384f4b1930f9a65ea60",
+          "message": "Add JS benchmark tracking in CI",
+          "timestamp": "2021-06-15T07:10:43Z",
+          "url": "https://github.com/matrix-org/matrix-react-sdk/pull/6177/commits/d5f7f524e13b580dc5ed0384f4b1930f9a65ea60"
+        },
+        "date": 1623749635472,
+        "tool": "jsperformanceentry",
+        "benches": [
+          {
+            "name": "mx_Register",
+            "value": 6046.299999999975,
             "unit": "ms",
             "extra": "type: measure"
           }
