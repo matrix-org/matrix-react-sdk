c94e309f05f5f909e2c96a63557f3c1844b55749
Andrew Morgan <andrew@amorgan.xyz>
2021-07-09 16:27:17 +0100

    Spacing fixes, dial button white on dark theme, fix field width

--- src/components/views/context_menus/DialpadContextMenu.tsx
+++ src/components/views/context_menus/DialpadContextMenu.tsx
@@ -57,7 +57,7 @@ export default class DialpadContextMenu extends React.Component<IProps, IState> 
         return <ContextMenu {...this.props}>
             <div className="mx_DialPadContextMenuWrapper">
                 <div>
-                    <AccessibleButton className="mx_DialPadModal_cancel" onClick={this.onCancelClick} />
+                    <AccessibleButton className="mx_DialPadContextMenu_cancel" onClick={this.onCancelClick} />
                 </div>
                 <div className="mx_DialPadContextMenu_header">
                     <Field className="mx_DialPadContextMenu_dialled"
