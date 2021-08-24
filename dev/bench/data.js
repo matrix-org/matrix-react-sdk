c3ba713775e56a88d1787c621bfab8aac1625290
github-action-benchmark <github@users.noreply.github.com>
2021-08-24 18:42:25 +0000

    add Benchmark (jsperformanceentry) benchmark result for 49453f2d497393845454941f8388b389d8eb6822

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1629830295267,
+  "lastUpdate": 1629830545309,
   "repoUrl": "https://github.com/matrix-org/matrix-react-sdk",
   "entries": {
     "Benchmark": [
@@ -19472,6 +19472,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "mx_VerifyE2EEUser",
             "value": 4507.699999999953,
+            "unit": "ms",
+            "extra": "type: measure"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "travisr@matrix.org",
+            "name": "Travis Ralston",
+            "username": "turt2live"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "49453f2d497393845454941f8388b389d8eb6822",
+          "message": "Merge pull request #6604 from SimonBrandner/task/colors-2\n\nColor de-duplication #2",
+          "timestamp": "2021-08-24T12:37:18-06:00",
+          "tree_id": "a9e755e9ac13cac89ff3e0255b7ff9af9838de90",
+          "url": "https://github.com/matrix-org/matrix-react-sdk/commit/49453f2d497393845454941f8388b389d8eb6822"
+        },
+        "date": 1629830543737,
+        "tool": "jsperformanceentry",
+        "benches": [
+          {
+            "name": "mx_Register",
+            "value": 4017.9000000000233,
+            "unit": "ms",
+            "extra": "type: measure"
+          },
+          {
+            "name": "mx_JoinRoom",
+            "value": 837.5999999999767,
+            "unit": "ms",
+            "extra": "type: measure"
+          },
+          {
+            "name": "mx_CreateDM",
+            "value": 1738,
+            "unit": "ms",
+            "extra": "type: measure"
+          },
+          {
+            "name": "mx_VerifyE2EEUser",
+            "value": 3990.5,
             "unit": "ms",
             "extra": "type: measure"
           }
