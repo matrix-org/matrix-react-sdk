da395e6573381484ded45d1d2dfcfb97fd4cffec
Robert Long <robert@robertlong.me>
2021-09-21 11:13:31 -0700

    Increase initialSyncLimit

--- src/MatrixClientPeg.ts
+++ src/MatrixClientPeg.ts
@@ -111,7 +111,7 @@ class MatrixClientPegClass implements IMatrixClientPeg {
     // at any time up to after the 'will_start_client'
     // event is finished processing.
     public opts: IStartClientOpts = {
-        initialSyncLimit: 20,
+        initialSyncLimit: 50,
     };
 
     private matrixClient: MatrixClient = null;
