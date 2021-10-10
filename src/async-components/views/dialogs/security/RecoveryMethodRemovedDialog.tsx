4b2a55291e46770e8fe38ee7e5d8ffca2140811c
Šimon Brandner <simon.bra.ag@gmail.com>
2021-10-10 08:24:16 +0200

    Convert RecoveryMethodRemovedDialog to TS
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/async-components/views/dialogs/security/RecoveryMethodRemovedDialog.tsx
+++ src/async-components/views/dialogs/security/RecoveryMethodRemovedDialog.tsx
@@ -16,35 +16,31 @@ limitations under the License.
 */
 
 import React from "react";
-import PropTypes from "prop-types";
-import * as sdk from "../../../../index";
 import dis from "../../../../dispatcher/dispatcher";
 import { _t } from "../../../../languageHandler";
 import Modal from "../../../../Modal";
 import { Action } from "../../../../dispatcher/actions";
+import { IDialogProps } from "../../../../components/views/dialogs/IDialogProps";
+import BaseDialog from "../../../../components/views/dialogs/BaseDialog";
+import DialogButtons from "../../../../components/views/elements/DialogButtons";
 
-export default class RecoveryMethodRemovedDialog extends React.PureComponent {
-    static propTypes = {
-        onFinished: PropTypes.func.isRequired,
-    }
+interface IProps extends IDialogProps {}
 
-    onGoToSettingsClick = () => {
+export default class RecoveryMethodRemovedDialog extends React.PureComponent<IProps> {
+    private onGoToSettingsClick = (): void => {
         this.props.onFinished();
         dis.fire(Action.ViewUserSettings);
-    }
+    };
 
-    onSetupClick = () => {
+    private onSetupClick = (): void => {
         this.props.onFinished();
         Modal.createTrackedDialogAsync("Key Backup", "Key Backup",
             import("./CreateKeyBackupDialog"),
             null, null, /* priority = */ false, /* static = */ true,
         );
-    }
+    };
 
-    render() {
-        const BaseDialog = sdk.getComponent("views.dialogs.BaseDialog");
-        const DialogButtons = sdk.getComponent("views.elements.DialogButtons");
-
+    public render(): JSX.Element {
         const title = <span className="mx_KeyBackupFailedDialog_title">
             { _t("Recovery Method Removed") }
         </span>;
