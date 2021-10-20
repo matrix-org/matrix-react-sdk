8983105c1863228868f0b0a5360a08a69467ef0b
Germain Souquet <germains@element.io>
2021-10-20 13:18:12 +0100

    Fix threads broken layout

--- src/components/structures/ThreadPanel.tsx
+++ src/components/structures/ThreadPanel.tsx
@@ -217,7 +217,7 @@ const ThreadPanel: React.FC<IProps> = ({ roomId, onClose }) => {
                     showReactions={true}
                     className="mx_RoomView_messagePanel mx_GroupLayout"
                     membersLoaded={true}
-                    tileShape={TileShape.Thread}
+                    tileShape={TileShape.ThreadPanel}
                 />
             </BaseCard>
         </RoomContext.Provider>
