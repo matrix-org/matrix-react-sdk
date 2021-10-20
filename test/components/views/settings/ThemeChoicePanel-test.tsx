254c41bafb06a40bf29e941ace3acae56470eb1e
Andy Balaam <andyb@element.io>
2021-10-20 15:51:04 +0100

    Fix copyright for ThemeChoicePanel

--- test/components/views/settings/ThemeChoicePanel-test.tsx
+++ test/components/views/settings/ThemeChoicePanel-test.tsx
@@ -23,6 +23,8 @@ import _ThemeChoicePanel from '../../../../src/components/views/settings/ThemeCh
 
 const ThemeChoicePanel = TestUtils.wrapInMatrixClientContext(_ThemeChoicePanel);
 
+// Avoid errors about global.matchMedia. See:
+// https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
 Object.defineProperty(window, 'matchMedia', {
     writable: true,
     value: jest.fn().mockImplementation(query => ({
