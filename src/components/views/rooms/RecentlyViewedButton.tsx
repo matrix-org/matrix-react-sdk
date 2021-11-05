391d89d05804703d533e097766282416b86d1f5b
Michael Telatynski <7t3chguy@gmail.com>
2021-11-05 12:42:08 +0000

    Make the recently viewed run on hover and move it to the right

--- src/components/views/rooms/RecentlyViewedButton.tsx
+++ src/components/views/rooms/RecentlyViewedButton.tsx
@@ -18,83 +18,68 @@ import React, { useContext } from "react";
 
 import { BreadcrumbsStore } from "../../../stores/BreadcrumbsStore";
 import { UPDATE_EVENT } from "../../../stores/AsyncStore";
-import {
-    ContextMenu,
-    ContextMenuTooltipButton,
-    MenuItem,
-    toRightOf,
-    useContextMenu,
-} from "../../structures/ContextMenu";
+import { MenuItem } from "../../structures/ContextMenu";
 import { useEventEmitterState } from "../../../hooks/useEventEmitter";
 import { _t } from "../../../languageHandler";
 import RoomAvatar from "../avatars/RoomAvatar";
 import dis from "../../../dispatcher/dispatcher";
 import SpaceStore from "../../../stores/SpaceStore";
 import MatrixClientContext from "../../../contexts/MatrixClientContext";
+import InteractiveTooltip, { Direction } from "../elements/InteractiveTooltip";
 
 const RecentlyViewedButton = () => {
     const cli = useContext(MatrixClientContext);
-    const [menuDisplayed, handle, openMenu, closeMenu] = useContextMenu<HTMLDivElement>();
     const crumbs = useEventEmitterState(BreadcrumbsStore.instance, UPDATE_EVENT, () => BreadcrumbsStore.instance.rooms);
 
-    let contextMenu: JSX.Element;
-    if (menuDisplayed) {
-        contextMenu = <ContextMenu
-            {...toRightOf(handle.current.getBoundingClientRect())}
-            onFinished={closeMenu}
-            wrapperClassName="mx_RecentlyViewedButton_ContextMenu"
-        >
-            <h4>{ _t("Recently viewed") }</h4>
+    const content = <div className="mx_RecentlyViewedButton_ContextMenu">
+        <h4>{ _t("Recently viewed") }</h4>
 
-            { crumbs.map(crumb => {
-                let parentsSection: JSX.Element;
-                if (!crumb.isSpaceRoom()) {
-                    const [parent, ...otherParents] = SpaceStore.instance.getKnownParents(crumb.roomId);
-                    if (parent) {
-                        parentsSection = <div className="mx_RecentlyViewedButton_entry_spaces">
-                            { _t("%(spaceName)s and %(count)s others", {
-                                spaceName: cli.getRoom(parent).name,
-                                count: otherParents.length,
-                            }) }
-                        </div>;
-                    }
+        { crumbs.map(crumb => {
+            let parentsSection: JSX.Element;
+            if (!crumb.isSpaceRoom()) {
+                const [parent, ...otherParents] = SpaceStore.instance.getKnownParents(crumb.roomId);
+                if (parent) {
+                    parentsSection = <div className="mx_RecentlyViewedButton_entry_spaces">
+                        { _t("%(spaceName)s and %(count)s others", {
+                            spaceName: cli.getRoom(parent).name,
+                            count: otherParents.length,
+                        }) }
+                    </div>;
                 }
+            }
 
-                return <MenuItem
-                    key={crumb.roomId}
-                    onClick={() => {
-                        dis.dispatch({
-                            action: "view_room",
-                            room_id: crumb.roomId,
-                        });
-                        closeMenu();
-                    }}
-                >
-                    <RoomAvatar
-                        room={crumb}
-                        width={24}
-                        height={24}
-                    />
-                    <span className="mx_RecentlyViewedButton_entry_label">
-                        <div>{ crumb.name }</div>
-                        { parentsSection }
-                    </span>
-                </MenuItem>;
-            }) }
-        </ContextMenu>;
-    }
+            return <MenuItem
+                key={crumb.roomId}
+                onClick={() => {
+                    dis.dispatch({
+                        action: "view_room",
+                        room_id: crumb.roomId,
+                    });
+                }}
+            >
+                <RoomAvatar
+                    room={crumb}
+                    width={24}
+                    height={24}
+                />
+                <span className="mx_RecentlyViewedButton_entry_label">
+                    <div>{ crumb.name }</div>
+                    { parentsSection }
+                </span>
+            </MenuItem>;
+        }) }
+    </div>;
 
-    return <>
-        <ContextMenuTooltipButton
-            className="mx_LeftPanel_recentsButton"
-            onClick={openMenu}
-            title={_t("Recently viewed")}
-            isExpanded={menuDisplayed}
-            inputRef={handle}
-        />
-
-        { contextMenu }
-    </>;
+    return <InteractiveTooltip content={content} direction={Direction.Right}>
+        { ({ ref, onMouseOver }) => (
+            <span
+                className="mx_LeftPanel_recentsButton"
+                title={_t("Recently viewed")}
+                ref={ref}
+                onMouseOver={onMouseOver}
+            />
+        ) }
+    </InteractiveTooltip>;
 };
 
 export default RecentlyViewedButton;
