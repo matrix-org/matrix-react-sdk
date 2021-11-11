1abc0518f4b60cae5146ae70f2a48eaae341c8b9
Robert Long <robert@robertlong.me>
2021-11-10 16:47:39 -0800

    Update video tile styling

--- src/components/views/voip/GroupCallView/VideoTile.tsx
+++ src/components/views/voip/GroupCallView/VideoTile.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { animated } from "@react-spring/web";
 import classNames from "classnames";
 import { useCallFeed } from "../../../../hooks/useCallFeed";
@@ -6,6 +6,8 @@ import { useMediaStream } from "../../../../hooks/useMediaStream";
 import { useRoomMemberName } from "../../../../hooks/useRoomMemberName";
 import { CallFeed } from "matrix-js-sdk/src/webrtc/callFeed";
 import { SDPStreamMetadataPurpose } from "matrix-js-sdk/src/webrtc/callEventTypes";
+
+const defaultColors = ['#0DBD8B', '#368bd6', '#ac3ba8'];
 
 interface IVideoTileProps {
     style?: any;
@@ -19,6 +21,10 @@ export default function VideoTile({
     disableSpeakingHighlight,
     ...rest
 }: IVideoTileProps) {
+    const [avatarBackgroundColor] = useState(() => {
+        const avatarBackgroundColorIndex = Math.round(Math.random() * (defaultColors.length - 1));
+        return defaultColors[avatarBackgroundColorIndex];
+    });
     const { isLocal, audioMuted, videoMuted, speaking, stream, member, purpose } = useCallFeed(callFeed);
     const name = useRoomMemberName(member);
     const mediaRef = useMediaStream<HTMLVideoElement>(stream, isLocal);
@@ -35,14 +41,22 @@ export default function VideoTile({
             style={style}
             {...rest}
         >
+            { videoMuted && (
+                <>
+                    <div className="mx_videoMutedOverlay" />
+                    <svg className="mx_videoMutedAvatar" height="50%" viewBox="0 0 50 50" preserveAspectRatio="xMinYmin">
+                        <circle r="25" cx="25" cy="25" fill={avatarBackgroundColor} />
+                        <text x="25" y="27" fontSize="20" textAnchor="middle" dominantBaseline="middle" fill="white">
+                            { name.slice(0, 1).toUpperCase() }
+                        </text>
+                    </svg>
+                </>
+            ) }
             { purpose === SDPStreamMetadataPurpose.Usermedia && (
                 <div className="mx_memberName">
                     <i className={audioMuted ? "mx_muteMicIcon" : "mx_micIcon"} />
                     <span title={name}>{ name }</span>
                 </div>
-            ) }
-            { videoMuted && (
-                <i className="mx_videoMutedIcon" />
             ) }
             <video ref={mediaRef} playsInline disablePictureInPicture />
         </animated.div>
