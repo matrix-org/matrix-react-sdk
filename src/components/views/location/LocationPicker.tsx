94f1ff9f1ac3d2da7487388ceb8d7f062e36347a
Matthew Hodgson <matthew@matrix.org>
2021-11-21 20:17:41 +0100

    disable the streaming options until they're hooked up

--- src/components/views/location/LocationPicker.tsx
+++ src/components/views/location/LocationPicker.tsx
@@ -40,16 +40,16 @@ const LocationShareTypeDropdown = ({
     onChange,
 }: IDropdownProps) => {
     const options = [
-        <div key={LocationShareType.CUSTOM}>{ _t("Share custom location") }</div>,
+        // <div key={LocationShareType.CUSTOM}>{ _t("Share custom location") }</div>,
         <div key={LocationShareType.ONE_OFF}>{ _t("Share my current location as a one off") }</div>,
-        <div key={LocationShareType.ONE_MIN}>{ _t("Share my current location for one minute") }</div>,
-        <div key={LocationShareType.FIVE_MINS}>{ _t("Share my current location for five minutes") }</div>,
-        <div key={LocationShareType.THIRTY_MINS}>{ _t("Share my current location for thirty minutes") }</div>,
-        <div key={LocationShareType.ONE_HOUR}>{ _t("Share my current location for one hour") }</div>,
-        <div key={LocationShareType.THREE_HOURS}>{ _t("Share my current location for three hours") }</div>,
-        <div key={LocationShareType.SIX_HOURS}>{ _t("Share my current location for six hours") }</div>,
-        <div key={LocationShareType.ONE_DAY}>{ _t("Share my current location for one day") }</div>,
-        <div key={LocationShareType.FOREVER}>{ _t("Share my current location until I disable it") }</div>,
+        // <div key={LocationShareType.ONE_MIN}>{ _t("Share my current location for one minute") }</div>,
+        // <div key={LocationShareType.FIVE_MINS}>{ _t("Share my current location for five minutes") }</div>,
+        // <div key={LocationShareType.THIRTY_MINS}>{ _t("Share my current location for thirty minutes") }</div>,
+        // <div key={LocationShareType.ONE_HOUR}>{ _t("Share my current location for one hour") }</div>,
+        // <div key={LocationShareType.THREE_HOURS}>{ _t("Share my current location for three hours") }</div>,
+        // <div key={LocationShareType.SIX_HOURS}>{ _t("Share my current location for six hours") }</div>,
+        // <div key={LocationShareType.ONE_DAY}>{ _t("Share my current location for one day") }</div>,
+        // <div key={LocationShareType.FOREVER}>{ _t("Share my current location until I disable it") }</div>,
     ];
 
     return <Dropdown
