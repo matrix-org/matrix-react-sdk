541359dc26192a2488bb93145c6734450ed8e40f
Dariusz Niemczyk <dariuszn@element.io>
2021-11-25 21:23:36 +0100

    Add some better docs

--- src/Markdown.ts
+++ src/Markdown.ts
@@ -110,6 +110,18 @@ export default class Markdown {
         this.parsed = this.repairLinks(this.parsed);
     }
 
+    /**
+     * This method is modifying the parsed AST in such a way that links are always
+     * properly linkified instead of sometimes being wrongly emphasised in case
+     * if you were to write a link like the example below:
+     * https://my_weird-link_domain.domain.com
+     * ^ this link would be parsed to something like this:
+     * <a href="https://my">https://my</a><b>weird-link</b><a href="https://domain.domain.com">domain.domain.com</a>
+     * This method makes it so the link gets properly modified to a version where it is
+     * not emphasised until it actually ends.
+     * See: https://github.com/vector-im/element-web/issues/4674
+     * @param parsed
+     */
     private repairLinks(parsed: commonmark.Node) {
         const walker = parsed.walker();
         let event: commonmark.NodeWalkingStep = null;
