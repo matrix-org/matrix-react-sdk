01aeec82c55879552f7097b3f21ea4d553f0c021
github-action-benchmark <github@users.noreply.github.com>
2021-11-26 11:51:31 +0000

    add Benchmark (jsperformanceentry) benchmark result for 138f6685d40ecafecbe0e0bd409f480d507a4a6b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1637923523727,
+  "lastUpdate": 1637927491038,
   "repoUrl": "https://github.com/matrix-org/matrix-react-sdk",
   "entries": {
     "Benchmark": [
@@ -41996,6 +41996,54 @@ window.BENCHMARK_DATA = {
           {
             "name": "mx_VerifyE2EEUser",
             "value": 4701.400000000023,
+            "unit": "ms",
+            "extra": "type: measure"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "7t3chguy@gmail.com",
+            "name": "Michael Telatynski",
+            "username": "t3chguy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "138f6685d40ecafecbe0e0bd409f480d507a4a6b",
+          "message": "re-enable Spaces e2e tests (#7207)",
+          "timestamp": "2021-11-26T11:45:31Z",
+          "tree_id": "f816873ac5599498d4c556f781dffa6e43dd7be3",
+          "url": "https://github.com/matrix-org/matrix-react-sdk/commit/138f6685d40ecafecbe0e0bd409f480d507a4a6b"
+        },
+        "date": 1637927488682,
+        "tool": "jsperformanceentry",
+        "benches": [
+          {
+            "name": "mx_Register",
+            "value": 5179.5,
+            "unit": "ms",
+            "extra": "type: measure"
+          },
+          {
+            "name": "mx_JoinRoom",
+            "value": 931.7999999999302,
+            "unit": "ms",
+            "extra": "type: measure"
+          },
+          {
+            "name": "mx_CreateDM",
+            "value": 1640.5999999999767,
+            "unit": "ms",
+            "extra": "type: measure"
+          },
+          {
+            "name": "mx_VerifyE2EEUser",
+            "value": 4702.099999999977,
             "unit": "ms",
             "extra": "type: measure"
           }
