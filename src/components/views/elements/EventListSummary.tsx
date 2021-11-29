6ba96babf4761e927cd373b2143427398d18a9b5
Michael Telatynski <7t3chguy@gmail.com>
2021-11-29 15:56:22 +0000

    delint

--- src/components/views/elements/EventListSummary.tsx
+++ src/components/views/elements/EventListSummary.tsx
@@ -14,7 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import React, { ReactNode, useEffect } from 'react';
+import React, { ReactNode, useEffect } from "react";
+import { uniqBy } from "lodash";
 import { MatrixEvent } from "matrix-js-sdk/src/models/event";
 import { RoomMember } from "matrix-js-sdk/src/models/room-member";
 
@@ -23,7 +24,6 @@ import { _t } from '../../../languageHandler';
 import { useStateToggle } from "../../../hooks/useStateToggle";
 import AccessibleButton from "./AccessibleButton";
 import { Layout } from '../../../settings/enums/Layout';
-import { uniqBy } from "lodash";
 
 interface IProps {
     // An array of member events to summarise
