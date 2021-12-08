c1e07c5c1ab8ba7034c53346a5b6bf751e8136ec
Kerry Archibald <kerrya@element.io>
2021-12-08 16:01:37 +0100

    SettingsFieldset component
    Signed-off-by: Kerry Archibald <kerrya@element.io>

---
+++ src/components/views/settings/SettingsFieldset.tsx
@@ -0,0 +1,15 @@
+import classNames from 'classnames';
+import React, { ReactNode, HTMLAttributes } from 'react';
+
+interface Props extends HTMLAttributes<HTMLFieldSetElement> {
+    // section title
+    legend: string | ReactNode;
+    description?: string | ReactNode;
+}
+
+export const SettingsFieldset: React.FC<Props> = ({ legend, className, children, description, ...rest }) =>
+    <fieldset {...rest} className={classNames('mx_SettingsFieldset', className)}>
+        <legend className='mx_SettingsFieldset_legend'>{legend}</legend>
+        { description && <p className='mx_SettingsFieldset_description'>{description}</p>}
+        {children}
+    </fieldset>;
