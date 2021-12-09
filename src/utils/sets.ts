905bcc4caa5fb602acce4aab3ba36192db3fa310
Michael Telatynski <7t3chguy@gmail.com>
2021-12-09 14:23:38 +0000

    delint

--- src/utils/sets.ts
+++ src/utils/sets.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { arrayDiff, arrayUnion } from "./arrays";
+import { arrayDiff } from "./arrays";
 
 /**
  * Determines if two sets are different through a shallow comparison.
