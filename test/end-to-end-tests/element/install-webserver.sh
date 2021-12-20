a4e2b3894609c0d0f3de83ae6850fe66d797784c
Olivier <olivier@librepush.net>
2021-12-20 17:48:33 +0000

    Faff around with install-webserver.sh

--- test/end-to-end-tests/element/install-webserver.sh
+++ test/end-to-end-tests/element/install-webserver.sh
@@ -6,14 +6,14 @@ cd $BASE_DIR
 # Install ComplexHttpServer (a drop in replacement for Python's SimpleHttpServer
 # but with support for multiple threads) into a virtualenv.
 (
-    virtualenv -p python3 env
-    source env/bin/activate
+    virtualenv -p python3 env-webserver
+    source env-webserver/bin/activate
 
     # Having been bitten by pip SSL fail too many times, I don't trust the existing pip
     # to be able to --upgrade itself, so grab a new one fresh from source.
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py
-    rm get-pip.py
+    #curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    pip install --upgrade pip
+    pip install setuptools==v59.8.0
 
     pip install ComplexHttpServer
 
