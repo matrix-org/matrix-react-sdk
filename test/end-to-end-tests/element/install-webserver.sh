f39aff4cd7ff4662bac977556505d21181c87292
Olivier <olivier@librepush.net>
2021-12-20 17:53:42 +0000

    Actually I don't think these clash anyway

--- test/end-to-end-tests/element/install-webserver.sh
+++ test/end-to-end-tests/element/install-webserver.sh
@@ -6,8 +6,8 @@ cd $BASE_DIR
 # Install ComplexHttpServer (a drop in replacement for Python's SimpleHttpServer
 # but with support for multiple threads) into a virtualenv.
 (
-    virtualenv -p python3 env-webserver
-    source env-webserver/bin/activate
+    virtualenv -p python3 env
+    source env/bin/activate
 
     # Having been bitten by pip SSL fail too many times, I don't trust the existing pip
     # to be able to --upgrade itself, so grab a new one fresh from source.
