34f7485d5c427eb79dc83f24703d8c0bd023a2ea
Kerry Archibald <kerrya@element.io>
2021-12-23 11:25:56 +0100

    inline link buttons in TextForEvent
    Signed-off-by: Kerry Archibald <kerrya@element.io>

--- src/TextForEvent.tsx
+++ src/TextForEvent.tsx
@@ -33,6 +33,7 @@ import defaultDispatcher from './dispatcher/dispatcher';
 import { SetRightPanelPhasePayload } from './dispatcher/payloads/SetRightPanelPhasePayload';
 import { MatrixClientPeg } from "./MatrixClientPeg";
 import { ROOM_SECURITY_TAB } from "./components/views/dialogs/RoomSettingsDialog";
+import AccessibleButton from './components/views/elements/AccessibleButton';
 
 // These functions are frequently used just to check whether an event has
 // any text to display at all. For this reason they return deferred values
@@ -229,9 +230,9 @@ function textForJoinRulesEvent(ev: MatrixEvent, allowJSX: boolean): () => string
                     { _t('%(senderDisplayName)s changed who can join this room. <a>View settings</a>.', {
                         senderDisplayName,
                     }, {
-                        "a": (sub) => <a onClick={onViewJoinRuleSettingsClick}>
+                        "a": (sub) => <AccessibleButton kind='link_inline' onClick={onViewJoinRuleSettingsClick}>
                             { sub }
-                        </a>,
+                        </AccessibleButton>,
                     }) }
                 </span>;
             }
@@ -532,13 +533,13 @@ function textForPinnedEvent(event: MatrixEvent, allowJSX: boolean): () => string
                         { senderName },
                         {
                             "a": (sub) =>
-                                <a onClick={(e) => onPinnedOrUnpinnedMessageClick(messageId, roomId)}>
+                                <AccessibleButton kind='link_inline' onClick={(e) => onPinnedOrUnpinnedMessageClick(messageId, roomId)}>
                                     { sub }
-                                </a>,
+                                </AccessibleButton>,
                             "b": (sub) =>
-                                <a onClick={onPinnedMessagesClick}>
+                                <AccessibleButton kind='link_inline' onClick={onPinnedMessagesClick}>
                                     { sub }
-                                </a>,
+                                </AccessibleButton>,
                         },
                     ) }
                 </span>
@@ -560,13 +561,13 @@ function textForPinnedEvent(event: MatrixEvent, allowJSX: boolean): () => string
                         { senderName },
                         {
                             "a": (sub) =>
-                                <a onClick={(e) => onPinnedOrUnpinnedMessageClick(messageId, roomId)}>
+                                <AccessibleButton kind='link_inline' onClick={(e) => onPinnedOrUnpinnedMessageClick(messageId, roomId)}>
                                     { sub }
-                                </a>,
+                                </AccessibleButton>,
                             "b": (sub) =>
-                                <a onClick={onPinnedMessagesClick}>
+                                <AccessibleButton kind='link_inline' onClick={onPinnedMessagesClick}>
                                     { sub }
-                                </a>,
+                                </AccessibleButton>,
                         },
                     ) }
                 </span>
@@ -582,7 +583,12 @@ function textForPinnedEvent(event: MatrixEvent, allowJSX: boolean): () => string
                 { _t(
                     "%(senderName)s changed the <a>pinned messages</a> for the room.",
                     { senderName },
-                    { "a": (sub) => <a onClick={onPinnedMessagesClick}> { sub } </a> },
+                    {
+                        "a": (sub) =>
+                            <AccessibleButton kind='link_inline' onClick={onPinnedMessagesClick}>
+                                { sub }
+                            </AccessibleButton>,
+                    },
                 ) }
             </span>
         );
