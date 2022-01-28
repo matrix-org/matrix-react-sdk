f661e8701c1666706e3ea3d894f8fb0e6200d67b
Germain Souquet <germains@element.io>
2022-01-28 12:36:54 +0000

    Use replyToEvent instead of lastReply

--- src/components/views/rooms/EventTile.tsx
+++ src/components/views/rooms/EventTile.tsx
@@ -402,7 +402,7 @@ export default class EventTile extends React.Component<IProps, IState> {
 
             thread,
             threadReplyCount: thread?.length,
-            threadLastReply: thread?.lastReply(),
+            threadLastReply: thread?.replyToEvent,
         };
 
         // don't do RR animations until we are mounted
@@ -556,7 +556,7 @@ export default class EventTile extends React.Component<IProps, IState> {
         }
 
         this.setState({
-            threadLastReply: thread?.lastReply(),
+            threadLastReply: thread?.replyToEvent,
             threadReplyCount: thread?.length,
             thread,
         });
@@ -1271,7 +1271,7 @@ export default class EventTile extends React.Component<IProps, IState> {
         // Thread panel shows the timestamp of the last reply in that thread
         const ts = this.props.tileShape !== TileShape.ThreadPanel
             ? this.props.mxEvent.getTs()
-            : thread?.lastReply().getTs();
+            : thread?.replyToEvent.getTs();
 
         const messageTimestamp = <MessageTimestamp
             showRelative={this.props.tileShape === TileShape.ThreadPanel}
