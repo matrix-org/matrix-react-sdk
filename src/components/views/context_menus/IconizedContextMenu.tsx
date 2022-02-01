element-hq/element-web

commit 7fa27f583490a4a9bf0b5469fef6143bc5f5bc06

Add jump to date functionality to date headers in timeline v2 (#7339)
Fix https://github.com/vector-im/element-web/issues/7677

Utilizes MSC3030: https://github.com/matrix-org/matrix-doc/pull/3030

https://user-images.githubusercontent.com/558581/150060664-79627573-f4fd-497c-b726-dc3485854bd0.png

res/css/views/context_menus/_IconizedContextMenu.scss
@@ -50,21 +50,21 @@ limitations under the License.
         }
 
         // round the top corners of the top button for the hover effect to be bounded
-        &:first-child .mx_AccessibleButton:first-child {
+        &:first-child .mx_AccessibleButton:not(.mx_AccessibleButton_hasKind):first-child {
             border-radius: 8px 8px 0 0; // radius matches .mx_ContextualMenu
         }
 
         // round the bottom corners of the bottom button for the hover effect to be bounded
-        &:last-child .mx_AccessibleButton:last-child {
+        &:last-child .mx_AccessibleButton:not(.mx_AccessibleButton_hasKind):last-child {
             border-radius: 0 0 8px 8px; // radius matches .mx_ContextualMenu
         }
 
         // round all corners of the only button for the hover effect to be bounded
-        &:first-child:last-child .mx_AccessibleButton:first-child:last-child {
+        &:first-child:last-child .mx_AccessibleButton:not(.mx_AccessibleButton_hasKind):first-child:last-child {
             border-radius: 8px; // radius matches .mx_ContextualMenu
         }
 
-        .mx_AccessibleButton {
+        .mx_AccessibleButton:not(.mx_AccessibleButton_hasKind) {
             // pad the inside of the button so that the hover background is padded too
             padding-top: 12px;
             padding-bottom: 12px;
@@ -130,7 +130,7 @@ limitations under the License.
     }
 
     .mx_IconizedContextMenu_optionList_red {
-        .mx_AccessibleButton {
+        .mx_AccessibleButton:not(.mx_AccessibleButton_hasKind) {
             color: $alert !important;
         }
 
@@ -148,7 +148,7 @@ limitations under the License.
     }
 
     .mx_IconizedContextMenu_active {
-        &.mx_AccessibleButton, .mx_AccessibleButton {
+        &.mx_AccessibleButton:not(.mx_AccessibleButton_hasKind), .mx_AccessibleButton:not(.mx_AccessibleButton_hasKind) {
             color: $accent !important;
         }
 

res/css/views/messages/_JumpToDatePicker.scss
@@ -1,0 +1,37 @@
+/*
+Copyright 2022 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+.mx_JumpToDatePicker_form {
+    display: flex;
+}
+
+.mx_JumpToDatePicker_label {
+    align-self: center;
+    font-size: $font-15px;
+}
+
+.mx_JumpToDatePicker_datePicker {
+    margin: 0;
+    margin-left: 8px;
+
+    &, & > input {
+        border-radius: 8px;
+    }
+}
+
+.mx_JumpToDatePicker_submitButton {
+    margin-left: 8px;
+}

src/components/views/messages/DateSeparator.tsx
@@ -16,12 +16,26 @@ limitations under the License.
 */
 
 import React from 'react';
+import { Direction } from 'matrix-js-sdk/src/models/event-timeline';
+import { logger } from "matrix-js-sdk/src/logger";
 
 import { _t } from '../../../languageHandler';
 import { formatFullDateNoTime } from '../../../DateUtils';
 import { replaceableComponent } from "../../../utils/replaceableComponent";
+import { MatrixClientPeg } from '../../../MatrixClientPeg';
+import dis from '../../../dispatcher/dispatcher';
+import { Action } from '../../../dispatcher/actions';
 import SettingsStore from '../../../settings/SettingsStore';
 import { UIFeature } from '../../../settings/UIFeature';
+import Modal from '../../../Modal';
+import ErrorDialog from '../dialogs/ErrorDialog';
+import { contextMenuBelow } from '../rooms/RoomTile';
+import { ContextMenuTooltipButton } from "../../structures/ContextMenu";
+import IconizedContextMenu, {
+    IconizedContextMenuOption,
+    IconizedContextMenuOptionList,
+} from "../context_menus/IconizedContextMenu";
+import JumpToDatePicker from './JumpToDatePicker';
 
 function getDaysArray(): string[] {
     return [
@@ -36,13 +50,59 @@ function getDaysArray(): string[] {
 }
 
 interface IProps {
+    roomId: string;
     ts: number;
     forExport?: boolean;
 }
 
+interface IState {
+    contextMenuPosition?: DOMRect;
+    jumpToDateEnabled: boolean;
+}
+
 @replaceableComponent("views.messages.DateSeparator")
-export default class DateSeparator extends React.Component<IProps> {
-    private getLabel() {
+export default class DateSeparator extends React.Component<IProps, IState> {
+    private settingWatcherRef = null;
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            jumpToDateEnabled: SettingsStore.getValue("feature_jump_to_date"),
+        };
+
+        // We're using a watcher so the date headers in the timeline are updated
+        // when the lab setting is toggled.
+        this.settingWatcherRef = SettingsStore.watchSetting(
+            "feature_jump_to_date",
+            null,
+            (settingName, roomId, level, newValAtLevel, newVal) => {
+                this.setState({ jumpToDateEnabled: newVal });
+            },
+        );
+    }
+
+    componentWillUnmount() {
+        SettingsStore.unwatchSetting(this.settingWatcherRef);
+    }
+
+    private onContextMenuOpenClick = (e: React.MouseEvent): void => {
+        e.preventDefault();
+        e.stopPropagation();
+        const target = e.target as HTMLButtonElement;
+        this.setState({ contextMenuPosition: target.getBoundingClientRect() });
+    };
+
+    private onContextMenuCloseClick = (): void => {
+        this.closeMenu();
+    };
+
+    private closeMenu = (): void => {
+        this.setState({
+            contextMenuPosition: null,
+        });
+    };
+
+    private getLabel(): string {
         const date = new Date(this.props.ts);
         const disableRelativeTimestamps = !SettingsStore.getValue(UIFeature.TimelineEnableRelativeDates);
 
@@ -65,13 +125,127 @@ export default class DateSeparator extends React.Component<IProps> {
         }
     }
 
+    private pickDate = async (inputTimestamp): Promise<void> => {
+        const unixTimestamp = new Date(inputTimestamp).getTime();
+
+        const cli = MatrixClientPeg.get();
+        try {
+            const roomId = this.props.roomId;
+            const { event_id: eventId, origin_server_ts: originServerTs } = await cli.timestampToEvent(
+                roomId,
+                unixTimestamp,
+                Direction.Forward,
+            );
+            logger.log(
+                `/timestamp_to_event: ` +
+                `found ${eventId} (${originServerTs}) for timestamp=${unixTimestamp} (looking forward)`,
+            );
+
+            dis.dispatch({
+                action: Action.ViewRoom,
+                event_id: eventId,
+                highlighted: true,
+                room_id: roomId,
+            });
+        } catch (e) {
+            const code = e.errcode || e.statusCode;
+            // only show the dialog if failing for something other than a network error
+            // (e.g. no errcode or statusCode) as in that case the redactions end up in the
+            // detached queue and we show the room status bar to allow retry
+            if (typeof code !== "undefined") {
+                // display error message stating you couldn't delete this.
+                Modal.createTrackedDialog('Unable to find event at that date', '', ErrorDialog, {
+                    title: _t('Error'),
+                    description: _t('Unable to find event at that date. (%(code)s)', { code }),
+                });
+            }
+        }
+    };
+
+    private onLastWeekClicked = (): void => {
+        const date = new Date();
+        date.setDate(date.getDate() - 7);
+        this.pickDate(date);
+        this.closeMenu();
+    };
+
+    private onLastMonthClicked = (): void => {
+        const date = new Date();
+        // Month numbers are 0 - 11 and `setMonth` handles the negative rollover
+        date.setMonth(date.getMonth() - 1, 1);
+        this.pickDate(date);
+        this.closeMenu();
+    };
+
+    private onTheBeginningClicked = (): void => {
+        const date = new Date(0);
+        this.pickDate(date);
+        this.closeMenu();
+    };
+
+    private onDatePicked = (dateString): void => {
+        this.pickDate(dateString);
+        this.closeMenu();
+    };
+
+    private renderJumpToDateMenu(): React.ReactElement {
+        let contextMenu: JSX.Element;
+        if (this.state.contextMenuPosition) {
+            contextMenu = <IconizedContextMenu
+                {...contextMenuBelow(this.state.contextMenuPosition)}
+                compact
+                onFinished={this.onContextMenuCloseClick}
+            >
+                <IconizedContextMenuOptionList first>
+                    <IconizedContextMenuOption
+                        label={_t("Last week")}
+                        onClick={this.onLastWeekClicked}
+                    />
+                    <IconizedContextMenuOption
+                        label={_t("Last month")}
+                        onClick={this.onLastMonthClicked}
+                    />
+                    <IconizedContextMenuOption
+                        label={_t("The beginning of the room")}
+                        onClick={this.onTheBeginningClicked}
+                    />
+                </IconizedContextMenuOptionList>
+
+                <IconizedContextMenuOptionList>
+                    <JumpToDatePicker ts={this.props.ts} onDatePicked={this.onDatePicked} />
+                </IconizedContextMenuOptionList>
+            </IconizedContextMenu>;
+        }
+
+        return (
+            <ContextMenuTooltipButton
+                className="mx_DateSeparator_jumpToDateMenu"
+                onClick={this.onContextMenuOpenClick}
+                isExpanded={!!this.state.contextMenuPosition}
+                title={_t("Jump to date")}
+            >
+                <div aria-hidden="true">{ this.getLabel() }</div>
+                <div className="mx_DateSeparator_chevron" />
+                { contextMenu }
+            </ContextMenuTooltipButton>
+        );
+    }
+
     render() {
         const label = this.getLabel();
+
+        let dateHeaderContent;
+        if (this.state.jumpToDateEnabled) {
+            dateHeaderContent = this.renderJumpToDateMenu();
+        } else {
+            dateHeaderContent = <div aria-hidden="true">{ label }</div>;
+        }
+
         // ARIA treats <hr/>s as separators, here we abuse them slightly so manually treat this entire thing as one
         // tab-index=-1 to allow it to be focusable but do not add tab stop for it, primarily for screen readers
         return <h2 className="mx_DateSeparator" role="separator" tabIndex={-1} aria-label={label}>
             <hr role="none" />
-            <div aria-hidden="true">{ label }</div>
+            { dateHeaderContent }
             <hr role="none" />
         </h2>;
     }

res/css/_components.scss
@@ -183,6 +183,7 @@
 @import "./views/messages/_CallEvent.scss";
 @import "./views/messages/_CreateEvent.scss";
 @import "./views/messages/_DateSeparator.scss";
+@import "./views/messages/_JumpToDatePicker.scss";
 @import "./views/messages/_EventTileBubble.scss";
 @import "./views/messages/_HiddenBody.scss";
 @import "./views/messages/_MEmoteBody.scss";

res/css/views/messages/_DateSeparator.scss
@@ -33,3 +33,18 @@ limitations under the License.
     margin: 0 25px;
     flex: 0 0 auto;
 }
+
+.mx_DateSeparator_jumpToDateMenu {
+    display: flex;
+}
+
+.mx_DateSeparator_chevron {
+    align-self: center;
+    width: 16px;
+    height: 16px;
+    mask-position: center;
+    mask-size: contain;
+    mask-repeat: no-repeat;
+    mask-image: url('$(res)/img/feather-customised/chevron-down.svg');
+    background-color: $tertiary-content;
+}

src/components/structures/MessagePanel.tsx
@@ -721,8 +721,12 @@ export default class MessagePanel extends React.Component<IProps, IState> {
 
         // do we need a date separator since the last event?
         const wantsDateSeparator = this.wantsDateSeparator(prevEvent, eventDate);
-        if (wantsDateSeparator && !isGrouped) {
-            const dateSeparator = <li key={ts1}><DateSeparator key={ts1} ts={ts1} /></li>;
+        if (wantsDateSeparator && !isGrouped && this.props.room) {
+            const dateSeparator = (
+                <li key={ts1}>
+                    <DateSeparator key={ts1} roomId={this.props.room.roomId} ts={ts1} />
+                </li>
+            );
             ret.push(dateSeparator);
         }
 
@@ -1118,7 +1122,7 @@ class CreationGrouper extends BaseGrouper {
         if (panel.wantsDateSeparator(this.prevEvent, createEvent.getDate())) {
             const ts = createEvent.getTs();
             ret.push(
-                <li key={ts+'~'}><DateSeparator key={ts+'~'} ts={ts} /></li>,
+                <li key={ts+'~'}><DateSeparator key={ts+'~'} roomId={createEvent.getRoomId()} ts={ts} /></li>,
             );
         }
 
@@ -1231,7 +1235,7 @@ class RedactionGrouper extends BaseGrouper {
         if (panel.wantsDateSeparator(this.prevEvent, this.events[0].getDate())) {
             const ts = this.events[0].getTs();
             ret.push(
-                <li key={ts+'~'}><DateSeparator key={ts+'~'} ts={ts} /></li>,
+                <li key={ts+'~'}><DateSeparator key={ts+'~'} roomId={this.events[0].getRoomId()} ts={ts} /></li>,
             );
         }
 
@@ -1327,7 +1331,7 @@ class MemberGrouper extends BaseGrouper {
         if (panel.wantsDateSeparator(this.prevEvent, this.events[0].getDate())) {
             const ts = this.events[0].getTs();
             ret.push(
-                <li key={ts+'~'}><DateSeparator key={ts+'~'} ts={ts} /></li>,
+                <li key={ts+'~'}><DateSeparator key={ts+'~'} roomId={this.events[0].getRoomId()} ts={ts} /></li>,
             );
         }
 
@@ -1429,7 +1433,7 @@ class HiddenEventGrouper extends BaseGrouper {
         if (panel.wantsDateSeparator(this.prevEvent, this.events[0].getDate())) {
             const ts = this.events[0].getTs();
             ret.push(
-                <li key={ts+'~'}><DateSeparator key={ts+'~'} ts={ts} /></li>,
+                <li key={ts+'~'}><DateSeparator key={ts+'~'} roomId={this.events[0].getRoomId()} ts={ts} /></li>,
             );
         }
 

src/components/views/dialogs/MessageEditHistoryDialog.tsx
@@ -130,7 +130,7 @@ export default class MessageEditHistoryDialog extends React.PureComponent<IProps
         const baseEventId = this.props.mxEvent.getId();
         allEvents.forEach((e, i) => {
             if (!lastEvent || wantsDateSeparator(lastEvent.getDate(), e.getDate())) {
-                nodes.push(<li key={e.getTs() + "~"}><DateSeparator ts={e.getTs()} /></li>);
+                nodes.push(<li key={e.getTs() + "~"}><DateSeparator roomId={e.getRoomId()} ts={e.getTs()} /></li>);
             }
             const isBaseEvent = e.getId() === baseEventId;
             nodes.push((

src/components/views/elements/Field.tsx
@@ -14,12 +14,13 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import React, { InputHTMLAttributes, SelectHTMLAttributes, TextareaHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, SelectHTMLAttributes, TextareaHTMLAttributes, RefObject } from 'react';
 import classNames from 'classnames';
 import { debounce } from "lodash";
 
 import * as sdk from '../../../index';
 import { IFieldState, IValidationResult } from "./Validation";
+import { ComponentClass } from "../../../@types/common";
 
 // Invoke validation from user input (when typing, etc.) at most once every N ms.
 const VALIDATION_THROTTLE_MS = 200;
@@ -78,26 +79,45 @@ interface IProps {
 }
 
 export interface IInputProps extends IProps, InputHTMLAttributes<HTMLInputElement> {
+    // The ref pass through to the input
+    inputRef?: RefObject<HTMLInputElement>;
     // The element to create. Defaults to "input".
     element?: "input";
+    componentClass?: undefined;
     // The input's value. This is a controlled component, so the value is required.
     value: string;
 }
 
 interface ISelectProps extends IProps, SelectHTMLAttributes<HTMLSelectElement> {
+    // The ref pass through to the select
+    inputRef?: RefObject<HTMLSelectElement>;
     // To define options for a select, use <Field><option ... /></Field>
     element: "select";
+    componentClass?: undefined;
     // The select's value. This is a controlled component, so the value is required.
     value: string;
 }
 
 interface ITextareaProps extends IProps, TextareaHTMLAttributes<HTMLTextAreaElement> {
+    // The ref pass through to the textarea
+    inputRef?: RefObject<HTMLTextAreaElement>;
     element: "textarea";
+    componentClass?: undefined;
     // The textarea's value. This is a controlled component, so the value is required.
     value: string;
 }
 
-type PropShapes = IInputProps | ISelectProps | ITextareaProps;
+export interface INativeOnChangeInputProps extends IProps, InputHTMLAttributes<HTMLInputElement> {
+    // The ref pass through to the input
+    inputRef?: RefObject<HTMLInputElement>;
+    element: "input";
+    // The custom component to render
+    componentClass: ComponentClass;
+    // The input's value. This is a controlled component, so the value is required.
+    value: string;
+}
+
+type PropShapes = IInputProps | ISelectProps | ITextareaProps | INativeOnChangeInputProps;
 
 interface IState {
     valid: boolean;
@@ -108,7 +128,7 @@ interface IState {
 
 export default class Field extends React.PureComponent<PropShapes, IState> {
     private id: string;
-    private input: HTMLInputElement;
+    private inputRef: RefObject<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>;
 
     public static readonly defaultProps = {
         element: "input",
@@ -146,7 +166,7 @@ export default class Field extends React.PureComponent<PropShapes, IState> {
     }
 
     public focus() {
-        this.input.focus();
+        this.inputRef.current?.focus();
         // programmatic does not fire onFocus handler
         this.setState({
             focused: true,
@@ -197,7 +217,7 @@ export default class Field extends React.PureComponent<PropShapes, IState> {
         if (!this.props.onValidate) {
             return;
         }
-        const value = this.input ? this.input.value : null;
+        const value = this.inputRef.current?.value ?? null;
         const { valid, feedback } = await this.props.onValidate({
             value,
             focused,
@@ -228,13 +248,13 @@ export default class Field extends React.PureComponent<PropShapes, IState> {
 
     public render() {
         /* eslint @typescript-eslint/no-unused-vars: ["error", { "ignoreRestSiblings": true }] */
-        const { element, prefixComponent, postfixComponent, className, onValidate, children,
+        const { element, componentClass, inputRef, prefixComponent, postfixComponent, className, onValidate, children,
             tooltipContent, forceValidity, tooltipClassName, list, validateOnBlur, validateOnChange, validateOnFocus,
             usePlaceholderAsHint, forceTooltipVisible,
             ...inputProps } = this.props;
 
-        // Set some defaults for the <input> element
-        const ref = input => this.input = input;
+        this.inputRef = inputRef || React.createRef();
+
         inputProps.placeholder = inputProps.placeholder || inputProps.label;
         inputProps.id = this.id; // this overwrites the id from props
 
@@ -243,9 +263,9 @@ export default class Field extends React.PureComponent<PropShapes, IState> {
         inputProps.onBlur = this.onBlur;
 
         // Appease typescript's inference
-        const inputProps_ = { ...inputProps, ref, list };
+        const inputProps_ = { ...inputProps, ref: this.inputRef, list };
 
-        const fieldInput = React.createElement(this.props.element, inputProps_, children);
+        const fieldInput = React.createElement(this.props.componentClass || this.props.element, inputProps_, children);
 
         let prefixContainer = null;
         if (prefixComponent) {
@@ -257,17 +277,22 @@ export default class Field extends React.PureComponent<PropShapes, IState> {
         }
 
         const hasValidationFlag = forceValidity !== null && forceValidity !== undefined;
-        const fieldClasses = classNames("mx_Field", `mx_Field_${this.props.element}`, className, {
-            // If we have a prefix element, leave the label always at the top left and
-            // don't animate it, as it looks a bit clunky and would add complexity to do
-            // properly.
-            mx_Field_labelAlwaysTopLeft: prefixComponent || usePlaceholderAsHint,
-            mx_Field_placeholderIsHint: usePlaceholderAsHint,
-            mx_Field_valid: hasValidationFlag ? forceValidity : onValidate && this.state.valid === true,
-            mx_Field_invalid: hasValidationFlag
-                ? !forceValidity
-                : onValidate && this.state.valid === false,
-        });
+        const fieldClasses = classNames(
+            "mx_Field",
+            `mx_Field_${this.props.element}`,
+            className,
+            {
+                // If we have a prefix element, leave the label always at the top left and
+                // don't animate it, as it looks a bit clunky and would add complexity to do
+                // properly.
+                mx_Field_labelAlwaysTopLeft: prefixComponent || usePlaceholderAsHint,
+                mx_Field_placeholderIsHint: usePlaceholderAsHint,
+                mx_Field_valid: hasValidationFlag ? forceValidity : onValidate && this.state.valid === true,
+                mx_Field_invalid: hasValidationFlag
+                    ? !forceValidity
+                    : onValidate && this.state.valid === false,
+            },
+        );
 
         // Handle displaying feedback on validity
         // FIXME: Using an import will result in test failures

matrix-org/matrix-react-sdk

commit 4dc9ef940c555811fa2d0f680ae9114c0d68c466

Fix vertical spacing in compact context menus
Fix https://github.com/vector-im/element-web/issues/20801

Regressed in https://github.com/matrix-org/matrix-react-sdk/pull/7339

Relevant styles were first added in https://github.com/matrix-org/matrix-react-sdk/pull/4858
(context behind why the original styles were added)

res/css/views/context_menus/_IconizedContextMenu.scss
@@ -50,21 +50,21 @@ limitations under the License.
         }
 
         // round the top corners of the top button for the hover effect to be bounded
-        &:first-child .mx_AccessibleButton:not(.mx_AccessibleButton_hasKind):first-child {
+        &:first-child .mx_IconizedContextMenu_item:first-child {
             border-radius: 8px 8px 0 0; // radius matches .mx_ContextualMenu
         }
 
         // round the bottom corners of the bottom button for the hover effect to be bounded
-        &:last-child .mx_AccessibleButton:not(.mx_AccessibleButton_hasKind):last-child {
+        &:last-child .mx_IconizedContextMenu_item:last-child {
             border-radius: 0 0 8px 8px; // radius matches .mx_ContextualMenu
         }
 
         // round all corners of the only button for the hover effect to be bounded
-        &:first-child:last-child .mx_AccessibleButton:not(.mx_AccessibleButton_hasKind):first-child:last-child {
+        &:first-child:last-child .mx_IconizedContextMenu_item:first-child:last-child {
             border-radius: 8px; // radius matches .mx_ContextualMenu
         }
 
-        .mx_AccessibleButton:not(.mx_AccessibleButton_hasKind) {
+        .mx_IconizedContextMenu_item {
             // pad the inside of the button so that the hover background is padded too
             padding-top: 12px;
             padding-bottom: 12px;
@@ -130,7 +130,7 @@ limitations under the License.
     }
 
     .mx_IconizedContextMenu_optionList_red {
-        .mx_AccessibleButton:not(.mx_AccessibleButton_hasKind) {
+        .mx_IconizedContextMenu_item {
             color: $alert !important;
         }
 
@@ -148,7 +148,7 @@ limitations under the License.
     }
 
     .mx_IconizedContextMenu_active {
-        &.mx_AccessibleButton:not(.mx_AccessibleButton_hasKind), .mx_AccessibleButton:not(.mx_AccessibleButton_hasKind) {
+        &.mx_IconizedContextMenu_item, .mx_IconizedContextMenu_item {
             color: $accent !important;
         }
 

res/css/views/messages/_JumpToDatePicker.scss
@@ -16,6 +16,8 @@ limitations under the License.
 
 .mx_JumpToDatePicker_form {
     display: flex;
+    padding-top: 12px;
+    padding-bottom: 12px;
 }
 
 .mx_JumpToDatePicker_label {

src/components/views/context_menus/IconizedContextMenu.tsx
@@ -59,6 +59,7 @@ export const IconizedContextMenuRadio: React.FC<IRadioProps> = ({
     return <MenuItemRadio
         {...props}
         className={classNames(className, {
+            "mx_IconizedContextMenu_item": true,
             mx_IconizedContextMenu_active: active,
         })}
         active={active}
@@ -93,6 +94,7 @@ export const IconizedContextMenuCheckbox: React.FC<ICheckboxProps> = ({
     return <MenuItemCheckbox
         {...props}
         className={classNames(className, {
+            "mx_IconizedContextMenu_item": true,
             mx_IconizedContextMenu_active: active,
         })}
         active={active}
@@ -105,7 +107,7 @@ export const IconizedContextMenuCheckbox: React.FC<ICheckboxProps> = ({
 };
 
 export const IconizedContextMenuOption: React.FC<IOptionProps> = ({ label, iconClassName, children, ...props }) => {
-    return <MenuItem {...props} label={label}>
+    return <MenuItem {...props} className="mx_IconizedContextMenu_item" label={label}>
         { iconClassName && <span className={classNames("mx_IconizedContextMenu_icon", iconClassName)} /> }
         <span className="mx_IconizedContextMenu_label">{ label }</span>
         { children }

src/components/views/messages/DateSeparator.tsx
@@ -193,7 +193,6 @@ export default class DateSeparator extends React.Component<IProps, IState> {
         if (this.state.contextMenuPosition) {
             contextMenu = <IconizedContextMenu
                 {...contextMenuBelow(this.state.contextMenuPosition)}
-                compact
                 onFinished={this.onContextMenuCloseClick}
             >
                 <IconizedContextMenuOptionList first>