9072541b45a70c9141e9a3cf296ae1a56fb55f01
Michael Telatynski <7t3chguy@gmail.com>
2022-02-09 08:51:28 +0000

    Wire up Analytics for where user goes to change theme

--- src/components/views/spaces/QuickSettingsButton.tsx
+++ src/components/views/spaces/QuickSettingsButton.tsx
@@ -35,6 +35,7 @@ import SettingsStore from "../../../settings/SettingsStore";
 import { SettingLevel } from "../../../settings/SettingLevel";
 import dis from "../../../dispatcher/dispatcher";
 import { RecheckThemePayload } from "../../../dispatcher/payloads/RecheckThemePayload";
+import { PosthogAnalytics } from "../../../PosthogAnalytics";
 
 const QuickSettingsButton = ({ isPanelCollapsed = false }) => {
     const orderedThemes = useMemo(getOrderedThemes, []);
@@ -104,6 +105,8 @@ const QuickSettingsButton = ({ isPanelCollapsed = false }) => {
                 <Dropdown
                     id="mx_QuickSettingsButton_themePickerDropdown"
                     onOptionChange={async (newTheme: string) => {
+                        PosthogAnalytics.trackInteraction("WebQuickSettingsThemeDropdown");
+
                         // XXX: mostly copied from ThemeChoicePanel
                         // doing getValue in the .catch will still return the value we failed to set,
                         // so remember what the value was before we tried to set it so we can revert
