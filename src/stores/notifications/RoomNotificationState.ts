841cb09540af631cc03fa99714a5e23b91451483
David Baker <dave@matrix.org>
2022-02-15 19:50:10 +0000

    Fix delayed badge update for mentions in encrypted rooms
    Fixes https://github.com/vector-im/element-web/issues/20859
    
    More detail on the issue

--- src/stores/notifications/RoomNotificationState.ts
+++ src/stores/notifications/RoomNotificationState.ts
@@ -26,6 +26,7 @@ import * as RoomNotifs from '../../RoomNotifs';
 import * as Unread from '../../Unread';
 import { NotificationState } from "./NotificationState";
 import { getUnsentMessages } from "../../components/structures/RoomStatusBar";
+import { logger } from "matrix-js-sdk/src/logger";
 
 export class RoomNotificationState extends NotificationState implements IDestroyable {
     constructor(public readonly room: Room) {
@@ -35,7 +36,7 @@ export class RoomNotificationState extends NotificationState implements IDestroy
         this.room.on("Room.redaction", this.handleRoomEventUpdate);
         this.room.on("Room.myMembership", this.handleMembershipUpdate);
         this.room.on("Room.localEchoUpdated", this.handleLocalEchoUpdated);
-        MatrixClientPeg.get().on("Event.decrypted", this.handleRoomEventUpdate);
+        MatrixClientPeg.get().on("Event.decrypted", this.onEventDecrypted);
         MatrixClientPeg.get().on("accountData", this.handleAccountDataUpdate);
         this.updateNotificationState();
     }
@@ -52,7 +53,7 @@ export class RoomNotificationState extends NotificationState implements IDestroy
         this.room.removeListener("Room.myMembership", this.handleMembershipUpdate);
         this.room.removeListener("Room.localEchoUpdated", this.handleLocalEchoUpdated);
         if (MatrixClientPeg.get()) {
-            MatrixClientPeg.get().removeListener("Event.decrypted", this.handleRoomEventUpdate);
+            MatrixClientPeg.get().removeListener("Event.decrypted", this.onDecrypt);
             MatrixClientPeg.get().removeListener("accountData", this.handleAccountDataUpdate);
         }
     }
@@ -71,8 +72,15 @@ export class RoomNotificationState extends NotificationState implements IDestroy
         this.updateNotificationState();
     };
 
+    private onEventDecrypted = (event: MatrixEvent) => {
+        if (event.getRoomId() !== this.room.roomId) return; // ignore - not for us or notifications timeline
+
+        this.updateNotificationState();
+    };
+
     private handleRoomEventUpdate = (event: MatrixEvent, room: Room | null) => {
         if (room?.roomId !== this.room.roomId) return; // ignore - not for us or notifications timeline
+
         this.updateNotificationState();
     };
 
