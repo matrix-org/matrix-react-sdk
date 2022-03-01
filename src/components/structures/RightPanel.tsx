e493ef2bd0bc4882d2ff4bcbf265b7da5ae8ad94
Michael Telatynski <7t3chguy@gmail.com>
2022-03-01 11:06:34 +0000

    Fix bypass never allowing clearing the right panel phase

--- src/components/structures/RightPanel.tsx
+++ src/components/structures/RightPanel.tsx
@@ -100,7 +100,7 @@ export default class RightPanel extends React.Component<IProps, IState> {
             currentCard = RightPanelStore.instance.currentGroup;
         }
 
-        if (currentCard && !RightPanelStore.instance.isPhaseValid(currentCard.phase, !!props.room)) {
+        if (currentCard?.phase && !RightPanelStore.instance.isPhaseValid(currentCard.phase, !!props.room)) {
             // XXX: We can probably get rid of this workaround once GroupView is dead, it's unmounting happens weirdly
             // late causing the app to soft-crash due to lack of a room object being passed to a RightPanel
             return null; // skip this update, we're about to be unmounted and don't have the appropriate props
