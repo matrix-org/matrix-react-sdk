396b799a7fba20b1cb5c52972bb87a6d4a538e98
Kerry Archibald <kerrya@element.io>
2022-03-01 09:54:51 +0100

    use same matrix client import
    Signed-off-by: Kerry Archibald <kerrya@element.io>

--- src/components/views/location/shareLocation.ts
+++ src/components/views/location/shareLocation.ts
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { MatrixClient } from "matrix-js-sdk";
+import { MatrixClient } from "matrix-js-sdk/src/client";
 import { makeLocationContent } from "matrix-js-sdk/src/content-helpers";
 import { logger } from "matrix-js-sdk/src/logger";
 
