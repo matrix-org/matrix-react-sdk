37cce11dddf131e5bdc44fb507e71659eb5a48c2
Robin Townsend <robin@robin.town>
2022-03-04 10:28:31 -0500

    Test basic pinned message functionality
    Signed-off-by: Robin Townsend <robin@robin.town>

--- test/components/views/right_panel/PinnedMessagesCard-test.tsx
+++ test/components/views/right_panel/PinnedMessagesCard-test.tsx
@@ -19,6 +19,7 @@ import { mount } from "enzyme";
 import { act } from "react-dom/test-utils";
 import { MatrixEvent } from "matrix-js-sdk/src/models/event";
 import { EventType, RelationType, MsgType } from "matrix-js-sdk/src/@types/event";
+import { RoomStateEvent } from "matrix-js-sdk/src/models/room-state";
 
 import "../../../skinned-sdk";
 import {
@@ -39,19 +40,19 @@ describe("<PinnedMessagesCard />", () => {
     cli.relations = jest.fn().mockResolvedValue({ events: [] });
     const PinnedMessagesCard = wrapInMatrixClientContext(_PinnedMessagesCard);
 
-    const mkRoom = (localPins: MatrixEvent[], nonLocalPins: MatrixEvent[]) => {
-        const pins = [...localPins, ...nonLocalPins];
+    const mkRoom = (localPins: MatrixEvent[], nonLocalPins: MatrixEvent[]): Room => {
         const room = mkStubRoom("!room:example.org");
+        // Deferred since we may be adding or removing pins later
+        const pins = () => [...localPins, ...nonLocalPins];
 
         // Insert pin IDs into room state
-        const pinState = mkEvent({
+        room.currentState.getStateEvents.mockImplementation(() => mkEvent({
             event: true,
             type: EventType.RoomPinnedEvents,
             content: {
-                pinned: pins.map(e => e.getId()),
+                pinned: pins().map(e => e.getId()),
             },
-        });
-        room.currentState.getStateEvents.mockReturnValue(pinState);
+        }));
 
         // Insert local pins into local timeline set
         room.getUnfilteredTimelineSet = () => ({
@@ -61,10 +62,85 @@ describe("<PinnedMessagesCard />", () => {
         });
 
         // Return all pins over fetchRoomEvent
-        cli.fetchRoomEvent = (roomId, eventId) => pins.find(e => e.getId() === eventId)?.event;
+        cli.fetchRoomEvent = (roomId, eventId) => pins().find(e => e.getId() === eventId)?.event;
 
         return room;
     };
+
+    const mountPins = async (room: Room): ReactWrapper => {
+        let pins;
+        await act(async () => {
+            pins = mount(<PinnedMessagesCard room={room} onClose={() => {}} />);
+            // Wait a tick for state updates
+            await new Promise(resolve => setImmediate(resolve));
+        });
+        pins.update();
+
+        return pins;
+    };
+
+    const emitPinUpdates = async (pins: ReactWrapper) => {
+        const room = pins.props().room;
+        const pinListener = room.currentState.on.mock.calls
+            .find(([eventName, listener]) => eventName === RoomStateEvent.Events)[1];
+
+        await act(async () => {
+            // Emit the update
+            pinListener(room.currentState.getStateEvents());
+            // Wait a tick for state updates
+            await new Promise(resolve => setImmediate(resolve));
+        });
+        pins.update();
+    };
+
+    const pin1 = mkMessage({
+        event: true,
+        room: "!room:example.org",
+        user: "@alice:example.org",
+        msg: "First pinned message",
+    });
+    const pin2 = mkMessage({
+        event: true,
+        room: "!room:example.org",
+        user: "@alice:example.org",
+        msg: "The second one",
+    });
+
+    it("updates when messages are pinned", async () => {
+        // Start with nothing pinned
+        const localPins = [];
+        const nonLocalPins = [];
+        const pins = await mountPins(mkRoom(localPins, nonLocalPins));
+        expect(pins.find(PinnedEventTile).length).toBe(0);
+
+        // Pin the first message
+        localPins.push(pin1);
+        await emitPinUpdates(pins);
+        expect(pins.find(PinnedEventTile).length).toBe(1);
+
+        // Pin the second message
+        nonLocalPins.push(pin2);
+        await emitPinUpdates(pins);
+        expect(pins.find(PinnedEventTile).length).toBe(2);
+    });
+
+    it("updates when messages are unpinned", async () => {
+        // Start with two pins
+        const localPins = [pin1];
+        const nonLocalPins = [pin2];
+        const pins = await mountPins(mkRoom(localPins, nonLocalPins));
+        expect(pins.find(PinnedEventTile).length).toBe(2);
+
+        // Unpin the first message
+        localPins.pop();
+        await emitPinUpdates(pins);
+        expect(pins.find(PinnedEventTile).length).toBe(1);
+
+        // Unpin the second message
+        nonLocalPins.pop();
+        await emitPinUpdates(pins);
+        expect(pins.find(PinnedEventTile).length).toBe(0);
+    });
 
     it("hides unpinnable events found in local timeline", async () => {
         // Redacted messages are unpinnable
@@ -75,13 +151,7 @@ describe("<PinnedMessagesCard />", () => {
             unsigned: { redacted_because: {} },
         });
 
-        let pins;
-        await act(async () => {
-            pins = mount(<PinnedMessagesCard room={mkRoom([pin], [])} onClose={() => {}} />);
-            // Wait a tick for state updates
-            await new Promise(resolve => setImmediate(resolve));
-        });
-        pins.update();
+        const pins = await mountPins(mkRoom([pin], []));
         expect(pins.find(PinnedEventTile).length).toBe(0);
     });
 
@@ -94,23 +164,11 @@ describe("<PinnedMessagesCard />", () => {
             unsigned: { redacted_because: {} },
         });
 
-        let pins;
-        await act(async () => {
-            pins = mount(<PinnedMessagesCard room={mkRoom([], [pin])} onClose={() => {}} />);
-            // Wait a tick for state updates
-            await new Promise(resolve => setImmediate(resolve));
-        });
-        pins.update();
+        const pins = await mountPins(mkRoom([], [pin]));
         expect(pins.find(PinnedEventTile).length).toBe(0);
     });
 
     it("accounts for edits", async () => {
-        const pin = mkMessage({
-            event: true,
-            room: "!room:example.org",
-            user: "@alice:example.org",
-            msg: "Hello!",
-        });
         cli.relations.mockResolvedValue({
             events: [mkEvent({
                 event: true,
@@ -119,29 +177,22 @@ describe("<PinnedMessagesCard />", () => {
                 user: "@alice:example.org",
                 content: {
                     "msgtype": MsgType.Text,
-                    "body": " * Hello again!",
+                    "body": " * First pinned message, edited",
                     "m.new_content": {
                         msgtype: MsgType.Text,
-                        body: "Hello again!",
+                        body: "First pinned message, edited",
                     },
                     "m.relates_to": {
                         rel_type: RelationType.Replace,
-                        event_id: pin.getId(),
+                        event_id: pin1.getId(),
                     },
                 },
             })],
         });
 
-        let pins;
-        await act(async () => {
-            pins = mount(<PinnedMessagesCard room={mkRoom([], [pin])} onClose={() => {}} />);
-            // Wait a tick for state updates
-            await new Promise(resolve => setImmediate(resolve));
-        });
-        pins.update();
-
+        const pins = await mountPins(mkRoom([], [pin1]));
         const pinTile = pins.find(PinnedEventTile);
         expect(pinTile.length).toBe(1);
-        expect(pinTile.find(".mx_EventTile_body").text()).toEqual("Hello again!");
+        expect(pinTile.find(".mx_EventTile_body").text()).toEqual("First pinned message, edited");
     });
 });
