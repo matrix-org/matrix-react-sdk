da6d6d101c81d7190b0d6b740d29f34a841bc46f
CicadaCinema <52425971+CicadaCinema@users.noreply.github.com>
2022-03-10 14:36:37 +0000

    copyright should be myself

--- src/effects/hearts/index.ts
+++ src/effects/hearts/index.ts
@@ -1,5 +1,6 @@
 /*
  Copyright 2021 The Matrix.org Foundation C.I.C.
+ Copyright 2022 Arseny Uskov
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
