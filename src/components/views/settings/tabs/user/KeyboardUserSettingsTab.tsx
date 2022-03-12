1f648dc98c494bd239876cfe2452b3c25cf20928
Šimon Brandner <simon.bra.ag@gmail.com>
2022-03-12 15:05:54 +0100

    Rest
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/components/views/settings/tabs/user/KeyboardUserSettingsTab.tsx
+++ src/components/views/settings/tabs/user/KeyboardUserSettingsTab.tsx
@@ -25,41 +25,80 @@ import {
 import SdkConfig from "../../../../../SdkConfig";
 import { _t } from "../../../../../languageHandler";
 import {
-    getKeyboardShortcutDisplayName, getKeyboardShortcutValue,
+    getKeyboardShortcutDisplayName,
+    getKeyboardShortcutHideEditUI,
+    getKeyboardShortcutValue,
 } from "../../../../../accessibility/KeyboardShortcutUtils";
 import { KeyboardShortcut } from "../../KeyboardShortcut";
+import AccessibleButton from "../../../elements/AccessibleButton";
+import SettingsStore from "../../../../../settings/SettingsStore";
+import { SettingLevel } from "../../../../../settings/SettingLevel";
+import Modal from "../../../../../Modal";
+import {
+    ChangeKeyboardShortcutDialog,
+    IProps as IChangeKeyboardShortcutDialogProps,
+} from "./ChangeKeyboardShortcutDialog";
 
 interface IKeyboardShortcutRowProps {
     name: string;
+    allowCustomization: boolean;
 }
 
 // Filter out the labs section if labs aren't enabled.
 const visibleCategories = Object.entries(CATEGORIES).filter(([categoryName]) =>
     categoryName !== CategoryName.LABS || SdkConfig.get("show_labs_settings"));
 
-const KeyboardShortcutRow: React.FC<IKeyboardShortcutRowProps> = ({ name }) => {
+const KeyboardShortcutRow: React.FC<IKeyboardShortcutRowProps> = ({ name, allowCustomization }) => {
     const displayName = getKeyboardShortcutDisplayName(name);
+    const hideEditUI = getKeyboardShortcutHideEditUI(name);
     const value = getKeyboardShortcutValue(name);
     if (!displayName || !value) return null;
 
+    const onEditClick = async (): Promise<void> => {
+        const { finished } = Modal.createDialog(ChangeKeyboardShortcutDialog, {
+            value,
+        } as IChangeKeyboardShortcutDialogProps);
+        const [newValue] = await finished;
+        if (!newValue) return;
+
+        //SettingsStore.setValue(name, null, SettingLevel.DEVICE, newValue);
+    };
+
+    const onResetClick = (): void => {
+        SettingsStore.setValue(name, null, SettingLevel.DEVICE, SettingsStore.getDefaultValue(name));
+    };
+
     return <div className="mx_KeyboardShortcut_shortcutRow">
-        { displayName }
+        <div className="mx_KeyboardShortcut_shortcutRow_displayName">
+            { displayName }
+        </div>
         <KeyboardShortcut value={value} />
+        { allowCustomization && <React.Fragment>
+            <AccessibleButton kind="primary_outline" disabled={hideEditUI} onClick={onEditClick}> { _t("Edit") } </AccessibleButton>
+            <AccessibleButton kind="primary_outline" disabled={hideEditUI} onClick={onResetClick}> { _t("Reset") } </AccessibleButton>
+        </React.Fragment> }
     </div>;
 };
 
 interface IKeyboardShortcutSectionProps {
     categoryName: CategoryName;
     category: ICategory;
+    allowCustomization: boolean;
 }
 
-const KeyboardShortcutSection: React.FC<IKeyboardShortcutSectionProps> = ({ categoryName, category }) => {
+const KeyboardShortcutSection: React.FC<IKeyboardShortcutSectionProps> = (
+    { categoryName, category, allowCustomization },
+) => {
     if (!category.categoryLabel) return null;
 
     return <div className="mx_SettingsTab_section" key={categoryName}>
         <div className="mx_SettingsTab_subheading">{ _t(category.categoryLabel) }</div>
         <div> { category.settingNames.map((shortcutName) => {
-            return <KeyboardShortcutRow key={shortcutName} name={shortcutName} />;
+            return <KeyboardShortcutRow
+                key={shortcutName}
+                name={shortcutName}
+                allowCustomization={allowCustomization}
+            />;
         }) } </div>
     </div>;
 };
@@ -68,7 +107,12 @@ const KeyboardUserSettingsTab: React.FC = () => {
     return <div className="mx_SettingsTab mx_KeyboardUserSettingsTab">
         <div className="mx_SettingsTab_heading">{ _t("Keyboard") }</div>
         { visibleCategories.map(([categoryName, category]: [CategoryName, ICategory]) => {
-            return <KeyboardShortcutSection key={categoryName} categoryName={categoryName} category={category} />;
+            return <KeyboardShortcutSection
+                key={categoryName}
+                categoryName={categoryName}
+                category={category}
+                allowCustomization={SettingsStore.getValue("feature_customizable_keybindings")}
+            />;
         }) }
     </div>;
 };
