3a6530d46eead4cf5f24d0cf7bda67de3fe15d8c
Kerry Archibald <kerrya@element.io>
2022-03-16 16:54:20 +0100

    test live share warning
    Signed-off-by: Kerry Archibald <kerrya@element.io>

--- test/components/views/beacon/LeftPanelLiveShareWarning-test.tsx
+++ test/components/views/beacon/LeftPanelLiveShareWarning-test.tsx
@@ -15,23 +15,66 @@ limitations under the License.
 */
 
 import React from 'react';
+import { mocked } from 'jest-mock';
 import { mount } from 'enzyme';
 
 import '../../../skinned-sdk';
 import LeftPanelLiveShareWarning from '../../../../src/components/views/beacon/LeftPanelLiveShareWarning';
+import { OwnBeaconStore, OwnBeaconStoreEvent } from '../../../../src/stores/OwnBeaconStore';
+import { flushPromises } from '../../../test-utils';
+
+jest.mock('../../../../src/stores/OwnBeaconStore', () => {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const EventEmitter = require("events");
+    class MockOwnBeaconStore extends EventEmitter {
+        public hasLiveBeacons = jest.fn().mockReturnValue(false);
+    }
+    return {
+        // @ts-ignore
+        ...jest.requireActual('../../../../src/stores/OwnBeaconStore'),
+        OwnBeaconStore: {
+            instance: new MockOwnBeaconStore() as unknown as OwnBeaconStore,
+        },
+    };
+},
+);
 
 describe('<LeftPanelLiveShareWarning />', () => {
     const defaultProps = {};
     const getComponent = (props = {}) =>
         mount(<LeftPanelLiveShareWarning {...defaultProps} {...props} />);
 
-    it('renders correctly when not minimized', () => {
+    it('renders nothing when user has no live beacons', () => {
         const component = getComponent();
-        expect(component).toMatchSnapshot();
+        expect(component.html()).toBe(null);
     });
 
-    it('renders correctly when minimized', () => {
-        const component = getComponent({ isMinimized: true });
-        expect(component).toMatchSnapshot();
+    describe('when user has live beacons', () => {
+        beforeEach(() => {
+            mocked(OwnBeaconStore.instance).hasLiveBeacons.mockReturnValue(true);
+        });
+        it('renders correctly when not minimized', () => {
+            const component = getComponent();
+            expect(component).toMatchSnapshot();
+        });
+
+        it('renders correctly when minimized', () => {
+            const component = getComponent({ isMinimized: true });
+            expect(component).toMatchSnapshot();
+        });
+
+        it('removes itself when user stops having live beacons', async () => {
+            const component = getComponent({ isMinimized: true });
+            // started out rendered
+            expect(component.html()).toBeTruthy();
+
+            mocked(OwnBeaconStore.instance).hasLiveBeacons.mockReturnValue(false);
+            OwnBeaconStore.instance.emit(OwnBeaconStoreEvent.LivenessChange, false);
+
+            await flushPromises();
+            component.setProps({});
+
+            expect(component.html()).toBe(null);
+        });
     });
 });
