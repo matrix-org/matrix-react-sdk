c6a5c8d7f2aaa0fd3a6634d576ccd22751997a01
Suguru Hirahara <luixxiul@users.noreply.github.com>
2022-03-24 16:25:32 +0000

    Responsive BetaCard on Labs
    Signed-off-by: Suguru Hirahara <luixxiul@users.noreply.github.com>

--- src/components/views/beta/BetaCard.tsx
+++ src/components/views/beta/BetaCard.tsx
@@ -101,9 +101,9 @@ const BetaCard = ({ title: titleOverride, featureId }: IProps) => {
 
     return <div className="mx_BetaCard">
         <div className="mx_BetaCard_columns">
-            <div>
+            <div className="mx_BetaCard_columns_description">
                 <h3 className="mx_BetaCard_title">
-                    { titleOverride || _t(title) }
+                    <span>{ titleOverride || _t(title) }</span>
                     <BetaPill />
                 </h3>
                 <span className="mx_BetaCard_caption">{ caption() }</span>
@@ -132,7 +132,7 @@ const BetaCard = ({ title: titleOverride, featureId }: IProps) => {
                     { disclaimer(value) }
                 </div> }
             </div>
-            <img src={image} alt="" />
+            <img className="mx_BetaCard_columns_image" src={image} alt="" />
         </div>
         { extraSettings && value && <div className="mx_BetaCard_relatedSettings">
             { extraSettings.map(key => (
