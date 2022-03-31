3f4e848d42507d3f56c59e625ed341c79a3d4b35
Kerry Archibald <kerrya@element.io>
2022-03-31 16:07:11 +0200

    fix ts issues
    Signed-off-by: Kerry Archibald <kerrya@element.io>

--- test/notifications/PushRuleVectorState-test.ts
+++ test/notifications/PushRuleVectorState-test.ts
@@ -1,5 +1,6 @@
 /*
 Copyright 2016 OpenMarket Ltd
+Copyright 2022 The Matrix.org Foundation C.I.C.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,47 +15,61 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-const notifications = require('../../src/notifications');
+import {
+    PushRuleActionName,
+    TweakHighlight,
+    TweakName,
+    TweakSound,
+} from "matrix-js-sdk/src/matrix";
 
-const prvs = notifications.PushRuleVectorState;
+import { PushRuleVectorState } from "../../src/notifications";
 
 describe("PushRuleVectorState", function() {
     describe("contentRuleVectorStateKind", function() {
         it("should understand normal notifications", function() {
             const rule = {
                 actions: [
-                    "notify",
+                    PushRuleActionName.Notify,
                 ],
+                default: false,
+                enabled: false,
+                rule_id: '1',
             };
 
-            expect(prvs.contentRuleVectorStateKind(rule)).
-                toEqual(prvs.ON);
+            expect(PushRuleVectorState.contentRuleVectorStateKind(rule)).
+                toEqual(PushRuleVectorState.ON);
         });
 
         it("should handle loud notifications", function() {
             const rule = {
                 actions: [
-                    "notify",
-                    { set_tweak: "highlight", value: true },
-                    { set_tweak: "sound", value: "default" },
+                    PushRuleActionName.Notify,
+                    { set_tweak: TweakName.Highlight, value: true } as TweakHighlight,
+                    { set_tweak: TweakName.Sound, value: "default" } as TweakSound,
                 ],
+                default: false,
+                enabled: false,
+                rule_id: '1',
             };
 
-            expect(prvs.contentRuleVectorStateKind(rule)).
-                toEqual(prvs.LOUD);
+            expect(PushRuleVectorState.contentRuleVectorStateKind(rule)).
+                toEqual(PushRuleVectorState.LOUD);
         });
 
         it("should understand missing highlight.value", function() {
             const rule = {
                 actions: [
-                    "notify",
-                    { set_tweak: "highlight" },
-                    { set_tweak: "sound", value: "default" },
+                    PushRuleActionName.Notify,
+                    { set_tweak: TweakName.Highlight } as TweakHighlight,
+                    { set_tweak: TweakName.Sound, value: "default" } as TweakSound,
                 ],
+                default: false,
+                enabled: false,
+                rule_id: '1',
             };
 
-            expect(prvs.contentRuleVectorStateKind(rule)).
-                toEqual(prvs.LOUD);
+            expect(PushRuleVectorState.contentRuleVectorStateKind(rule)).
+                toEqual(PushRuleVectorState.LOUD);
         });
     });
 });
