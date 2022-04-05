3a01fe83f0345d1486553add85ee70e9308cc514
Kerry Archibald <kerrya@element.io>
2022-04-05 16:47:56 +0200

    wrap marker in smartmarker
    Signed-off-by: Kerry Archibald <kerrya@element.io>

--- src/components/views/messages/MLocationBody.tsx
+++ src/components/views/messages/MLocationBody.tsx
@@ -30,7 +30,7 @@ import Modal from '../../../Modal';
 import {
     parseGeoUri,
     locationEventGeoUri,
-    createMap,
+    createMapWithCoords,
     getLocationShareErrorMessage,
     LocationShareError,
 } from '../../../utils/location';
@@ -75,7 +75,7 @@ export default class MLocationBody extends React.Component<IBodyProps, IState> {
 
         this.context.on(ClientEvent.ClientWellKnown, this.updateStyleUrl);
 
-        this.map = createMap(
+        this.map = createMapWithCoords(
             this.coords,
             false,
             this.bodyId,
