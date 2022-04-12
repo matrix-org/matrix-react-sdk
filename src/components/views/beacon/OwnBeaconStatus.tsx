87c329ff1477001fb2da3f343c44505217c7e046
Kerry Archibald <kerrya@element.io>
2022-04-12 16:14:32 +0200

    improve button styling
    Signed-off-by: Kerry Archibald <kerrya@element.io>

--- src/components/views/beacon/OwnBeaconStatus.tsx
+++ src/components/views/beacon/OwnBeaconStatus.tsx
@@ -56,33 +56,33 @@ const OwnBeaconStatus: React.FC<Props & HTMLProps<HTMLDivElement>> = ({
         displayLiveTimeRemaining
         {...rest}
     >
-        {ownDisplayStatus === BeaconDisplayStatus.Active && <AccessibleButton
+        { ownDisplayStatus === BeaconDisplayStatus.Active && <AccessibleButton
             data-test-id='beacon-status-stop-beacon'
             kind='link'
             onClick={onStopSharing}
-            className='mx_BeaconStatus_destructiveButton'
+            className='mx_OwnBeaconStatus_button mx_OwnBeaconStatus_destructiveButton'
             disabled={stoppingInProgress}
         >
-            {_t('Stop')}
+            { _t('Stop') }
         </AccessibleButton>
         }
-        {hasWireError && <AccessibleButton
+        { hasWireError && <AccessibleButton
             data-test-id='beacon-status-reset-wire-error'
             kind='link'
             onClick={onResetWireError}
-            className='mx_BeaconStatus_destructiveButton'
+            className='mx_OwnBeaconStatus_button mx_OwnBeaconStatus_destructiveButton'
         >
-            {_t('Retry')}
+            { _t('Retry') }
         </AccessibleButton>
         }
-        {hasStopSharingError && <AccessibleButton
+        { hasStopSharingError && <AccessibleButton
             data-test-id='beacon-status-stop-beacon-retry'
             kind='link'
             onClick={onStopSharing}
-            className='mx_BeaconStatus_destructiveButton'
+            className='mx_OwnBeaconStatus_button mx_OwnBeaconStatus_destructiveButton'
         >
-            {_t('Retry')}
-        </AccessibleButton>}
+            { _t('Retry') }
+        </AccessibleButton> }
     </BeaconStatus>;
 };
 
