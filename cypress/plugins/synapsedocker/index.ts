6c0acdb7d5231c61b87c6b7277ba640314d3b492
David Baker <dave@matrix.org>
2022-04-12 18:49:20 +0100

    sigh

--- cypress/plugins/synapsedocker/index.ts
+++ cypress/plugins/synapsedocker/index.ts
@@ -37,15 +37,18 @@ async function cfgDirFromTemplate(template: string): Promise<SynapseConfig> {
     const tempstats = await fse.stat(tempDir);
     console.log("Stats for temp dir: ", tempstats)
 
-    await new Promise<void>((resolve, reject) => {
+    /*await new Promise<void>((resolve, reject) => {
         childProcess.execFile('ls -ld', [ tempDir ], (err, stdout) => {
             if (err) reject(err);
             console.log(stdout);
             resolve();
         });
-    });
+    });*/
 
     await fse.chmod(tempDir, 0o777);
+
+    const tempstats2 = await fse.stat(tempDir);
+    console.log("New stats for temp dir: ", tempstats2)
 
     await new Promise<void>((resolve, reject) => {
         childProcess.execFile('ls -ld', [ tempDir ], (err, stdout) => {
