c4380c911c208a439e2c5ce3e99a67eb3fa8a439
Hugh Nimmo-Smith <hughns@element.io>
2022-04-13 13:04:58 +0100

    Remove trailing whitespace from translations

--- src/components/structures/auth/ForgotPassword.tsx
+++ src/components/structures/auth/ForgotPassword.tsx
@@ -202,13 +202,14 @@ export default class ForgotPassword extends React.Component<IProps, IState> {
                             _t(
                                 "Resetting your password on this homeserver will cause all of your devices to be " +
                                 "signed out. This will delete the message encryption keys stored on them, " +
-                                "making encrypted chat history unreadable. ",
+                                "making encrypted chat history unreadable.",
                             ) :
                             _t(
                                 "Signing out your devices will delete the message encryption keys stored on them, " +
-                                "making encrypted chat history unreadable. ",
+                                "making encrypted chat history unreadable.",
                             )
                         }
+                        <span> </span>
                         { _t("If you want to access your encrypted chat history then set up Key Backup or export " +
                             "your message keys from one of your other devices before proceeding.",
                         ) }
