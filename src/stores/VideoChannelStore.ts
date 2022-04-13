56ea403ecad0483e50d4634261c840e2a452ce8e
Robin Townsend <robin@robin.town>
2022-04-12 20:07:23 -0400

    Make VideoChannelStore constructor private

--- src/stores/VideoChannelStore.ts
+++ src/stores/VideoChannelStore.ts
@@ -57,7 +57,7 @@ export default class VideoChannelStore extends AsyncStoreWithClient<null> {
         return VideoChannelStore._instance;
     }
 
-    constructor() {
+    private constructor() {
         super(defaultDispatcher);
     }
 
