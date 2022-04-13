a50e011c40a80fea5fa1b1356e1e8802cfa8b50a
Eric Eastwood <erice@element.io>
2022-04-13 02:47:53 -0500

    Fix type lints

--- src/indexing/EventIndex.ts
+++ src/indexing/EventIndex.ts
@@ -808,7 +808,9 @@ export default class EventIndex extends EventEmitter {
         // Add the events to the timeline of the file panel.
         matrixEvents.forEach(e => {
             if (!timelineSet.eventIdToTimeline(e.getId())) {
-                timelineSet.addEventToTimeline(e, timeline, direction == EventTimeline.BACKWARDS);
+                timelineSet.addEventToTimeline(e, timeline, {
+                    toStartOfTimeline: direction == EventTimeline.BACKWARDS,
+                });
             }
         });
 
