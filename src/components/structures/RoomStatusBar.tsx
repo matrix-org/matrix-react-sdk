d9001ce19fb4351651a38f5fc06098968a175c5d
Eric Eastwood <erice@element.io>
2022-04-18 23:28:49 -0500

    Remove unreadable image descriptions
    See https://github.com/matrix-org/matrix-react-sdk/pull/8303#discussion_r850188154

--- src/components/structures/RoomStatusBar.tsx
+++ src/components/structures/RoomStatusBar.tsx
@@ -318,8 +318,7 @@ export default class RoomStatusBar extends React.PureComponent<IProps, IState> {
                                 src={require("../../../res/img/feather-customised/warning-triangle.svg").default}
                                 width="24"
                                 height="24"
-                                title="/!\ "
-                                alt="/!\ " />
+                                alt="" />
                             <div>
                                 <div className="mx_RoomStatusBar_connectionLostBar_title">
                                     { _t('Connectivity to the server has been lost.') }
@@ -347,8 +346,7 @@ export default class RoomStatusBar extends React.PureComponent<IProps, IState> {
                                 src={require("../../../res/img/feather-customised/warning-triangle.svg").default}
                                 width="24"
                                 height="24"
-                                title="/!\ "
-                                alt="/!\ " />
+                                alt="" />
                         </div>
                         <div>
                             <div className="mx_RoomStatusBar_unsentTitle">
