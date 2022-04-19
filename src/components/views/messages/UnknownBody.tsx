b283649ddc4fc7af789cccdb6b8ec412238e00bf
Suguru Hirahara <luixxiul@users.noreply.github.com>
2022-04-19 20:27:32 +0000

    Replace span with div
    Signed-off-by: Suguru Hirahara <luixxiul@users.noreply.github.com>

--- src/components/views/messages/UnknownBody.tsx
+++ src/components/views/messages/UnknownBody.tsx
@@ -26,9 +26,9 @@ interface IProps {
 export default forwardRef(({ mxEvent, children }: IProps, ref: React.RefObject<HTMLSpanElement>) => {
     const text = mxEvent.getContent().body;
     return (
-        <span className="mx_UnknownBody" ref={ref}>
+        <div className="mx_UnknownBody" ref={ref}>
             { text }
             { children }
-        </span>
+        </div>
     );
 });
