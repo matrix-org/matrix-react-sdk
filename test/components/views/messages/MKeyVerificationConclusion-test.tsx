6da9c770575a941bdcf2c9cfc50b069e89b23d14
Kerry Archibald <kerrya@element.io>
2022-04-28 11:50:08 +0200

    ts fixes
    Signed-off-by: Kerry Archibald <kerrya@element.io>

--- test/components/views/messages/MKeyVerificationConclusion-test.tsx
+++ test/components/views/messages/MKeyVerificationConclusion-test.tsx
@@ -1,25 +1,50 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import { EventEmitter } from 'events';
-import { MatrixEvent } from 'matrix-js-sdk/src/matrix';
+import { MatrixEvent, EventType } from 'matrix-js-sdk/src/matrix';
+import { CryptoEvent } from 'matrix-js-sdk/src/crypto';
+import { UserTrustLevel } from 'matrix-js-sdk/src/crypto/CrossSigning';
+import { VerificationRequest } from 'matrix-js-sdk/src/crypto/verification/request/VerificationRequest';
 
-import * as TestUtils from '../../../test-utils';
 import { MatrixClientPeg } from '../../../../src/MatrixClientPeg';
 import MKeyVerificationConclusion from '../../../../src/components/views/messages/MKeyVerificationConclusion';
+import { getMockClientWithEventEmitter } from '../../../test-utils';
 
-const trustworthy = () => ({ isCrossSigningVerified: () => true });
-const untrustworthy = () => ({ isCrossSigningVerified: () => false });
+const trustworthy = ({ isCrossSigningVerified: () => true }) as unknown as UserTrustLevel;
+const untrustworthy = ({ isCrossSigningVerified: () => false }) as unknown as UserTrustLevel;
 
 describe("MKeyVerificationConclusion", () => {
-    beforeEach(() => {
-        TestUtils.stubClient();
-        const client = MatrixClientPeg.get();
-        client.checkUserTrust = trustworthy;
+    const userId = '@user:server';
+    const mockClient = getMockClientWithEventEmitter({
+        getRoom: jest.fn(),
+        getUserId: jest.fn().mockReturnValue(userId),
+        checkUserTrust: jest.fn(),
+    });
 
-        const emitter = new EventEmitter();
-        client.on = emitter.on.bind(emitter);
-        client.removeListener = emitter.removeListener.bind(emitter);
-        client.emit = emitter.emit.bind(emitter);
+    const getMockVerificationRequest = (
+        { pending, cancelled, done, otherUserId }:
+        { pending?: boolean, cancelled?: boolean, done?: boolean, otherUserId?: string },
+    ) => {
+        class MockVerificationRequest extends EventEmitter {
+            constructor(
+                public readonly pending: boolean,
+                public readonly cancelled: boolean,
+                public readonly done: boolean,
+                public readonly otherUserId: string,
+            ) {
+                super();
+            }
+        }
+        return new MockVerificationRequest(pending, cancelled, done, otherUserId) as unknown as VerificationRequest;
+    };
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        mockClient.checkUserTrust.mockReturnValue(trustworthy);
+    });
+
+    afterAll(() => {
+        jest.spyOn(MatrixClientPeg, 'get').mockRestore();
     });
 
     it("shouldn't render if there's no verificationRequest", () => {
@@ -32,8 +57,7 @@ describe("MKeyVerificationConclusion", () => {
 
     it("shouldn't render if the verificationRequest is pending", () => {
         const event = new MatrixEvent({});
-        event.verificationRequest = new EventEmitter();
-        event.verificationRequest.pending = true;
+        event.verificationRequest = getMockVerificationRequest({ pending: true });
         const renderer = TestRenderer.create(
             <MKeyVerificationConclusion mxEvent={event} />,
         );
@@ -41,9 +65,8 @@ describe("MKeyVerificationConclusion", () => {
     });
 
     it("shouldn't render if the event type is cancel but the request type isn't", () => {
-        const event = new MatrixEvent({ type: "m.key.verification.cancel" });
-        event.verificationRequest = new EventEmitter();
-        event.verificationRequest.cancelled = false;
+        const event = new MatrixEvent({ type: EventType.KeyVerificationCancel });
+        event.verificationRequest = getMockVerificationRequest({ cancelled: false });
         const renderer = TestRenderer.create(
             <MKeyVerificationConclusion mxEvent={event} />,
         );
@@ -52,8 +75,7 @@ describe("MKeyVerificationConclusion", () => {
 
     it("shouldn't render if the event type is done but the request type isn't", () => {
         const event = new MatrixEvent({ type: "m.key.verification.done" });
-        event.verificationRequest = new EventEmitter();
-        event.verificationRequest.done = false;
+        event.verificationRequest = getMockVerificationRequest({ done: false });
         const renderer = TestRenderer.create(
             <MKeyVerificationConclusion mxEvent={event} />,
         );
@@ -61,12 +83,10 @@ describe("MKeyVerificationConclusion", () => {
     });
 
     it("shouldn't render if the user isn't actually trusted", () => {
-        const client = MatrixClientPeg.get();
-        client.checkUserTrust = untrustworthy;
+        mockClient.checkUserTrust.mockReturnValue(untrustworthy);
 
         const event = new MatrixEvent({ type: "m.key.verification.done" });
-        event.verificationRequest = new EventEmitter();
-        event.verificationRequest.done = true;
+        event.verificationRequest = getMockVerificationRequest({ done: true });
         const renderer = TestRenderer.create(
             <MKeyVerificationConclusion mxEvent={event} />,
         );
@@ -74,26 +94,23 @@ describe("MKeyVerificationConclusion", () => {
     });
 
     it("should rerender appropriately if user trust status changes", () => {
-        const client = MatrixClientPeg.get();
-        client.checkUserTrust = untrustworthy;
+        mockClient.checkUserTrust.mockReturnValue(untrustworthy);
 
         const event = new MatrixEvent({ type: "m.key.verification.done" });
-        event.verificationRequest = new EventEmitter();
-        event.verificationRequest.done = true;
-        event.verificationRequest.otherUserId = "@someuser:domain";
+        event.verificationRequest = getMockVerificationRequest({ done: true, otherUserId: "@someuser:domain" });
         const renderer = TestRenderer.create(
             <MKeyVerificationConclusion mxEvent={event} />,
         );
         expect(renderer.toJSON()).toBeNull();
 
-        client.checkUserTrust = trustworthy;
+        mockClient.checkUserTrust.mockReturnValue(trustworthy);
 
         /* Ensure we don't rerender for every trust status change of any user */
-        client.emit("userTrustStatusChanged", "@anotheruser:domain");
+        mockClient.emit(CryptoEvent.UserTrustStatusChanged, "@anotheruser:domain");
         expect(renderer.toJSON()).toBeNull();
 
         /* But when our user changes, we do rerender */
-        client.emit("userTrustStatusChanged", event.verificationRequest.otherUserId);
+        mockClient.emit(CryptoEvent.UserTrustStatusChanged, event.verificationRequest.otherUserId);
         expect(renderer.toJSON()).not.toBeNull();
     });
 });
