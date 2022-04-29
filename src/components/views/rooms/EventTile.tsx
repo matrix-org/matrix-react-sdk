7c06cbe664178fea935638018e12509b1257dc71
Suguru Hirahara <luixxiul@users.noreply.github.com>
2022-04-29 10:35:18 +0000

    Update src/components/views/rooms/EventTile.tsx
    Based on the suggestion.
    
    Co-authored-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/views/rooms/EventTile.tsx
+++ src/components/views/rooms/EventTile.tsx
@@ -1033,7 +1033,7 @@ export class UnwrappedEventTile extends React.Component<IProps, IState> {
             avatarSize = 24;
             needsSenderProfile = true;
         } else if (this.context.timelineRenderingType === TimelineRenderingType.ThreadsList ||
-                   this.context.timelineRenderingType === TimelineRenderingType.Thread && !this.props.continuation
+            (this.context.timelineRenderingType === TimelineRenderingType.Thread && !this.props.continuation)
         ) {
             avatarSize = 32;
             needsSenderProfile = true;
