b9f56e14c2b6a95fb869c5ecc1ca071cd3a519d6
Sinharitik589 <sinharitik18112835@gmail.com>
2022-05-16 10:22:46 +0530

    Prevent invites from showing up in breadcrumbs

--- src/stores/BreadcrumbsStore.ts
+++ src/stores/BreadcrumbsStore.ts
@@ -85,7 +85,7 @@ export class BreadcrumbsStore extends AsyncStoreWithClient<IState> {
                 // The tests might not result in a valid room object.
                 const room = this.matrixClient.getRoom(payload.room_id);
                 const membership = room?.getMyMembership();
-                if (room && membership==='join') await this.appendRoom(room);
+                if (room && membership==="join") await this.appendRoom(room);
             }
         } else if (payload.action === Action.JoinRoom) {
             const room = this.matrixClient.getRoom(payload.roomId);
