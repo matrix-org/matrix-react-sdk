4c403e1cc070426831724f049866f0fb2c163a49
github-action-benchmark <github@users.noreply.github.com>
2022-05-17 14:46:13 +0000

    add Cypress measurements (jsperformanceentry) benchmark result for c122c5cd3ba00bc62eac9a22e655424f78dd6bd8

---
+++ cypress/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1652798773912,
+  "repoUrl": "https://github.com/matrix-org/matrix-react-sdk",
+  "entries": {
+    "Cypress measurements": [
+      {
+        "commit": {
+          "author": {
+            "email": "jryans@gmail.com",
+            "name": "J. Ryan Stinnett",
+            "username": "jryans"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c122c5cd3ba00bc62eac9a22e655424f78dd6bd8",
+          "message": "Add basic performance testing via Cypress (#8586)",
+          "timestamp": "2022-05-17T15:38:45+01:00",
+          "tree_id": "85ef7dcfd21a62b284a9946c22ee6c527b7ab4b6",
+          "url": "https://github.com/matrix-org/matrix-react-sdk/commit/c122c5cd3ba00bc62eac9a22e655424f78dd6bd8"
+        },
+        "date": 1652798764554,
+        "tool": "jsperformanceentry",
+        "benches": [
+          {
+            "name": "cy:1-register/register:create-account",
+            "value": 1428.3999999999942,
+            "unit": "ms",
+            "extra": "type: measure"
+          },
+          {
+            "name": "cy:1-register/register:from-submit-to-home",
+            "value": 2813,
+            "unit": "ms",
+            "extra": "type: measure"
+          },
+          {
+            "name": "cy:2-login/login:from-submit-to-home",
+            "value": 2718.7999999999884,
+            "unit": "ms",
+            "extra": "type: measure"
+          },
+          {
+            "name": "cy:4-create-room/create-room:from-submit-to-room",
+            "value": 1572.7999999999884,
+            "unit": "ms",
+            "extra": "type: measure"
+          }
+        ]
+      }
+    ]
+  }
+}
