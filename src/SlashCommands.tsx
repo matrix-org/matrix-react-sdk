d7d74d5f1d37510f0ec3101c55c87d5d1e961f9a
Johannes Marbach <johannesm@element.io>
2022-05-20 18:39:57 +0200

    Remove trailing space

--- src/SlashCommands.tsx
+++ src/SlashCommands.tsx
@@ -476,7 +476,7 @@ export const Commands = [
             }
 
             const content: MRoomTopicEventContent = room.currentState.getStateEvents('m.room.topic', '')?.getContent();
-            const topic = !!content 
+            const topic = !!content
                 ? ContentHelpers.parseTopicContent(content)
                 : { text: _t('This room has no topic.') };
 
