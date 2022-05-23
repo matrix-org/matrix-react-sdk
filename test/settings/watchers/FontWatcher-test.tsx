74159bc0fbb2d5ad5b41e2349d654eaeb4904d62
r00ster <r00ster91@protonmail.com>
2022-05-23 17:25:58 +0200

    Apply suggestions

--- test/settings/watchers/FontWatcher-test.tsx
+++ test/settings/watchers/FontWatcher-test.tsx
@@ -1,4 +1,5 @@
 /*
+Copyright 2022 r00ster91
 Copyright 2022 The Matrix.org Foundation C.I.C.
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,18 +19,19 @@ import SettingsStore from '../../../src/settings/SettingsStore';
 import { SettingLevel } from '../../../src/settings/SettingLevel';
 
 describe('FontWatcher', function() {
-    it('should set a system font', () => {
-        SettingsStore.setValue("useSystemFont", null, SettingLevel.DEVICE, true).then(() => {
-            SettingsStore.setValue("systemFont", null, SettingLevel.DEVICE, "Fira Sans Thin, Commodore 64").then(() => {
-                expect(SettingsStore.getValue("systemFont")).toBe(`"Fira Sans Thin","Commodore 64"`);
-            });
-            SettingsStore.setValue("systemFont", null, SettingLevel.DEVICE, `"Commodore 64"`).then(() => {
-                expect(SettingsStore.getValue("systemFont")).toBe(`"Commodore 64"`);
-            });
-            const font = `  Fira Code  ,  "   Commodore 64   " `;
-            SettingsStore.setValue("systemFont", null, SettingLevel.DEVICE, font).then(() => {
-                expect(SettingsStore.getValue("systemFont")).toBe(`"Fira Code","Commodore 64"`);
-            });
-        });
+    beforeEach(() => await SettingsStore.setValue("useSystemFont", null, SettingLevel.DEVICE, true));
+
+    it('encloses the fonts by double quotes and sets them as the system font', () => {
+        await SettingsStore.setValue("systemFont", null, SettingLevel.DEVICE, "Fira Sans Thin, Commodore 64");
+        expect(SettingsStore.getValue("systemFont")).toBe(`"Fira Sans Thin","Commodore 64"`);
+    });
+    it('does not add double quotes if already present and sets the font as the system font', () => {
+        await SettingsStore.setValue("systemFont", null, SettingLevel.DEVICE, `"Commodore 64"`);
+        expect(SettingsStore.getValue("systemFont")).toBe(`"Commodore 64"`);
+    });
+    it('trims whitespace, encloses the fonts by double quotes, and sets them as the system font', () => {
+        const font = `  Fira Code  ,  "   Commodore 64   " `;
+        await SettingsStore.setValue("systemFont", null, SettingLevel.DEVICE, font)
+        expect(SettingsStore.getValue("systemFont")).toBe(`"Fira Code","Commodore 64"`);
     });
 });
