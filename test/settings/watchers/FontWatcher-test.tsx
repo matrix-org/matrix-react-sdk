07aec46043af51bba3f4198b5c9a86d1d02d5ffc
Michael Telatynski <7t3chguy@gmail.com>
2022-05-23 16:43:04 +0100

    Apply suggestions from code review

--- test/settings/watchers/FontWatcher-test.tsx
+++ test/settings/watchers/FontWatcher-test.tsx
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 r00ster91
+Copyright 2022 r00ster91 <r00ster91@proton.me>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
