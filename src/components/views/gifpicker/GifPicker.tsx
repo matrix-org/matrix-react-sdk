6815713cedc6da91bd0910f96ec83d8cc2c6302c
Alexander Neuhaeuser <alexander.neuhaeuser.py@gmail.com>
2022-05-27 20:08:28 +0200

    Change Gif Layout => 2 Columns intersecting

--- src/components/views/gifpicker/GifPicker.tsx
+++ src/components/views/gifpicker/GifPicker.tsx
@@ -6,7 +6,7 @@ interface IProps{
 }
 
 interface IState{
-    gifs?: Gif[];
+    gifs?: Gif[][];
     searchTerm: string;
 }
 
@@ -38,24 +38,28 @@ class GifPicker extends React.Component<IProps, IState> {
     };
 
     setGifsToTrending = () => {
-        fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${(this.APIKEY)}&limit=25&rating=r`)
+        const gifs = [[], []];
+        fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${(this.APIKEY)}&limit=26&rating=r`)
             .then(response => response.json())
             .then(data => {
+                gifs[0] = data.data.slice(0, 13);
+                gifs[1] = data.data.slice(14, 25);
                 this.setState({
-                    gifs: data.data,
+                    gifs: gifs,
                 });
             });
     };
 
     setGifsToSearch = (query, offset) => {
-        let gifs = [];
+        let gifs = [[], []];
         if (offset!=0) {
             gifs = this.state.gifs;
         }
         fetch(`https://api.giphy.com/v1/gifs/search?api_key=${(this.APIKEY)}&q=${query}&limit=${this.LIMIT}&rating=r&offset=${offset}`)
             .then(response => response.json())
             .then(data => {
-                gifs.push(...data.data);
+                gifs[0].push(...data.data.slice(0, this.LIMIT/2-1));
+                gifs[1].push(...data.data.slice(this.LIMIT/2, this.LIMIT-1));
                 this.setState({
                     gifs: gifs,
                 });
@@ -66,19 +70,20 @@ class GifPicker extends React.Component<IProps, IState> {
         const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
         if (bottom) {
             if (this.state.searchTerm != '') {
-                this.setGifsToSearch(this.state.searchTerm, this.state.gifs.length);
+                this.setGifsToSearch(this.state.searchTerm, this.state.gifs[0].length*2+1);
             }
         }
     };
 
     render() {
         return (
-            <div>
+            <div className="mx_GifPicker_Box">
                 <div className="mx_GifPicker_Menubar">
                     <input
                         type="text"
                         onChange={this.handleSearchBar}
                         placeholder="Search GIPHY"
+                        className="mx_GifPicker_Searchbar"
                     />
                     <img key="poweredBy"
                         src={require("../../../../res/img/element-icons/gifpicker/powered_by_giphy.png")}
@@ -87,12 +92,21 @@ class GifPicker extends React.Component<IProps, IState> {
                     />
                 </div>
                 <div className="mx_GifPicker" onScroll={this.handleScroll}>
-                    { this.state.gifs.map(d => (
-                        <div key={d.id}>
-                            <button key={"button_"+d.id} onClick={() => this.handleClick(d)}>
-                                <img key={d.id} src={d.images.fixed_height_small.url} />
-                            </button>
-                        </div>
+                    { this.state.gifs.map(column => (
+                        <>
+                            <div className="mx_GifPicker_Column">
+                                { column.map(gif => (
+                                    <div key={gif.id}>
+                                        <button key={"button_"+gif.id} onClick={() => this.handleClick(gif)}>
+                                            <img key={"gif_"+gif.id}
+                                                src={gif.images.fixed_height_small.url}
+                                                className="mx_GifPicker_Gif"
+                                                alt={gif.slug} />
+                                        </button>
+                                    </div>
+                                )) }
+                            </div>
+                        </>
                     )) }
                 </div>
             </div>
