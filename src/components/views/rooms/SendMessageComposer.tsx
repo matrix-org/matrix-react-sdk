d74ccd61f8616256d2a0edd3a0fe377a62e5b615
Michael Telatynski <7t3chguy@gmail.com>
2022-05-30 11:02:36 +0100

    Relations is now accessible on the Room instead

--- src/components/views/rooms/SendMessageComposer.tsx
+++ src/components/views/rooms/SendMessageComposer.tsx
@@ -285,7 +285,7 @@ export class SendMessageComposer extends React.Component<ISendMessageComposerPro
                 let shouldReact = true;
                 const lastMessage = events[i];
                 const userId = MatrixClientPeg.get().getUserId();
-                const messageReactions = this.props.room.getUnfilteredTimelineSet()
+                const messageReactions = this.props.room.relations
                     .getRelationsForEvent(lastMessage.getId(), RelationType.Annotation, EventType.Reaction);
 
                 // if we have already sent this reaction, don't redact but don't re-send
