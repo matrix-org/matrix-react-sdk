f945f98f8071c5ef6238c13782a19a10ec4abecb
Michael Telatynski <7t3chguy@gmail.com>
2022-06-13 16:41:18 +0100

    Move room directory Puppeteer tests over to Cypress

--- cypress/global.d.ts
+++ cypress/global.d.ts
@@ -16,7 +16,14 @@ limitations under the License.
 
 import "matrix-js-sdk/src/@types/global";
 import type { MatrixClient, ClientEvent } from "matrix-js-sdk/src/client";
-import type { MatrixScheduler, MemoryCryptoStore, MemoryStore, RoomStateEvent } from "matrix-js-sdk/src/matrix";
+import type {
+    MatrixScheduler,
+    MemoryCryptoStore,
+    MemoryStore,
+    Preset,
+    RoomStateEvent,
+    Visibility,
+} from "matrix-js-sdk/src/matrix";
 import type { RoomMemberEvent } from "matrix-js-sdk/src/models/room-member";
 import type { WebStorageSessionStore } from "matrix-js-sdk/src/store/session/webstorage";
 import type { MatrixDispatcher } from "../src/dispatcher/dispatcher";
@@ -42,6 +49,8 @@ declare global {
                 MemoryStore: typeof MemoryStore;
                 MemoryCryptoStore: typeof MemoryCryptoStore;
                 WebStorageSessionStore: typeof WebStorageSessionStore;
+                Visibility: typeof Visibility;
+                Preset: typeof Preset;
             };
         }
     }
