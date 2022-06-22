5822e549dfd165406eb724203af3a70a4e3f7895
Suguru Hirahara <luixxiul@users.noreply.github.com>
2022-06-22 11:35:17 +0900

    Add mx_GeneralUserSettingsTab and wrap style blocks with it
    Signed-off-by: Suguru Hirahara <luixxiul@users.noreply.github.com>

--- src/components/views/settings/tabs/user/GeneralUserSettingsTab.tsx
+++ src/components/views/settings/tabs/user/GeneralUserSettingsTab.tsx
@@ -528,7 +528,7 @@ export default class GeneralUserSettingsTab extends React.Component<IProps, ISta
         }
 
         return (
-            <div className="mx_SettingsTab">
+            <div className="mx_SettingsTab mx_GeneralUserSettingsTab">
                 <div className="mx_SettingsTab_heading">{_t("General")}</div>
                 {this.renderProfileSection()}
                 {this.renderAccountSection()}
