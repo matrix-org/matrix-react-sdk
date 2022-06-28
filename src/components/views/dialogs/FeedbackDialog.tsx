e940b51eb3e11db111590557cef8b20070bff063
Janne Mareike Koschinski <jannemk@element.io>
2022-06-28 17:13:15 +0200

    Add additional metadata to feedback submitted through spotlight dialog

--- src/components/views/dialogs/FeedbackDialog.tsx
+++ src/components/views/dialogs/FeedbackDialog.tsx
@@ -33,7 +33,9 @@ const existingIssuesUrl = "https://github.com/vector-im/element-web/issues" +
     "?q=is%3Aopen+is%3Aissue+sort%3Areactions-%2B1-desc";
 const newIssueUrl = "https://github.com/vector-im/element-web/issues/new/choose";
 
-interface IProps extends IDialogProps {}
+interface IProps extends IDialogProps {
+    extraData?: Record<string, string>;
+}
 
 const FeedbackDialog: React.FC<IProps> = (props: IProps) => {
     const feedbackRef = useRef<Field>();
@@ -55,7 +57,7 @@ const FeedbackDialog: React.FC<IProps> = (props: IProps) => {
     const onFinished = (sendFeedback: boolean): void => {
         if (hasFeedback && sendFeedback) {
             if (rageshakeUrl) {
-                submitFeedback(rageshakeUrl, "feedback", comment, canContact);
+                submitFeedback(rageshakeUrl, "feedback", comment, canContact, props.extraData);
             }
 
             Modal.createDialog(InfoDialog, {
