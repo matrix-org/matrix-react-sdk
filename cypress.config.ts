4c2d2a22f1858427fe03ab50e00f74cce3e0ed5b
Michael Telatynski <7t3chguy@gmail.com>
2022-07-07 13:12:19 +0100

    Remove stale comment

--- cypress.config.ts
+++ cypress.config.ts
@@ -23,8 +23,6 @@ export default defineConfig({
     defaultCommandTimeout: 10000,
     chromeWebSecurity: false,
     e2e: {
-        // We've imported your old cypress plugins here.
-        // You may want to clean this up later by importing these.
         setupNodeEvents(on, config) {
             return require('./cypress/plugins/index.ts').default(on, config);
         },
