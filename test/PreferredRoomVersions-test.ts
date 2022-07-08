c4c88f4070d3d3bc1c4d88639dbf2b07e17d9856
Travis Ralston <travisr@matrix.org>
2022-07-07 23:07:43 -0600

    It helps to use Jest correctly

--- test/PreferredRoomVersions-test.ts
+++ test/PreferredRoomVersions-test.ts
@@ -39,7 +39,8 @@ describe("doesRoomVersionSupport", () => {
     it("should detect restricted rooms in v9 and v10", () => {
         // Dev note: we consider it a feature that v8 rooms have to upgrade considering the bug in v8.
         // https://spec.matrix.org/v1.3/rooms/v8/#redactions
-        expect(doesRoomVersionSupport("9", PreferredRoomVersions.RestrictedRooms));
-        expect(doesRoomVersionSupport("10", PreferredRoomVersions.RestrictedRooms));
+        expect(doesRoomVersionSupport("8", PreferredRoomVersions.RestrictedRooms)).toBe(false);
+        expect(doesRoomVersionSupport("9", PreferredRoomVersions.RestrictedRooms)).toBe(true);
+        expect(doesRoomVersionSupport("10", PreferredRoomVersions.RestrictedRooms)).toBe(true);
     });
 });
