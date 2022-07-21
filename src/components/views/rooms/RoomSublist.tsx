050903d1f4b9dda87e1c5f8f8f5ea6f4251a96a6
Kegan Dougal <kegan@matrix.org>
2022-07-21 10:49:13 +0100

    Comment why any

--- src/components/views/rooms/RoomSublist.tsx
+++ src/components/views/rooms/RoomSublist.tsx
@@ -125,7 +125,7 @@ export default class RoomSublist extends React.Component<IProps, IState> {
     private notificationState: ListNotificationState;
 
     private slidingSyncMode: boolean;
-    private slidingSyncInit: Promise<any>;
+    private slidingSyncInit: Promise<any>; // we don't care about the resulting list, hence 'any'
 
     constructor(props: IProps) {
         super(props);
