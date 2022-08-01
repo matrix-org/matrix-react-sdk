e297a6fae0b077d802b39ecb88fbb6174d3211a3
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2022-08-01 10:25:10 +0100

    Make linter happy

--- cypress/e2e/trafficlight/register.spec.ts
+++ cypress/e2e/trafficlight/register.spec.ts
@@ -17,39 +17,35 @@ limitations under the License.
 /// <reference types="cypress" />
 
 function recurse() {
-          cy.log("Requesting next action...") 
-          var poll_url = Cypress.env("TRAFFICLIGHT_URL") + "/client/" + Cypress.env("TRAFFICLIGHT_UUID") + "/poll"
-          var respond_url = Cypress.env("TRAFFICLIGHT_URL") + "/client/" + Cypress.env("TRAFFICLIGHT_UUID") + "/respond"
-          cy.request(poll_url).then((resp) => {
-            expect(resp.status).to.eq(200);
-            // promote response out of the callback for future use.
-            var data = resp.body.data
-            var action = resp.body.action
-            cy.log("... got ", action, data)
-            switch(action) {
-              case "register":
+    cy.log("Requesting next action...");
+    const pollUrl = Cypress.env("TRAFFICLIGHT_URL") + "/client/" + Cypress.env("TRAFFICLIGHT_UUID") + "/poll";
+    const respondUrl = Cypress.env("TRAFFICLIGHT_URL") + "/client/" + Cypress.env("TRAFFICLIGHT_UUID") + "/respond";
+    cy.request(pollUrl).then((resp) => {
+        expect(resp.status).to.eq(200);
+        // promote response out of the callback for future use.
+        const data = resp.body.data;
+        const action = resp.body.action;
+        cy.log("... got ", action, data);
+        switch (action) {
+            case "register":
                 cy.get(".mx_ServerPicker_change", { timeout: 15000 }).click();
                 cy.get(".mx_ServerPickerDialog_continue").should("be.visible");
-        
                 cy.get(".mx_ServerPickerDialog_otherHomeserver").type(data['homeserver_url']['local']);
                 cy.get(".mx_ServerPickerDialog_continue").click();
                 // wait for the dialog to go away
                 cy.get('.mx_ServerPickerDialog').should('not.exist');
-        
                 cy.get("#mx_RegistrationForm_username").should("be.visible");
                 // Hide the server text as it contains the randomly allocated Synapse port
-        
                 cy.get("#mx_RegistrationForm_username").type(data['username']);
                 cy.get("#mx_RegistrationForm_password").type(data['password']);
                 cy.get("#mx_RegistrationForm_passwordConfirm").type(data['password']);
                 cy.get(".mx_Login_submit").click();
                 cy.get('.mx_UseCaseSelection_skip > .mx_AccessibleButton').click();
-
-                cy.request('POST', respond_url,{ response: "registered" }).then((response) => {
-                  expect(response.status).to.eq(200);
-                })
+                cy.request('POST', respondUrl, { response: "registered" }).then((response) => {
+                    expect(response.status).to.eq(200);
+                });
                 break;
-              case "login": 
+            case "login":
                 cy.visit("http://localhost:8083/#/login");
                 cy.get("#mx_LoginForm_username", { timeout: 15000 }).should("be.visible");
                 cy.get(".mx_ServerPicker_change").click();
@@ -57,66 +53,54 @@ function recurse() {
                 cy.get(".mx_ServerPickerDialog_continue").click();
                 // wait for the dialog to go away
                 cy.get('.mx_ServerPickerDialog').should('not.exist');
-    
                 cy.get("#mx_LoginForm_username").type(data['username']);
                 cy.get("#mx_LoginForm_password").type(data['password']);
                 cy.get(".mx_Login_submit").click();
-    
-                cy.request('POST', respond_url,{ response: "loggedin" }).then((response) => {
-                  expect(response.status).to.eq(200);
-                })
+                cy.request('POST', respondUrl, { response: "loggedin" }).then((response) => {
+                    expect(response.status).to.eq(200);
+                });
                 break;
-              case "start_crosssign":
+            case "start_crosssign":
                 cy.get('.mx_CompleteSecurity_actionRow > .mx_AccessibleButton').click();
-                cy.request('POST', respond_url,{ response: "started_crosssign" }).then((response) => {
-                  expect(response.status).to.eq(200);
-                })
+                cy.request('POST', respondUrl, { response: "started_crosssign" }).then((response) => {
+                    expect(response.status).to.eq(200);
+                });
                 break;
-              case "accept_crosssign":
-		// Can we please tag some buttons :)
-		// Click "Verify" when it comes up
+            case "accept_crosssign":
+                // Can we please tag some buttons :)
+                // Click "Verify" when it comes up
                 cy.get('.mx_AccessibleButton_kind_primary').click();
                 // Click to move to emoji verification
-                cy.get('.mx_VerificationPanel_QRPhase_startOption > .mx_AccessibleButton').click()
-                //cy.get(':nth-child(3) > .mx_AccessibleButton').click();
-                cy.request('POST', respond_url,{ response: "accepted_crosssign" }).then((response) => {
-                  expect(response.status).to.eq(200);
-                })
+                cy.get('.mx_VerificationPanel_QRPhase_startOption > .mx_AccessibleButton').click();
+                cy.request('POST', respondUrl, { response: "accepted_crosssign" }).then((response) => {
+                    expect(response.status).to.eq(200);
+                });
                 break;
-              case "verify_crosssign_emoji":
-                cy.get('.mx_VerificationShowSas_buttonRow > .mx_AccessibleButton_kind_primary').click()
-                cy.get('.mx_UserInfo_container > .mx_AccessibleButton').click()
-                cy.request('POST', respond_url,{ response: "verified_crosssign" }).then((response) => {
-                  expect(response.status).to.eq(200);
-                })
-                break 
-              case "idle": 
-                cy.wait(5000)
+            case "verify_crosssign_emoji":
+                cy.get('.mx_VerificationShowSas_buttonRow > .mx_AccessibleButton_kind_primary').click();
+                cy.get('.mx_UserInfo_container > .mx_AccessibleButton').click();
+                cy.request('POST', respondUrl, { response: "verified_crosssign" }).then((response) => {
+                    expect(response.status).to.eq(200);
+                });
                 break;
-              case "exit": 
-                cy.log("Client asked to exit, test complete or server teardown")
+            case "idle":
+                cy.wait(5000);
+                break;
+            case "exit":
+                cy.log("Client asked to exit, test complete or server teardown");
                 return;
-              default:
-                cy.log("WARNING: unknown action ", action)
-		return;
-            }
-        recurse()
-	
-        })
-
+            default:
+                cy.log("WARNING: unknown action ", action);
+                return;
+        }
+        recurse();
+    });
 }
 
-
 describe("traffic light cycle", () => {
-
-    // Ideally ensure this is a fresh cypress visit, rather than re-using
-    // TODO: validate; possibly by having more than one test in the entire framework.
-    beforeEach(() => {
-    });
-
     it("run trafficlight client", () => {
-          cy.visit("http://localhost:8083/#/register");
-          cy.log("Beginning recursion")
-          recurse()
+        cy.visit("http://localhost:8083/#/register");
+        cy.log("Beginning recursion");
+        recurse();
     });
 });
