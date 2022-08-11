1eb48e732614e3bd8d050a58cd44407b2b45bad2
Kerry Archibald <kerrya@element.io>
2022-08-11 13:28:43 +0200

    move types into type file

--- src/components/views/settings/devices/filter.ts
+++ src/components/views/settings/devices/filter.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { DeviceWithVerification } from "./useOwnDevices";
+import { DeviceWithVerification } from "./types";
 
 export enum DeviceSecurityVariation {
     Verified = 'Verified',
