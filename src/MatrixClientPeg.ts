09511422927e151ee0be7ec32e4d1bd6b4320941
Michael Telatynski <7t3chguy@gmail.com>
2022-08-22 13:47:31 +0100

    Make use of js-sdk roomNameGenerator to handle i18n for generated room names

--- src/MatrixClientPeg.ts
+++ src/MatrixClientPeg.ts
@@ -17,7 +17,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { ICreateClientOpts, PendingEventOrdering } from 'matrix-js-sdk/src/matrix';
+import { ICreateClientOpts, PendingEventOrdering, RoomNameState, RoomNameType } from 'matrix-js-sdk/src/matrix';
 import { IStartClientOpts, MatrixClient } from 'matrix-js-sdk/src/client';
 import { MemoryStore } from 'matrix-js-sdk/src/store/memory';
 import * as utils from 'matrix-js-sdk/src/utils';
@@ -37,6 +37,7 @@ import IdentityAuthClient from './IdentityAuthClient';
 import { crossSigningCallbacks, tryToUnlockSecretStorageWithDehydrationKey } from './SecurityManager';
 import SecurityCustomisations from "./customisations/Security";
 import CryptoStoreTooNewDialog from "./components/views/dialogs/CryptoStoreTooNewDialog";
+import { _t } from "./languageHandler";
 
 export interface IMatrixClientCreds {
     homeserverUrl: string;
@@ -278,6 +279,46 @@ class MatrixClientPegClass implements IMatrixClientPeg {
         return matches[1];
     }
 
+    private memberNamesToRoomName(names: string[], count: number): string {
+        const countWithoutMe = count - 1;
+        if (!names.length) {
+            return _t("Empty room");
+        }
+        if (names.length === 1 && countWithoutMe <= 1) {
+            return names[0];
+        }
+        if (names.length === 2 && countWithoutMe <= 2) {
+            return _t("%(user1)s and %(user2)s", {
+                user1: names[0],
+                user2: names[1],
+            });
+        }
+        return _t("%(user)s and %(count)s others", {
+            user: names[0],
+            count: countWithoutMe,
+        });
+    }
+
+    private inviteeNamesToRoomName(names: string[], count: number): string {
+        const countWithoutMe = count - 1;
+        if (!names.length) {
+            return _t("Empty room");
+        }
+        if (names.length === 1 && countWithoutMe <= 1) {
+            return names[0];
+        }
+        if (names.length === 2 && countWithoutMe <= 2) {
+            return _t("Inviting %(user1)s and %(user2)s", {
+                user1: names[0],
+                user2: names[1],
+            });
+        }
+        return _t("Inviting %(user)s and %(count)s others", {
+            user: names[0],
+            count: countWithoutMe,
+        });
+    }
+
     private createClient(creds: IMatrixClientCreds): void {
         const opts: ICreateClientOpts = {
             baseUrl: creds.homeserverUrl,
@@ -300,6 +341,27 @@ class MatrixClientPegClass implements IMatrixClientPeg {
             ],
             identityServer: new IdentityAuthClient(),
             cryptoCallbacks: {},
+            roomNameGenerator: (_: string, state: RoomNameState) => {
+                switch (state.type) {
+                    case RoomNameType.Generated:
+                        switch (state.subtype) {
+                            case "Inviting":
+                                return this.inviteeNamesToRoomName(state.names, state.count);
+                            default:
+                                return this.memberNamesToRoomName(state.names, state.count);
+                        }
+                    case RoomNameType.EmptyRoom:
+                        if (state.oldName) {
+                            return _t("Empty room (was %(oldName)s)", {
+                                oldName: state.oldName,
+                            });
+                        } else {
+                            return _t("Empty room");
+                        }
+                    default:
+                        return null;
+                }
+            },
         };
 
         // These are always installed regardless of the labs flag so that
