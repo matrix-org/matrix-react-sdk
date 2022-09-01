0fc15eebe1523e217ad2ff6ac1a3574604715acc
Germain Souquet <germains@element.io>
2022-09-01 09:39:54 +0100

    Only unsubscribe from threads events if needed

--- src/components/structures/ThreadView.tsx
+++ src/components/structures/ThreadView.tsx
@@ -198,12 +198,11 @@ export default class ThreadView extends React.Component<IProps, IState> {
     };
 
     private updateThread = (thread?: Thread) => {
-        if (this.state.thread) {
-            this.state.thread.off(ThreadEvent.NewReply, this.updateThreadRelation);
-            this.props.room.off(RoomEvent.LocalEchoUpdated, this.updateThreadRelation);
-        }
-
         if (thread && this.state.thread !== thread) {
+            if (this.state.thread) {
+                this.state.thread.off(ThreadEvent.NewReply, this.updateThreadRelation);
+                this.props.room.off(RoomEvent.LocalEchoUpdated, this.updateThreadRelation);
+            }
             thread.on(ThreadEvent.NewReply, this.updateThreadRelation);
             this.props.room.on(RoomEvent.LocalEchoUpdated, this.updateThreadRelation);
             this.setState({
