a335dedc791098ea64c9588c51d3b52f08c29f44
gefgu <gefgu@hotmail.com>
2022-09-10 09:11:42 -0300

    Remove unused import from cypress test

--- cypress/e2e/space-panel/space-panel.spec.ts
+++ cypress/e2e/space-panel/space-panel.spec.ts
@@ -17,7 +17,6 @@ limitations under the License.
 /// <reference types="cypress" />
 
 import { SynapseInstance } from "../../plugins/synapsedocker";
-// import Chainable = Cypress.Chainable;
 
 describe("Create Room", () => {
     let synapse: SynapseInstance;
