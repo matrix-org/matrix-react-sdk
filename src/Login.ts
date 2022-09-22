0255f41797c9cada735bfdbd53af4449babd39ff
Hugh Nimmo-Smith <hughns@users.noreply.github.com>
2022-09-22 11:36:28 +0100

    Update src/Login.ts
    Co-authored-by: Travis Ralston <travisr@matrix.org>

--- src/Login.ts
+++ src/Login.ts
@@ -25,7 +25,10 @@ import { IMatrixClientCreds } from "./MatrixClientPeg";
 import SecurityCustomisations from "./customisations/Security";
 
 export {
-    IdentityProviderBrand, IIdentityProvider, ISSOFlow, LoginFlow,
+    IdentityProviderBrand,
+    IIdentityProvider,
+    ISSOFlow,
+    LoginFlow,
 } from "matrix-js-sdk/src/@types/auth";
 
 interface ILoginOptions {
