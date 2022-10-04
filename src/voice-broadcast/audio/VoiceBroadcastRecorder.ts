1c5ab662fe571c49ee0acb0d8a6182c95743a872
Michael Weimann <michaelw@matrix.org>
2022-10-04 10:06:16 +0200

    Replace dev value with config

--- src/voice-broadcast/audio/VoiceBroadcastRecorder.ts
+++ src/voice-broadcast/audio/VoiceBroadcastRecorder.ts
@@ -135,7 +135,6 @@ export class VoiceBroadcastRecorder implements IDestroyable {
 }
 
 export const createVoiceBroadcastRecorder = () => {
-    //const targetChunkLength = SdkConfig.get("voice_broadcast")?.chunk_length || DEFAULTS.voice_broadcast.chunk_length;
-    const targetChunkLength = 10;
+    const targetChunkLength = SdkConfig.get("voice_broadcast")?.chunk_length || DEFAULTS.voice_broadcast.chunk_length;
     return new VoiceBroadcastRecorder(new VoiceRecording(), targetChunkLength);
 };
