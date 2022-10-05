75fab4c83490fe28d9384038d1d54fb24b65951f
Šimon Brandner <simon.bra.ag@gmail.com>
2022-10-05 20:50:00 +0200

    Add `participant_limit` to settigns
    Signed-off-by: Šimon Brandner <simon.bra.ag@gmail.com>

--- src/SdkConfig.ts
+++ src/SdkConfig.ts
@@ -33,6 +33,7 @@ export const DEFAULTS: IConfigOptions = {
     element_call: {
         url: "https://call.element.io",
         use_exclusively: false,
+        participant_limit: 8,
     },
 
     // @ts-ignore - we deliberately use the camelCase version here so we trigger
