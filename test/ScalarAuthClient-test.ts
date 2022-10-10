38aa3e5d3b81a99e69c440c4b1638cda692b992a
Michael Telatynski <7t3chguy@gmail.com>
2022-10-10 16:23:47 +0100

    Improve coverage

--- test/ScalarAuthClient-test.ts
+++ test/ScalarAuthClient-test.ts
@@ -14,12 +14,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import { mocked } from "jest-mock";
 import fetchMock from "fetch-mock-jest";
-import { MatrixError } from "matrix-js-sdk/src/matrix";
 
 import ScalarAuthClient from '../src/ScalarAuthClient';
-import { MatrixClientPeg } from '../src/MatrixClientPeg';
 import { stubClient } from './test-utils';
+import SdkConfig from "../src/SdkConfig";
 
 describe('ScalarAuthClient', function() {
     const apiUrl = 'https://test.com/api';
@@ -31,25 +31,24 @@ describe('ScalarAuthClient', function() {
         expires_in: 999,
     };
 
+    let client;
     beforeEach(function() {
-        window.localStorage.getItem = jest.fn((arg) => {
-            if (arg === "mx_scalar_token") return "brokentoken";
-        });
-        stubClient();
+        window.localStorage.setItem("mx_scalar_token", "brokentoken");
+        client = stubClient();
     });
 
     it('should request a new token if the old one fails', async function() {
         const sac = new ScalarAuthClient(apiUrl, uiUrl);
 
         fetchMock.get("https://test.com/api/account?scalar_token=brokentoken&v=1.1", {
-            throws: new MatrixError({ message: "Invalid token" }),
+            body: { message: "Invalid token" },
         });
 
         fetchMock.get("https://test.com/api/account?scalar_token=wokentoken&v=1.1", {
-            body: { user_id: MatrixClientPeg.get().getUserId() },
+            body: { user_id: client.getUserId() },
         });
 
-        MatrixClientPeg.get().getOpenIdToken = jest.fn().mockResolvedValue(tokenObject);
+        client.getOpenIdToken = jest.fn().mockResolvedValue(tokenObject);
 
         sac.exchangeForScalarToken = jest.fn((arg) => {
             if (arg === tokenObject) return Promise.resolve("wokentoken");
@@ -72,6 +71,103 @@ describe('ScalarAuthClient', function() {
             });
 
             await expect(sac.exchangeForScalarToken(tokenObject)).resolves.toBe("stoken");
+        });
+
+        it("should throw upon non-20x code", async () => {
+            const sac = new ScalarAuthClient(apiUrl, uiUrl);
+
+            fetchMock.post("https://test.com/api/register?v=1.1", {
+                status: 500,
+            });
+
+            await expect(sac.exchangeForScalarToken(tokenObject)).rejects.toThrow("Scalar request failed: 500");
+        });
+
+        it("should throw if scalar_token is missing in response", async () => {
+            const sac = new ScalarAuthClient(apiUrl, uiUrl);
+
+            fetchMock.post("https://test.com/api/register?v=1.1", {
+                body: {},
+            });
+
+            await expect(sac.exchangeForScalarToken(tokenObject)).rejects.toThrow("Missing scalar_token in response");
+        });
+    });
+
+    describe("registerForToken", () => {
+        it("should call `termsInteractionCallback` upon M_TERMS_NOT_SIGNED error", async () => {
+            const sac = new ScalarAuthClient(apiUrl, uiUrl);
+            const termsInteractionCallback = jest.fn();
+            sac.setTermsInteractionCallback(termsInteractionCallback);
+            fetchMock.get("https://test.com/api/account?scalar_token=testtoken&v=1.1", {
+                body: { errcode: "M_TERMS_NOT_SIGNED" },
+            });
+            sac.exchangeForScalarToken = jest.fn(() => Promise.resolve("testtoken"));
+            mocked(client.getTerms).mockResolvedValue({ policies: [] });
+
+            await expect(sac.registerForToken()).resolves.toBe("testtoken");
+        });
+
+        it("should throw upon non-20x code", async () => {
+            const sac = new ScalarAuthClient(apiUrl, uiUrl);
+            fetchMock.get("https://test.com/api/account?scalar_token=testtoken&v=1.1", {
+                body: { errcode: "SERVER_IS_SAD" },
+                status: 500,
+            });
+            sac.exchangeForScalarToken = jest.fn(() => Promise.resolve("testtoken"));
+
+            await expect(sac.registerForToken()).rejects.toBeTruthy();
+        });
+
+        it("should throw if user_id is missing from response", async () => {
+            const sac = new ScalarAuthClient(apiUrl, uiUrl);
+            fetchMock.get("https://test.com/api/account?scalar_token=testtoken&v=1.1", {
+                body: {},
+            });
+            sac.exchangeForScalarToken = jest.fn(() => Promise.resolve("testtoken"));
+
+            await expect(sac.registerForToken()).rejects.toThrow("Missing user_id in response");
+        });
+    });
+
+    describe("getScalarPageTitle", () => {
+        let sac: ScalarAuthClient;
+
+        beforeEach(async () => {
+            window.localStorage.setItem("mx_scalar_token", "wokentoken");
+            SdkConfig.put({
+                integrations_rest_url: apiUrl,
+                integrations_ui_url: uiUrl,
+            });
+
+            fetchMock.get("https://test.com/api/account?scalar_token=wokentoken&v=1.1", {
+                body: { user_id: client.getUserId() },
+            });
+
+            sac = new ScalarAuthClient(apiUrl, uiUrl);
+            await sac.connect();
+        });
+
+        it("should return `cached_title` from API /widgets/title_lookup", async () => {
+            const url = "google.com";
+            fetchMock.get("https://test.com/api/widgets/title_lookup?scalar_token=wokentoken&curl=" + url, {
+                body: {
+                    page_title_cache_item: {
+                        cached_title: "Google",
+                    },
+                },
+            });
+
+            await expect(sac.getScalarPageTitle(url)).resolves.toBe("Google");
+        });
+
+        it("should throw upon non-20x code", async () => {
+            const url = "google.com";
+            fetchMock.get("https://test.com/api/widgets/title_lookup?scalar_token=wokentoken&curl=" + url, {
+                status: 500,
+            });
+
+            await expect(sac.getScalarPageTitle(url)).rejects.toThrow("Scalar request failed: 500");
         });
     });
 });
--- test/test-utils/test-utils.ts
+++ test/test-utils/test-utils.ts
@@ -158,7 +158,7 @@ export function createTestClient(): MatrixClient {
         getOpenIdToken: jest.fn().mockResolvedValue(undefined),
         registerWithIdentityServer: jest.fn().mockResolvedValue({}),
         getIdentityAccount: jest.fn().mockResolvedValue({}),
-        getTerms: jest.fn().mockResolvedValueOnce(undefined),
+        getTerms: jest.fn().mockResolvedValue({}),
         doesServerSupportUnstableFeature: jest.fn().mockResolvedValue(undefined),
         isVersionSupported: jest.fn().mockResolvedValue(undefined),
         getPushRules: jest.fn().mockResolvedValue(undefined),
