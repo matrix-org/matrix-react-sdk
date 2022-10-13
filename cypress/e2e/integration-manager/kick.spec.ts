18ed154d9bfee4d9896fd2fe81a3aff098faee0d
Michael Telatynski <7t3chguy@gmail.com>
2022-10-13 13:01:45 +0100

    Update kick.spec.ts

--- cypress/e2e/integration-manager/kick.spec.ts
+++ cypress/e2e/integration-manager/kick.spec.ts
@@ -91,8 +91,7 @@ function expectKickedMessage(shouldExist: boolean) {
     });
 
     // Check for the event message (or lack thereof)
-    cy.get(".mx_EventTile_line")
-        .contains(`${USER_DISPLAY_NAME} removed ${BOT_DISPLAY_NAME}: ${KICK_REASON}`)
+    cy.contains(".mx_EventTile_line", `${USER_DISPLAY_NAME} removed ${BOT_DISPLAY_NAME}: ${KICK_REASON}`)
         .should(shouldExist ? "exist" : "not.exist");
 }
 
