22b896b2085320035dae512cdacfec652cc2e6ca
Michael Telatynski <7t3chguy@gmail.com>
2022-11-01 12:06:43 +0000

    Update types to match js-sdk --strict mode

--- test/toasts/IncomingLegacyCallToast-test.tsx
+++ test/toasts/IncomingLegacyCallToast-test.tsx
@@ -39,7 +39,7 @@ describe('<IncomingLegacyCallToast />', () => {
     const mockRoom = new Room('!room:server.org', mockClient, userId);
     mockClient.deviceId = deviceId;
 
-    const call = new MatrixCall({ client: mockClient });
+    const call = new MatrixCall({ client: mockClient, roomId: "!roomId:server" });
     const defaultProps = {
         call,
     };
