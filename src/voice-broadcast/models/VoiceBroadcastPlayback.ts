308fbbb210b4466a98d2d7d1a84404b7e5ed9ff5
Michael Weimann <michaelw@matrix.org>
2022-11-09 11:46:23 +0100

    Remove local event check from voice broadcast playback

--- src/voice-broadcast/models/VoiceBroadcastPlayback.ts
+++ src/voice-broadcast/models/VoiceBroadcastPlayback.ts
@@ -131,8 +131,8 @@ export class VoiceBroadcastPlayback
     private addChunkEvent = async (event: MatrixEvent): Promise<boolean> => {
         const eventId = event.getId();
 
-        if (!eventId
-            || eventId.startsWith("~!") // don't add local events
+        if (
+            !eventId
             || event.getContent()?.msgtype !== MsgType.Audio // don't add non-audio event
         ) {
             return false;
