d3b6064d8ab691e931f611458851fa31365dbfa2
László Várady <laszlo.varady@protonmail.com>
2022-11-27 17:56:37 +0100

    VoiceRecording: test audio settings
    Signed-off-by: László Várady <laszlo.varady@protonmail.com>

--- src/audio/VoiceRecording.ts
+++ src/audio/VoiceRecording.ts
@@ -14,7 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import * as Recorder from 'opus-recorder';
+// @ts-ignore
+import Recorder from 'opus-recorder/dist/recorder.min.js';
 import encoderPath from 'opus-recorder/dist/encoderWorker.min.js';
 import { SimpleObservable } from "matrix-widget-api";
 import EventEmitter from "events";
