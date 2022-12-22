401c9de534d61cfd0bf92ed4bf17356d47be87e3
Alun Turner <alunt@element.io>
2022-12-22 12:47:37 +0000

    fix type error

--- test/components/views/rooms/wysiwyg_composer/utils/message-test.ts
+++ test/components/views/rooms/wysiwyg_composer/utils/message-test.ts
@@ -104,9 +104,7 @@ describe("message", () => {
                     roomContext: defaultRoomContext,
                     mxClient: mockClient,
                     permalinkCreator,
-                    relation: {
-                        event_id: null,
-                    },
+                    relation: {},
                 });
 
                 // Then
