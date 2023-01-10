32df6a661a45f82d03e47cb3cfb96ce4e54413d2
AHMAD KADRI <52747422+ahmadkadri@users.noreply.github.com>
2023-01-10 13:05:57 +0100

    change the copyright

--- cypress/e2e/one-to-one-chat/one-to-one-chat.spec.ts
+++ cypress/e2e/one-to-one-chat/one-to-one-chat.spec.ts
@@ -1,5 +1,6 @@
 /*
-Copyright 2022 The Matrix.org Foundation C.I.C.
+Copyright 2023 Ahmad Kadri
+Copyright 2023 Nordeck IT + Consulting GmbH.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
