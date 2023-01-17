ce8ec6de34b382d9294ba4aab7bfc7f93949a5b4
Janne Mareike Koschinski <jannemk@element.io>
2023-01-17 16:01:19 +0100

    Add option to show full identifier as tooltip on sender profiles

--- src/components/views/messages/DisambiguatedProfile.tsx
+++ src/components/views/messages/DisambiguatedProfile.tsx
@@ -1,6 +1,6 @@
 /*
 Copyright 2021 Šimon Brandner <simon.bra.ag@gmail.com>
-Copyright 2022 The Matrix.org Foundation C.I.C.
+Copyright 2022-2023 The Matrix.org Foundation C.I.C.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ limitations under the License.
 import React from "react";
 import { RoomMember } from "matrix-js-sdk/src/models/room-member";
 import classNames from "classnames";
+import { _t } from "../../../languageHandler";
 
 import { getUserNameColorClass } from "../../../utils/FormattingUtils";
 import UserIdentifier from "../../../customisations/UserIdentifier";
@@ -28,11 +29,12 @@ interface IProps {
     onClick?(): void;
     colored?: boolean;
     emphasizeDisplayName?: boolean;
+    withTooltip?: boolean;
 }
 
 export default class DisambiguatedProfile extends React.Component<IProps> {
     public render(): JSX.Element {
-        const { fallbackName, member, colored, emphasizeDisplayName, onClick } = this.props;
+        const { fallbackName, member, colored, emphasizeDisplayName, withTooltip, onClick } = this.props;
         const rawDisplayName = member?.rawDisplayName || fallbackName;
         const mxid = member?.userId;
 
@@ -50,13 +52,24 @@ export default class DisambiguatedProfile extends React.Component<IProps> {
             );
         }
 
+        let title: string;
+        if (mxid) {
+            title = _t('%(displayName)s (%(identifier)s)', {
+                displayName: rawDisplayName,
+                matrixId: UserIdentifier.getDisplayUserIdentifier(mxid, {
+                    withDisplayName: true,
+                    roomId: member.roomId
+                }),
+            });
+        }
+
         const displayNameClasses = classNames({
             mx_DisambiguatedProfile_displayName: emphasizeDisplayName,
             [colorClass]: true,
         });
 
         return (
-            <div className="mx_DisambiguatedProfile" onClick={onClick}>
+            <div className="mx_DisambiguatedProfile" title={withTooltip ? title : undefined} onClick={onClick}>
                 <span className={displayNameClasses} dir="auto">
                     {rawDisplayName}
                 </span>
