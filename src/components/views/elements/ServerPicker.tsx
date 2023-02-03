08ac6e48c375a22d886392638ed7bf4252179864
Half-Shot <will@half-shot.uk>
2023-02-03 11:36:14 +0000

    Fix "please change your homeserver" message

--- src/components/views/elements/ServerPicker.tsx
+++ src/components/views/elements/ServerPicker.tsx
@@ -93,14 +93,6 @@ const ServerPicker: React.FC<IProps> = ({ title, dialogTitle, serverConfig, onSe
         desc = (
             <span className="mx_ServerPicker_desc">{_t("Join millions for free on the largest public server")}</span>
         );
-    } else if (serverConfig.hsName === "attendees.fosdem.org") {
-        desc = <span className="mx_ServerPicker_desc">
-            Please change your homeserver to "fosdem.org" if you registered your account for FOSDEM 2021
-        </span>;
-    } else if (serverConfig.hsName === "attendees.fosdem.org") {
-        desc = <span className="mx_ServerPicker_desc">
-            Please change your homeserver to "fosdem.org" if you registered your account for FOSDEM 2021
-        </span>;
     } else if (serverConfig.hsName === "attendees.fosdem.org" && onServerConfigChange) {
         desc = <span className="mx_ServerPicker_desc">
             Please change your homeserver to "fosdem.org" if you registered your account for FOSDEM 2021
