ba07ab583a4c783447be929efa27b918905e7a2b
Michael Telatynski <7t3chguy@gmail.com>
2023-02-16 11:59:41 +0000

    Iterate

--- scripts/ci/layered.sh
+++ scripts/ci/layered.sh
@@ -14,7 +14,7 @@ set -ex
 # for the primary repo (react-sdk in this case).
 
 # Set up the js-sdk first
-scripts/fetchdep.sh matrix-org matrix-js-sdk
+GITHUB_BASE_REF=$JS_SDK_GITHUB_BASE_REF scripts/fetchdep.sh matrix-org matrix-js-sdk
 pushd matrix-js-sdk
 yarn link
 yarn install --pure-lockfile
