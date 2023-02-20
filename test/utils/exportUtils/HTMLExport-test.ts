a473b96d7bfa5ed1b7984f88ee795d117de5c4e5
grimhilt <grimhilt@users.noreply.github.com>
2023-02-20 14:59:48 +0100

    add test

--- test/utils/exportUtils/HTMLExport-test.ts
+++ test/utils/exportUtils/HTMLExport-test.ts
@@ -315,4 +315,33 @@ describe("HTMLExport", () => {
             expect(fileName).not.toMatch(/^files\/hello/);
         }
     });
+
+    it("should add link to next and previous file", async () => {
+        const exporter = new HTMLExporter(
+            room,
+            ExportType.LastNMessages,
+            {
+                attachmentsIncluded: false,
+                maxSize: 1_024 * 1_024,
+            },
+            () => {},
+        );
+        
+        //@ts-ignore private access
+        const firstPage = exporter.wrapHTML("", 0, 3).then((res) => {
+            expect(res).not.toContain('Previous group of messages');
+            expect(res).toContain('<div style="text-align:center;margin:10px"><a href="./messages2.html" style="font-weight:bold">Next group of messages</a></div>');
+        });
+        //@ts-ignore private access
+        const middlePage = exporter.wrapHTML("", 1, 3).then((res) => {
+            expect(res).toContain('<div style="text-align:center"><a href="./messages.html" style="font-weight:bold">Previous group of messages</a></div>');
+            expect(res).toContain('<div style="text-align:center;margin:10px"><a href="./messages3.html" style="font-weight:bold">Next group of messages</a></div>');
+        });
+        //@ts-ignore private access
+        const lastPage = exporter.wrapHTML("", 2, 3).then((res) => {
+            expect(res).toContain('<div style="text-align:center"><a href="./messages2.html" style="font-weight:bold">Previous group of messages</a></div>');
+            expect(res).not.toContain('Next group of messages');
+        });
+       
+    });
 });
