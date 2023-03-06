e24713735f32d065b650e77f3a29eddcaa1a30de
Mikhail Aheichyk <mikhail.aheichyk@nordeck.net>
2023-03-06 20:54:27 +0300

    Test is removed
    Signed-off-by: Mikhail Aheichyk <mikhail.aheichyk@nordeck.net>

--- test/test-utils/test-utils.ts
+++ test/test-utils/test-utils.ts
@@ -231,9 +231,6 @@ export function createTestClient(): MatrixClient {
                 room_id: roomId,
             });
         }),
-        startClient: jest.fn().mockResolvedValue(undefined),
-        stopClient: jest.fn().mockReturnValue(undefined),
-        removeAllListeners: jest.fn().mockReturnValue(undefined),
     } as unknown as MatrixClient;
 
     client.reEmitter = new ReEmitter(client);
