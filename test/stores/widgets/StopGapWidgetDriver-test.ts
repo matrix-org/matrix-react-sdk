17debfa85f7ef2bda93297c02223c3927444276f
Michael Telatynski <7t3chguy@gmail.com>
2023-03-07 09:45:55 +0000

    Apply `strictNullChecks` around the codebase

--- test/stores/widgets/StopGapWidgetDriver-test.ts
+++ test/stores/widgets/StopGapWidgetDriver-test.ts
@@ -338,30 +338,22 @@ describe("StopGapWidgetDriver", () => {
         });
 
         it("sends chat effects", async () => {
-            await driver.sendEvent(
-                EventType.RoomMessage,
-                {
-                    msgtype: MsgType.Text,
-                    body: "🎉",
-                },
-                null,
-            );
+            await driver.sendEvent(EventType.RoomMessage, {
+                msgtype: MsgType.Text,
+                body: "🎉",
+            });
 
             expect(dis.dispatch).toHaveBeenCalled();
         });
 
         it("does not send chat effects in threads", async () => {
-            await driver.sendEvent(
-                EventType.RoomMessage,
-                {
-                    "body": "🎉",
-                    "m.relates_to": {
-                        rel_type: RelationType.Thread,
-                        event_id: "$123",
-                    },
+            await driver.sendEvent(EventType.RoomMessage, {
+                "body": "🎉",
+                "m.relates_to": {
+                    rel_type: RelationType.Thread,
+                    event_id: "$123",
                 },
-                null,
-            );
+            });
 
             expect(dis.dispatch).not.toHaveBeenCalled();
         });
