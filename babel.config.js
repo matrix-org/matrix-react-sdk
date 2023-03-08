5e20068f9c9c04e51702722d1af3a428e4528f1c
Michael Telatynski <7t3chguy@gmail.com>
2023-03-08 09:19:14 +0000

    Stash work

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     sourceMaps: "inline",
+    assumptions: {
+        setPublicClassFields: true,
+    },
     presets: [
         [
             "@babel/preset-env",
@@ -10,16 +13,15 @@ module.exports = {
                     "last 2 Safari versions",
                     "last 2 Edge versions",
                 ],
+                include: ["@babel/plugin-proposal-class-properties"],
             },
         ],
-        ["@babel/preset-typescript", { allowDeclareFields: true }],
         "@babel/preset-react",
+        ["@babel/preset-typescript", { allowDeclareFields: true }],
     ],
     plugins: [
-        ["@babel/plugin-transform-typescript", { allowDeclareFields: true }],
         "@babel/plugin-proposal-export-default-from",
         "@babel/plugin-proposal-numeric-separator",
-        "@babel/plugin-proposal-class-properties",
         "@babel/plugin-proposal-object-rest-spread",
         "@babel/plugin-syntax-dynamic-import",
         "@babel/plugin-transform-runtime",
