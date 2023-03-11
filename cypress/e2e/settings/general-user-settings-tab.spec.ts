292b29b77876af4ab4accb4f9da7b96ba04ba39a
Suguru Hirahara <luixxiul@users.noreply.github.com>
2023-03-11 12:09:22 +0900

    lint
    Signed-off-by: Suguru Hirahara <luixxiul@users.noreply.github.com>

--- cypress/e2e/settings/general-user-settings-tab.spec.ts
+++ cypress/e2e/settings/general-user-settings-tab.spec.ts
@@ -39,45 +39,45 @@ describe("General user settings tab", () => {
         cy.get(".mx_SettingsTab.mx_GeneralUserSettingsTab").within(() => {
             cy.get("[data-testid='general']").should("have.text", "General");
 
-        // Check a random userId exists
-        cy.contains(".mx_ProfileSettings_profile_controls_userId", ":localhost");
+            // Check a random userId exists
+            cy.contains(".mx_ProfileSettings_profile_controls_userId", ":localhost");
 
-        // Wait until spinners disappear
-        cy.get(".mx_GeneralUserSettingsTab_accountSection .mx_Spinner").should("not.exist");
-        cy.get(".mx_GeneralUserSettingsTab_discovery .mx_Spinner").should("not.exist");
+            // Wait until spinners disappear
+            cy.get(".mx_GeneralUserSettingsTab_accountSection .mx_Spinner").should("not.exist");
+            cy.get(".mx_GeneralUserSettingsTab_discovery .mx_Spinner").should("not.exist");
 
-        // Check input areas for password change exist
-        cy.get("form.mx_GeneralUserSettingsTab_changePassword").within(() => {
-            cy.get("input[label='Current password']").should("exist");
-            cy.get("input[label='New password']").should("exist");
-            cy.get("input[label='Confirm password']").should("exist");
-        });
+            // Check input areas for password change exist
+            cy.get("form.mx_GeneralUserSettingsTab_changePassword").within(() => {
+                cy.get("input[label='Current password']").should("exist");
+                cy.get("input[label='New password']").should("exist");
+                cy.get("input[label='Confirm password']").should("exist");
+            });
 
-        // Check an input area for a new email address exists
-        cy.get("form.mx_EmailAddresses_new input[label='Email Address']").should("exist");
+            // Check an input area for a new email address exists
+            cy.get("form.mx_EmailAddresses_new input[label='Email Address']").should("exist");
 
-        // Check an input area for a new phone exists
-        cy.get("form.mx_PhoneNumbers_new input[label='Phone Number']").should("exist");
-        cy.contains("#mx_CountryDropdown_value", "+44");
+            // Check an input area for a new phone exists
+            cy.get("form.mx_PhoneNumbers_new input[label='Phone Number']").should("exist");
+            cy.contains("#mx_CountryDropdown_value", "+44");
 
-        // Check language dropdown menu
-        cy.get(".mx_GeneralUserSettingsTab_languageInput").should("have.text", "English");
+            // Check language dropdown menu
+            cy.get(".mx_GeneralUserSettingsTab_languageInput").should("have.text", "English");
 
-        // Check an input area for identity server exists
-        cy.get("form.mx_SetIdServer input[label='Enter a new identity server']").should("exist");
+            // Check an input area for identity server exists
+            cy.get("form.mx_SetIdServer input[label='Enter a new identity server']").should("exist");
 
-        // Check default integration manager
-        cy.get(".mx_SetIntegrationManager").within(() => {
-            cy.contains(".mx_SetIntegrationManager_heading_manager", "scalar.vector.im");
+            // Check default integration manager
+            cy.get(".mx_SetIntegrationManager").within(() => {
+                cy.contains(".mx_SetIntegrationManager_heading_manager", "scalar.vector.im");
 
-            // Make sure integration manager's toggle switch is enabled
-            cy.get(".mx_ToggleSwitch_enabled").should("exist");
-        });
+                // Make sure integration manager's toggle switch is enabled
+                cy.get(".mx_ToggleSwitch_enabled").should("exist");
+            });
 
-        // Make sure the account deactivation button is displayed
-        cy.get(
-            ".mx_SettingsTab_section[data-testid='account-management-section'] .mx_AccessibleButton_kind_danger",
-        ).should("have.text", "Deactivate Account");
+            // Make sure the account deactivation button is displayed
+            cy.get(
+                ".mx_SettingsTab_section[data-testid='account-management-section'] .mx_AccessibleButton_kind_danger",
+            ).should("have.text", "Deactivate Account");
         });
 
         // Exclude the random userId from snapshot
