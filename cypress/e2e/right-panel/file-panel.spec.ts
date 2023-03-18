1e923baac878896c917ad4214d1b68aefb3a3bb6
Suguru Hirahara <luixxiul@users.noreply.github.com>
2023-03-18 20:55:50 +0900

    'Assert'
    Signed-off-by: Suguru Hirahara <luixxiul@users.noreply.github.com>

--- cypress/e2e/right-panel/file-panel.spec.ts
+++ cypress/e2e/right-panel/file-panel.spec.ts
@@ -78,37 +78,37 @@ describe("FilePanel", () => {
             uploadFile("cypress/fixtures/matrix-org-client-versions.json"); // JSON
 
             cy.get(".mx_RoomView_body").within(() => {
-                // Ensure all of the file were uploaded and rendered
+                // Assert that all of the file were uploaded and rendered
                 cy.get(".mx_EventTile[data-layout='group']").should("have.length", 3);
 
-                // Ensure the image exists and has the alt string
+                // Assert that the image exists and has the alt string
                 cy.get(".mx_EventTile[data-layout='group'] img[alt='riot.png']").should("exist");
 
-                // Ensure the audio player is rendered
+                // Assert that the audio player is rendered
                 cy.get(".mx_EventTile[data-layout='group'] .mx_AudioPlayer_container").should("exist");
 
-                // Ensure the file button exists
+                // Assert that the file button exists
                 cy.contains(".mx_EventTile_last[data-layout='group'] .mx_MFileBody", ".json").should("exist");
             });
 
             cy.get(".mx_FilePanel").within(() => {
                 cy.get(".mx_RoomView_MessageList").within(() => {
-                    // Ensure that data-layout attribute is not applied to file tiles on the panel
+                    // Assert that data-layout attribute is not applied to file tiles on the panel
                     cy.get(".mx_EventTile[data-layout='group']").should("not.exist");
 
-                    // Ensure all of the file tiles are rendered
+                    // Assert that all of the file tiles are rendered
                     cy.get(".mx_EventTile").should("have.length", 3);
 
-                    // Ensure the download links are rendered
+                    // Assert that the download links are rendered
                     cy.get(".mx_MFileBody_download").should("have.length", 3);
 
-                    // Ensure the sender of the files is rendered on all of the tiles
+                    // Assert that the sender of the files is rendered on all of the tiles
                     cy.get(".mx_EventTile_senderDetails .mx_DisambiguatedProfile_displayName").should("have.length", 3);
                     cy.contains(".mx_EventTile_senderDetails .mx_DisambiguatedProfile_displayName", NAME);
 
                     // Detect the image file
                     cy.get(".mx_EventTile_mediaLine.mx_EventTile_image").within(() => {
-                        // Ensure the image is specified as thumbnail and has the alt string
+                        // Assert that the image is specified as thumbnail and has the alt string
                         cy.get(".mx_MImageBody").within(() => {
                             cy.get("img[class='mx_MImageBody_thumbnail']").should("exist");
                             cy.get("img[alt='riot.png']").should("exist");
@@ -117,17 +117,17 @@ describe("FilePanel", () => {
 
                     // Detect the audio file
                     cy.get(".mx_EventTile_mediaLine .mx_MAudioBody").within(() => {
-                        // Ensure the audio player is rendered
+                        // Assert that the audio player is rendered
                         cy.get(".mx_AudioPlayer_container").within(() => {
-                            // Ensure the play button is rendered
+                            // Assert that the play button is rendered
                             cy.get("[data-testid='play-pause-button']").should("exist");
                         });
                     });
 
                     // Detect the JSON file
-                    // Ensure the tile is rendered as a button
+                    // Assert that the tile is rendered as a button
                     cy.get(".mx_EventTile_mediaLine .mx_MFileBody .mx_MFileBody_info[role='button']").within(() => {
-                        // Ensure the file name is rendered inside the button
+                        // Assert that the file name is rendered inside the button
                         // File name: matrix-org-client-versions.json
                         cy.contains(".mx_MFileBody_info_filename", "matrix-org");
                     });
@@ -147,28 +147,28 @@ describe("FilePanel", () => {
                 cy.get(".mx_RoomView_MessageList").within(() => {
                     // Detect the audio file
                     cy.get(".mx_EventTile_mediaLine .mx_MAudioBody").within(() => {
-                        // Ensure the audio player is rendered
+                        // Assert that the audio player is rendered
                         cy.get(".mx_AudioPlayer_container").within(() => {
-                            // Ensure the audio file information is rendered
+                            // Assert that the audio file information is rendered
                             cy.get(".mx_AudioPlayer_mediaInfo").within(() => {
                                 cy.get(".mx_AudioPlayer_mediaName").should("have.text", "1sec.ogg");
                                 cy.contains(".mx_AudioPlayer_byline", "00:01").should("exist");
                                 cy.contains(".mx_AudioPlayer_byline", "(3.56 KB)").should("exist"); // actual size
                             });
 
-                            // Ensure the counter is zero before clicking the play button
+                            // Assert that the counter is zero before clicking the play button
                             cy.contains(".mx_AudioPlayer_seek [role='timer']", "00:00").should("exist");
 
                             // Click the play button
                             cy.get("[data-testid='play-pause-button'][aria-label='Play']").click();
 
-                            // Ensure the pause button is rendered
+                            // Assert that the pause button is rendered
                             cy.get("[data-testid='play-pause-button'][aria-label='Pause']").should("exist");
 
-                            // Ensure the timer is reset when the audio file finished playing
+                            // Assert that the timer is reset when the audio file finished playing
                             cy.contains(".mx_AudioPlayer_seek [role='timer']", "00:00").should("exist");
 
-                            // Ensure the play button is rendered
+                            // Assert that the play button is rendered
                             cy.get("[data-testid='play-pause-button'][aria-label='Play']").should("exist");
                         });
                     });
