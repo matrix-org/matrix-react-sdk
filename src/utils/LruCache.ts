478ede3c8f9eae3bbdf274788bbf77cb98e8069b
Michael Weimann <michaelw@matrix.org>
2023-03-22 15:51:28 +0100

    Make LruCache error robust

--- src/utils/LruCache.ts
+++ src/utils/LruCache.ts
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import { logger } from "matrix-js-sdk/src/logger";
+
 interface CacheItem<K, V> {
     key: K;
     value: V;
@@ -26,6 +28,7 @@ interface CacheItem<K, V> {
 /**
  * Least Recently Used cache.
  * Can be initialised with a capacity and drops the least recently used items.
+ * This cache should be error robust: Cache miss on error.
  *
  * Implemented via a key lookup map and a double linked list:
  *             head              tail
@@ -63,7 +66,13 @@ export class LruCache<K, V> {
      * @returns true: item in cache, else false
      */
     public has(key: K): boolean {
-        return this.getItem(key) !== undefined;
+        try {
+            return this.getItem(key) !== undefined;
+        } catch (e) {
+            // Should not happen but makes it more robust to the unknown.
+            this.onError(e);
+            return false;
+        }
     }
 
     /**
@@ -74,7 +83,13 @@ export class LruCache<K, V> {
      * @returns The value if found, else undefined
      */
     public get(key: K): V | undefined {
-        return this.getItem(key)?.value;
+        try {
+            return this.getItem(key)?.value;
+        } catch (e) {
+            // Should not happen but makes it more robust to the unknown.
+            this.onError(e);
+            return undefined;
+        }
     }
 
     /**
@@ -85,42 +100,11 @@ export class LruCache<K, V> {
      * @param value - Item value
      */
     public set(key: K, value: V): void {
-        const item = this.getItem(key);
-
-        if (item) {
-            // The item is already stored under this key. Update the value.
-            item.value = value;
-            return;
-        }
-
-        const newItem = {
-            key,
-            value,
-            next: null,
-            prev: null,
-        };
-
-        if (this.head) {
-            // Put item in front of the list.
-            this.head.prev = newItem;
-            newItem.next = this.head;
-        }
-
-        this.head = newItem;
-
-        if (!this.tail) {
-            // This is the first item added to the list. Also set it as tail.
-            this.tail = newItem;
-        }
-
-        // Store item in lookup map.
-        this.map.set(key, newItem);
-
-        if (this.map.size > this.capacity) {
-            // Map size exceeded cache capcity. Drop tail item.
-            this.map.delete(this.tail.key);
-            this.tail = this.tail.prev;
-            this.tail.next = null;
+        try {
+            this.safeSet(key, value);
+        } catch (e) {
+            // Should not happen but makes it more robust to the unknown.
+            this.onError(e);
         }
     }
 
@@ -135,8 +119,22 @@ export class LruCache<K, V> {
         // Unknown item.
         if (!item) return;
 
-        this.removeItemFromList(item);
-        this.map.delete(key);
+        try {
+            this.removeItemFromList(item);
+            this.map.delete(key);
+        } catch (e) {
+            // Should not happen but makes it more robust to the unknown.
+            this.onError(e);
+        }
+    }
+
+    /**
+     * Clears the cache.
+     */
+    public clear(): void {
+        this.map = new Map();
+        this.head = null;
+        this.tail = null;
     }
 
     /**
@@ -146,6 +144,44 @@ export class LruCache<K, V> {
         for (const item of this.map.values()) {
             yield item.value;
         }
+    }
+
+    private safeSet(key: K, value: V): void {
+        const item = this.getItem(key);
+
+        if (item) {
+            // The item is already stored under this key. Update the value.
+            item.value = value;
+            return;
+        }
+
+        const newItem: CacheItem<K, V> = {
+            key,
+            value,
+            next: null,
+            prev: null,
+        };
+
+        if (this.head) {
+            // Put item in front of the list.
+            this.head.prev = newItem;
+            newItem.next = this.head;
+        }
+
+        this.setHeadTail(newItem);
+
+        // Store item in lookup map.
+        this.map.set(key, newItem);
+
+        if (this.tail && this.map.size > this.capacity) {
+            // Map size exceeded cache capcity. Drop tail item.
+            this.delete(this.tail.key);
+        }
+    }
+
+    private onError(e: unknown): void {
+        logger.warn("LruCache error", e);
+        this.clear();
     }
 
     private getItem(key: K): CacheItem<K, V> | undefined {
@@ -159,22 +195,40 @@ export class LruCache<K, V> {
         if (item === this.head) return item;
 
         this.removeItemFromList(item);
-        // …and put it to the front.
-        this.head.prev = item;
+
+        // Put item to the front.
+
+        if (this.head) {
+            this.head.prev = item;
+        }
+
         item.prev = null;
         item.next = this.head;
-        this.head = item;
+
+        this.setHeadTail(item);
 
         return item;
     }
 
+    private setHeadTail(item: CacheItem<K, V>): void {
+        if (item.prev === null) {
+            // Item has no previous item → head
+            this.head = item;
+        }
+
+        if (item.next === null) {
+            // Item has no next item → tail
+            this.tail = item;
+        }
+    }
+
     private removeItemFromList(item: CacheItem<K, V>): void {
         if (item === this.head) {
-            this.head = item.next ?? null;
+            this.head = item.next;
         }
 
         if (item === this.tail) {
-            this.tail = item.prev ?? null;
+            this.tail = item.prev;
         }
 
         if (item.prev) {
