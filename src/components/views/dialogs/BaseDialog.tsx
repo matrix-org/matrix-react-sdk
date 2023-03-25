1cdb6a10ca079c2a34a2762868fad893326511f8
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2023-03-25 18:23:52 +0530

    updated by increasing touch area for cancel button

--- src/components/views/dialogs/BaseDialog.tsx
+++ src/components/views/dialogs/BaseDialog.tsx
@@ -119,14 +119,11 @@ export default class BaseDialog extends React.Component<IProps> {
         let cancelButton;
         if (this.props.hasCancel) {
             cancelButton = (
-                <div className="mx_Dialog_cancelButton_wrapper" onClick={this.onCancelClick}>
-                    <AccessibleButton
-                        onClick={this.onCancelClick}
-                        className="mx_Dialog_cancelButton"
-                        aria-label={_t("Close dialog")}
-                    />
-                </div>
-               
+                <AccessibleButton
+                    onClick={this.onCancelClick}
+                    className="mx_Dialog_cancelButton"
+                    aria-label={_t("Close dialog")}
+                />
             );
         }
 
