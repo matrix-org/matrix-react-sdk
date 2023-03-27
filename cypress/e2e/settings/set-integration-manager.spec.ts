9151d6a34de109c0dffa95046397c13f4b61f5a4
Suguru Hirahara <luixxiul@users.noreply.github.com>
2023-03-28 02:34:30 +0900

    Scroll to 'mx_SetIntegrationManager' and take a snapshot of the dialog
    Signed-off-by: Suguru Hirahara <luixxiul@users.noreply.github.com>

--- cypress/e2e/settings/set-integration-manager.spec.ts
+++ cypress/e2e/settings/set-integration-manager.spec.ts
@@ -53,6 +53,11 @@ describe("Set integration manager", () => {
             });
         });
 
-        cy.get(".mx_SetIntegrationManager").percySnapshotElement("'Manage integrations' on General settings tab");
+        // Scroll to 'mx_SetIntegrationManager' and take a snapshot of the dialog
+        cy.get(".mx_SetIntegrationManager")
+            .scrollIntoView()
+            .should("be.visible")
+            .get(".mx_Dialog_wrapper")
+            .percySnapshotElement("'Manage integrations' on General settings tab");
     });
 });
