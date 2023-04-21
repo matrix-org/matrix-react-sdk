799f9a25a390dd0708eb915d8eb118ced8f2730e
Alun Turner <alunt@element.io>
2023-04-21 15:51:15 +0100

    add check to cypress tests

--- cypress/e2e/composer/composer.spec.ts
+++ cypress/e2e/composer/composer.spec.ts
@@ -135,6 +135,8 @@ describe("Composer", () => {
                     // Entering `//` or `/ ` hides the autocomplete contents
                     cy.findByRole("textbox").type(" ");
                     cy.findByTestId("autocomplete-wrapper").should("be.empty");
+                    cy.findByRole("textbox").type("{Backspace}");
+                    cy.findByTestId("autocomplete-wrapper").should("not.be.empty");
                     cy.findByRole("textbox").type("{Backspace}/");
                     cy.findByTestId("autocomplete-wrapper").should("be.empty");
 
