cda35615067d58c6ef661bbea9b97d8945724607
Michael Telatynski <7t3chguy@gmail.com>
2023-04-27 10:14:11 +0100

    Use generics to better type TabbedView

--- src/components/views/dialogs/RoomSettingsDialog.tsx
+++ src/components/views/dialogs/RoomSettingsDialog.tsx
@@ -37,14 +37,16 @@ import { ActionPayload } from "../../../dispatcher/payloads";
 import { NonEmptyArray } from "../../../@types/common";
 import { PollHistoryTab } from "../settings/tabs/room/PollHistoryTab";
 
-export const ROOM_GENERAL_TAB = "ROOM_GENERAL_TAB";
-export const ROOM_VOIP_TAB = "ROOM_VOIP_TAB";
-export const ROOM_SECURITY_TAB = "ROOM_SECURITY_TAB";
-export const ROOM_ROLES_TAB = "ROOM_ROLES_TAB";
-export const ROOM_NOTIFICATIONS_TAB = "ROOM_NOTIFICATIONS_TAB";
-export const ROOM_BRIDGES_TAB = "ROOM_BRIDGES_TAB";
-export const ROOM_ADVANCED_TAB = "ROOM_ADVANCED_TAB";
-export const ROOM_POLL_HISTORY_TAB = "ROOM_POLL_HISTORY_TAB";
+export enum RoomSettingsTab {
+    General = "ROOM_GENERAL_TAB",
+    Voip = "ROOM_VOIP_TAB",
+    Security = "ROOM_SECURITY_TAB",
+    Roles = "ROOM_ROLES_TAB",
+    Notifications = "ROOM_NOTIFICATIONS_TAB",
+    Bridges = "ROOM_BRIDGES_TAB",
+    Advanced = "ROOM_ADVANCED_TAB",
+    PollHistory = "ROOM_POLL_HISTORY_TAB",
+}
 
 interface IProps {
     roomId: string;
@@ -92,12 +94,12 @@ export default class RoomSettingsDialog extends React.Component<IProps, IState> 
         });
     };
 
-    private getTabs(): NonEmptyArray<Tab> {
-        const tabs: Tab[] = [];
+    private getTabs(): NonEmptyArray<Tab<RoomSettingsTab>> {
+        const tabs: Tab<RoomSettingsTab>[] = [];
 
         tabs.push(
             new Tab(
-                ROOM_GENERAL_TAB,
+                RoomSettingsTab.General,
                 _td("General"),
                 "mx_RoomSettingsDialog_settingsIcon",
                 <GeneralRoomSettingsTab roomId={this.props.roomId} />,
@@ -107,7 +109,7 @@ export default class RoomSettingsDialog extends React.Component<IProps, IState> 
         if (SettingsStore.getValue("feature_group_calls")) {
             tabs.push(
                 new Tab(
-                    ROOM_VOIP_TAB,
+                    RoomSettingsTab.Voip,
                     _td("Voice & Video"),
                     "mx_RoomSettingsDialog_voiceIcon",
                     <VoipRoomSettingsTab roomId={this.props.roomId} />,
@@ -116,7 +118,7 @@ export default class RoomSettingsDialog extends React.Component<IProps, IState> 
         }
         tabs.push(
             new Tab(
-                ROOM_SECURITY_TAB,
+                RoomSettingsTab.Security,
                 _td("Security & Privacy"),
                 "mx_RoomSettingsDialog_securityIcon",
                 (
@@ -130,7 +132,7 @@ export default class RoomSettingsDialog extends React.Component<IProps, IState> 
         );
         tabs.push(
             new Tab(
-                ROOM_ROLES_TAB,
+                RoomSettingsTab.Roles,
                 _td("Roles & Permissions"),
                 "mx_RoomSettingsDialog_rolesIcon",
                 <RolesRoomSettingsTab roomId={this.props.roomId} />,
@@ -139,7 +141,7 @@ export default class RoomSettingsDialog extends React.Component<IProps, IState> 
         );
         tabs.push(
             new Tab(
-                ROOM_NOTIFICATIONS_TAB,
+                RoomSettingsTab.Notifications,
                 _td("Notifications"),
                 "mx_RoomSettingsDialog_notificationsIcon",
                 (
@@ -155,7 +157,7 @@ export default class RoomSettingsDialog extends React.Component<IProps, IState> 
         if (SettingsStore.getValue("feature_bridge_state")) {
             tabs.push(
                 new Tab(
-                    ROOM_BRIDGES_TAB,
+                    RoomSettingsTab.Bridges,
                     _td("Bridges"),
                     "mx_RoomSettingsDialog_bridgesIcon",
                     <BridgeSettingsTab roomId={this.props.roomId} />,
@@ -166,7 +168,7 @@ export default class RoomSettingsDialog extends React.Component<IProps, IState> 
 
         tabs.push(
             new Tab(
-                ROOM_POLL_HISTORY_TAB,
+                RoomSettingsTab.PollHistory,
                 _td("Poll history"),
                 "mx_RoomSettingsDialog_pollsIcon",
                 <PollHistoryTab roomId={this.props.roomId} onFinished={() => this.props.onFinished(true)} />,
@@ -176,7 +178,7 @@ export default class RoomSettingsDialog extends React.Component<IProps, IState> 
         if (SettingsStore.getValue(UIFeature.AdvancedSettings)) {
             tabs.push(
                 new Tab(
-                    ROOM_ADVANCED_TAB,
+                    RoomSettingsTab.Advanced,
                     _td("Advanced"),
                     "mx_RoomSettingsDialog_warningIcon",
                     (
@@ -190,7 +192,7 @@ export default class RoomSettingsDialog extends React.Component<IProps, IState> 
             );
         }
 
-        return tabs as NonEmptyArray<Tab>;
+        return tabs as NonEmptyArray<Tab<RoomSettingsTab>>;
     }
 
     public render(): React.ReactNode {
