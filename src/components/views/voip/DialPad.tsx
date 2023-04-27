d6412f00ef2cd3d7c34aed4b848f99d782853d0f
Alun Turner <alunt@element.io>
2023-04-27 11:59:53 +0100

    export consts for testing

--- src/components/views/voip/DialPad.tsx
+++ src/components/views/voip/DialPad.tsx
@@ -20,8 +20,8 @@ import AccessibleButton, { ButtonEvent } from "../elements/AccessibleButton";
 import { _t } from "../../../languageHandler";
 import { XOR } from "../../../@types/common";
 
-const BUTTONS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"];
-const BUTTON_LETTERS = ["", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ", "", "+", ""];
+export const BUTTONS = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "*", "0", "#"];
+export const BUTTON_LETTERS = ["", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ", "", "+", ""];
 
 enum DialPadButtonKind {
     Digit,
