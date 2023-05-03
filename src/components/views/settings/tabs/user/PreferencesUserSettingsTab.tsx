ea5edebaae8d4469c37aeca6fff5ca6918082f1c
Kerry Archibald <kerrya@element.io>
2023-05-03 15:27:36 +1200

    semantic headings in keyboard settings tab

--- src/components/views/settings/tabs/user/PreferencesUserSettingsTab.tsx
+++ src/components/views/settings/tabs/user/PreferencesUserSettingsTab.tsx
@@ -1,5 +1,5 @@
 /*
-Copyright 2019-2021 The Matrix.org Foundation C.I.C.
+Copyright 2019-2023 The Matrix.org Foundation C.I.C.
 Copyright 2019 Michael Telatynski <7t3chguy@gmail.com>
 
 Licensed under the Apache License, Version 2.0 (the "License");
