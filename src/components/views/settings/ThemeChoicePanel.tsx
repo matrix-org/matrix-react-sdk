cfdb77468101b31c091888c06ab92028a85fd47b
Kerry Archibald <kerrya@element.io>
2023-05-09 16:51:53 +1200

    use semantic headings in User settings appearance tab

--- src/components/views/settings/ThemeChoicePanel.tsx
+++ src/components/views/settings/ThemeChoicePanel.tsx
@@ -30,6 +30,7 @@ import Field from "../elements/Field";
 import StyledRadioGroup from "../elements/StyledRadioGroup";
 import { SettingLevel } from "../../../settings/SettingLevel";
 import PosthogTrackers from "../../../PosthogTrackers";
+import SettingsSubsection from "./shared/SettingsSubsection";
 
 interface IProps {}
 
@@ -245,10 +246,9 @@ export default class ThemeChoicePanel extends React.Component<IProps, IState> {
 
         const orderedThemes = getOrderedThemes();
         return (
-            <div className="mx_SettingsTab_section mx_ThemeChoicePanel">
-                <span className="mx_SettingsTab_subheading">{_t("Theme")}</span>
+            <SettingsSubsection heading={_t("Theme")}>
                 {systemThemeSection}
-                <div className="mx_ThemeSelectors" data-testid="theme-choice-panel-selectors">
+                <div className="mx_ThemeChoicePanel_themeSelectors" data-testid="theme-choice-panel-selectors">
                     <StyledRadioGroup
                         name="theme"
                         definitions={orderedThemes.map((t) => ({
@@ -264,7 +264,7 @@ export default class ThemeChoicePanel extends React.Component<IProps, IState> {
                 </div>
                 {this.renderHighContrastCheckbox()}
                 {customThemeForm}
-            </div>
+            </SettingsSubsection>
         );
     }
 
