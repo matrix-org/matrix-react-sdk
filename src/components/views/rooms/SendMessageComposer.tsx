20f533994917211ce34770ac5c3f06e160f0ec97
ken <1610057945@qq.com>
2023-05-11 18:39:32 +0800

    Support Safari `Insert from iPhone or iPad`

--- src/components/views/rooms/SendMessageComposer.tsx
+++ src/components/views/rooms/SendMessageComposer.tsx
@@ -61,6 +61,8 @@ import { addReplyToMessageContent } from "../../../utils/Reply";
 import { doMaybeLocalRoomAction } from "../../../utils/local-room";
 import { Caret } from "../../../editor/caret";
 import { IDiff } from "../../../editor/diff";
+import { MIMEType } from "util";
+import { getFileInfo } from "prettier";
 
 /**
  * Build the mentions information based on the editor model (and any related events):
@@ -685,21 +687,16 @@ export class SendMessageComposer extends React.Component<ISendMessageComposerPro
         // Safari `Insert from iPhone or iPad`
         // data.getData("text/html") returns a string like: <img src="blob:https://...">
         if (data.types.includes("text/html")) {
-            const img_element_str=data.getData("text/html");
-            const img_element = document.createElement("div");
-            img_element.innerHTML = img_element_str;
-            const img_src = img_element.querySelector("img")?.src;
+            const imgElementStr=data.getData("text/html");
+            const parser = new DOMParser();
+            const imgDoc=parser.parseFromString(imgElementStr,"text/html");
 
-            fetch(img_src).then((response) => {
-                response.blob().then((img_blob) => {
-                    const file = new File([img_blob],"safari_pasted_image.png",{type:"image/png"});
-                    // ContentMessages.sharedInstance().sendContentListToRoom(
-                    //     [file],
-                    //     this.props.room.roomId,
-                    //     this.props.relation,
-                    //     this.props.mxClient,
-                    //     this.context.timelineRenderingType,
-                    // );
+            const imgSrc = imgDoc.querySelector("img").src;
+
+            fetch(imgSrc).then((response) => {
+                response.blob().then((imgBlob) => {
+                    const ext=imgBlob.type.split("/")[1];
+                    const file = new File([imgBlob],"safari_inserted_image."+ext,{type:imgBlob.type});
                     ContentMessages.sharedInstance().sendContentToRoom(
                         file,
                         this.props.room.roomId,
@@ -707,7 +704,13 @@ export class SendMessageComposer extends React.Component<ISendMessageComposerPro
                         this.props.mxClient,
                         null,
                     );
+                },
+                (error) => {
+                    console.log(error);
                 }
+                );
+            }, (error) => {
+                console.log(error);
             });
         }
 
