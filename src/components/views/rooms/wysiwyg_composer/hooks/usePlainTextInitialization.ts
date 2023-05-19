a31a7d03c79b073bcb675c8b9ca82e306c25d8a2
Alun Turner <alunt@element.io>
2023-05-19 12:30:52 +0100

    change to set with .innerHTML, add utils

--- src/components/views/rooms/wysiwyg_composer/hooks/usePlainTextInitialization.ts
+++ src/components/views/rooms/wysiwyg_composer/hooks/usePlainTextInitialization.ts
@@ -19,7 +19,41 @@ import { RefObject, useEffect } from "react";
 export function usePlainTextInitialization(initialContent = "", ref: RefObject<HTMLElement>): void {
     useEffect(() => {
         if (ref.current) {
-            ref.current.innerText = initialContent;
+            ref.current.innerHTML = initialContent;
         }
     }, [ref, initialContent]);
+}
+
+/**
+ * Decodes a string containing html character entities and returns the string with the entities replaced
+ * with regular characters, eg "&lt;" becomes "<"
+ */
+export function decodeHtmlEntities(html = ""): string {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = html;
+    return textArea.value;
+}
+
+/**
+ * Encodes a string so that characters are replaced with their corresponding html entities
+ * eg "<" becomes "&lt;".
+ *
+ */
+export function encodeHtmlEntities(text = ""): string {
+    const textArea = document.createElement("textarea");
+    const textNode = document.createTextNode(text);
+    textArea.appendChild(textNode);
+    return textArea.innerHTML;
+}
+
+// A markdown link looks like [link text](<href>)
+// We need to stop the regex backtracking to avoid super-linear performance. To do this, we use a lookahead
+// assertion (?=...), which greedily matches up to the closing "]" and then match the contents of that assertion
+// with a backreference \1. Since the backreference matches something that has already matched, it will not backtrack.
+const escapedMdLinkRegex = /\[(?=([^\]]*))\1\]\(&lt;(.*)&gt;\)/g;
+
+export function convertLinksForParser(text: string): string {
+    return text.replace(escapedMdLinkRegex, (match, linkText, href) => {
+        return `[${linkText}](<${href}>)`;
+    });
 }
