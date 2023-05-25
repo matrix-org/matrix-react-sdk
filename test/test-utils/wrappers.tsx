536c7eda110f0bfd01b53cb623d0a1252f11b31f
Michael Telatynski <7t3chguy@gmail.com>
2023-05-25 16:36:21 +0100

    Make tsc happier

--- test/test-utils/wrappers.tsx
+++ test/test-utils/wrappers.tsx
@@ -16,6 +16,7 @@ limitations under the License.
 
 import React, { ComponentType, Ref } from "react";
 import { MatrixClient } from "matrix-js-sdk/src/matrix";
+import { RenderOptions } from "@testing-library/react";
 
 import { MatrixClientPeg as peg } from "../../src/MatrixClientPeg";
 import MatrixClientContext from "../../src/contexts/MatrixClientContext";
@@ -58,7 +59,7 @@ export function wrapInSdkContext<T>(
     };
 }
 
-export function renderWithClientContextOptions(client: MatrixClient) {
+export function renderWithClientContextOptions(client: MatrixClient): RenderOptions {
     return {
         wrapper: ({ children }) => (
             <MatrixClientContext.Provider value={client}>{children}</MatrixClientContext.Provider>
