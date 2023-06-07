e342e780836dbbcf8606d7a8539a1fb79f104242
Alun Turner <alunt@element.io>
2023-06-07 10:06:28 +0100

    tidy up tests

--- test/components/views/rooms/wysiwyg_composer/hooks/useInputEventProcessor-test.tsx
+++ test/components/views/rooms/wysiwyg_composer/hooks/useInputEventProcessor-test.tsx
@@ -13,63 +13,29 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-import { IEventRelation } from "matrix-js-sdk/src/matrix";
+import { IEventRelation, MatrixEvent } from "matrix-js-sdk/src/matrix";
 import { waitFor } from "@testing-library/react";
 
 import { handleClipboardEvent } from "../../../../../../src/components/views/rooms/wysiwyg_composer/hooks/useInputEventProcessor";
 import { TimelineRenderingType } from "../../../../../../src/contexts/RoomContext";
-import { Layout } from "../../../../../../src/settings/enums/Layout";
 import { mkStubRoom, stubClient } from "../../../../../test-utils";
 import ContentMessages from "../../../../../../src/ContentMessages";
+import { IRoomState } from "../../../../../../src/components/structures/RoomView";
+
+const mockClient = stubClient();
+const mockRoom = mkStubRoom("mock room", "mock room", mockClient);
+const mockRoomState = {
+    room: mockRoom,
+    timelineRenderingType: TimelineRenderingType.Room,
+    replyToEvent: {} as unknown as MatrixEvent,
+} as unknown as IRoomState;
+
+const sendContentListToRoomSpy = jest.spyOn(ContentMessages.sharedInstance(), "sendContentListToRoom");
+const sendContentToRoomSpy = jest.spyOn(ContentMessages.sharedInstance(), "sendContentToRoom");
+const fetchSpy = jest.spyOn(window, "fetch");
+const logSpy = jest.spyOn(console, "log").mockImplementation(() => {});
 
 describe("handleClipboardEvent", () => {
-    const mockClient = stubClient();
-    const mockRoom = mkStubRoom("mock room", "mock room", mockClient);
-    const mockRoomState = {
-        room: mockRoom,
-        roomLoading: true,
-        peekLoading: false,
-        shouldPeek: true,
-        membersLoaded: false,
-        numUnreadMessages: 0,
-        canPeek: false,
-        showApps: false,
-        isPeeking: false,
-        showRightPanel: true,
-        joining: false,
-        showTopUnreadMessagesBar: false,
-        statusBarVisible: false,
-        canReact: false,
-        canSelfRedact: false,
-        canSendMessages: false,
-        resizing: false,
-        layout: Layout.Group,
-        lowBandwidth: false,
-        alwaysShowTimestamps: false,
-        showTwelveHourTimestamps: false,
-        readMarkerInViewThresholdMs: 3000,
-        readMarkerOutOfViewThresholdMs: 30000,
-        showHiddenEvents: false,
-        showReadReceipts: true,
-        showRedactions: true,
-        showJoinLeaves: true,
-        showAvatarChanges: true,
-        showDisplaynameChanges: true,
-        matrixClientIsReady: false,
-        showUrlPreview: false,
-        timelineRenderingType: TimelineRenderingType.Room,
-        threadId: undefined,
-        liveTimeline: undefined,
-        narrow: false,
-        activeCall: null,
-        msc3946ProcessDynamicPredecessor: false,
-    };
-
-    const sendContentListToRoomSpy = jest.spyOn(ContentMessages.sharedInstance(), "sendContentListToRoom");
-    const sendContentToRoomSpy = jest.spyOn(ContentMessages.sharedInstance(), "sendContentToRoom");
-    const fetchSpy = jest.spyOn(window, "fetch");
-    const logSpy = jest.spyOn(console, "log").mockImplementation(() => {});
-
     beforeEach(() => {
         jest.clearAllMocks();
     });
@@ -78,45 +44,47 @@ describe("handleClipboardEvent", () => {
         jest.restoreAllMocks();
     });
 
+    function createMockClipboardEvent(props: any): ClipboardEvent {
+        return { ...props } as ClipboardEvent;
+    }
+
     it("returns false if it is not a paste event", () => {
-        const originalEvent = { type: "copy" } as ClipboardEvent;
-        const input = [originalEvent, mockRoomState, mockClient] as const;
-        const output = handleClipboardEvent(...input);
+        const originalEvent = createMockClipboardEvent({ type: "copy" });
+        const output = handleClipboardEvent(originalEvent, mockRoomState, mockClient);
+
         expect(output).toBe(false);
     });
 
     it("returns false if clipboard data is null", () => {
-        const originalEvent = { type: "paste", clipboardData: null } as ClipboardEvent;
-        const input = [originalEvent, mockRoomState, mockClient] as const;
-        const output = handleClipboardEvent(...input);
+        const originalEvent = createMockClipboardEvent({ type: "paste", clipboardData: null });
+        const output = handleClipboardEvent(originalEvent, mockRoomState, mockClient);
+
         expect(output).toBe(false);
     });
 
     it("returns false if room is undefined", () => {
-        const originalEvent = { type: "paste" } as ClipboardEvent;
+        const originalEvent = createMockClipboardEvent({ type: "paste" });
         const { room, ...roomStateWithoutRoom } = mockRoomState;
-        const input = [originalEvent, roomStateWithoutRoom, mockClient] as const;
-        const output = handleClipboardEvent(...input);
+        const output = handleClipboardEvent(originalEvent, roomStateWithoutRoom, mockClient);
+
         expect(output).toBe(false);
     });
 
     it("returns false if room clipboardData files and types are empty", () => {
-        const originalEvent = {
+        const originalEvent = createMockClipboardEvent({
             type: "paste",
             clipboardData: { files: [], types: [] },
-        } as unknown as ClipboardEvent;
-        const input = [originalEvent, mockRoomState, mockClient] as const;
-        const output = handleClipboardEvent(...input);
+        });
+        const output = handleClipboardEvent(originalEvent, mockRoomState, mockClient);
         expect(output).toBe(false);
     });
 
     it("handles event and calls sendContentListToRoom when data files are present", () => {
-        const originalEvent = {
+        const originalEvent = createMockClipboardEvent({
             type: "paste",
             clipboardData: { files: ["something here"], types: [] },
-        } as unknown as ClipboardEvent;
-        const input = [originalEvent, mockRoomState, mockClient] as const;
-        const output = handleClipboardEvent(...input);
+        });
+        const output = handleClipboardEvent(originalEvent, mockRoomState, mockClient);
 
         expect(sendContentListToRoomSpy).toHaveBeenCalledTimes(1);
         expect(sendContentListToRoomSpy).toHaveBeenCalledWith(
@@ -130,13 +98,12 @@ describe("handleClipboardEvent", () => {
     });
 
     it("calls sendContentListToRoom with eventRelation when present", () => {
-        const originalEvent = {
+        const originalEvent = createMockClipboardEvent({
             type: "paste",
             clipboardData: { files: ["something here"], types: [] },
-        } as unknown as ClipboardEvent;
+        });
         const mockEventRelation = {} as unknown as IEventRelation;
-        const input = [originalEvent, mockRoomState, mockClient, mockEventRelation] as const;
-        const output = handleClipboardEvent(...input);
+        const output = handleClipboardEvent(originalEvent, mockRoomState, mockClient, mockEventRelation);
 
         expect(sendContentListToRoomSpy).toHaveBeenCalledTimes(1);
         expect(sendContentListToRoomSpy).toHaveBeenCalledWith(
@@ -153,13 +120,12 @@ describe("handleClipboardEvent", () => {
         const mockErrorMessage = "something went wrong";
         sendContentListToRoomSpy.mockRejectedValueOnce(new Error(mockErrorMessage));
 
-        const originalEvent = {
+        const originalEvent = createMockClipboardEvent({
             type: "paste",
             clipboardData: { files: ["something here"], types: [] },
-        } as unknown as ClipboardEvent;
+        });
         const mockEventRelation = {} as unknown as IEventRelation;
-        const input = [originalEvent, mockRoomState, mockClient, mockEventRelation] as const;
-        const output = handleClipboardEvent(...input);
+        const output = handleClipboardEvent(originalEvent, mockRoomState, mockClient, mockEventRelation);
 
         expect(sendContentListToRoomSpy).toHaveBeenCalledTimes(1);
         await waitFor(() => {
@@ -169,34 +135,32 @@ describe("handleClipboardEvent", () => {
     });
 
     it("calls the error handler when data types has text/html but data can not be parsed", () => {
-        const originalEvent = {
+        const originalEvent = createMockClipboardEvent({
             type: "paste",
             clipboardData: {
                 files: [],
                 types: ["text/html"],
                 getData: jest.fn().mockReturnValue("<div>invalid html"),
             },
-        } as unknown as ClipboardEvent;
+        });
         const mockEventRelation = {} as unknown as IEventRelation;
-        const input = [originalEvent, mockRoomState, mockClient, mockEventRelation] as const;
-        const output = handleClipboardEvent(...input);
+        const output = handleClipboardEvent(originalEvent, mockRoomState, mockClient, mockEventRelation);
 
         expect(logSpy).toHaveBeenCalledWith("Failed to handle pasted content as Safari inserted content");
         expect(output).toBe(false);
     });
 
     it("calls fetch when data types has text/html and data can parsed", () => {
-        const originalEvent = {
+        const originalEvent = createMockClipboardEvent({
             type: "paste",
             clipboardData: {
                 files: [],
                 types: ["text/html"],
                 getData: jest.fn().mockReturnValue(`<img src="blob:" />`),
             },
-        } as unknown as ClipboardEvent;
+        });
         const mockEventRelation = {} as unknown as IEventRelation;
-        const input = [originalEvent, mockRoomState, mockClient, mockEventRelation] as const;
-        handleClipboardEvent(...input);
+        handleClipboardEvent(originalEvent, mockRoomState, mockClient, mockEventRelation);
 
         expect(fetchSpy).toHaveBeenCalledTimes(1);
         expect(fetchSpy).toHaveBeenCalledWith("blob:");
@@ -205,17 +169,16 @@ describe("handleClipboardEvent", () => {
     it("calls error handler when fetch fails", async () => {
         const mockErrorMessage = "fetch failed";
         fetchSpy.mockRejectedValueOnce(mockErrorMessage);
-        const originalEvent = {
+        const originalEvent = createMockClipboardEvent({
             type: "paste",
             clipboardData: {
                 files: [],
                 types: ["text/html"],
                 getData: jest.fn().mockReturnValue(`<img src="blob:" />`),
             },
-        } as unknown as ClipboardEvent;
+        });
         const mockEventRelation = {} as unknown as IEventRelation;
-        const input = [originalEvent, mockRoomState, mockClient, mockEventRelation] as const;
-        const output = handleClipboardEvent(...input);
+        const output = handleClipboardEvent(originalEvent, mockRoomState, mockClient, mockEventRelation);
 
         await waitFor(() => {
             expect(logSpy).toHaveBeenCalledWith(mockErrorMessage);
@@ -231,20 +194,25 @@ describe("handleClipboardEvent", () => {
             },
         } as Response);
 
-        const originalEvent = {
+        const originalEvent = createMockClipboardEvent({
             type: "paste",
             clipboardData: {
                 files: [],
                 types: ["text/html"],
                 getData: jest.fn().mockReturnValue(`<img src="blob:" />`),
             },
-        } as unknown as ClipboardEvent;
+        });
         const mockEventRelation = {} as unknown as IEventRelation;
-        const input = [originalEvent, mockRoomState, mockClient, mockEventRelation] as const;
-        const output = handleClipboardEvent(...input);
+        const output = handleClipboardEvent(originalEvent, mockRoomState, mockClient, mockEventRelation);
 
         await waitFor(() => {
-            expect(sendContentToRoomSpy).toHaveBeenCalledTimes(1);
+            expect(sendContentToRoomSpy).toHaveBeenCalledWith(
+                expect.any(File),
+                mockRoom.roomId,
+                mockEventRelation,
+                mockClient,
+                mockRoomState.replyToEvent,
+            );
         });
         expect(output).toBe(true);
     });
@@ -259,17 +227,16 @@ describe("handleClipboardEvent", () => {
         const mockErrorMessage = "sendContentToRoom failed";
         sendContentToRoomSpy.mockRejectedValueOnce(mockErrorMessage);
 
-        const originalEvent = {
+        const originalEvent = createMockClipboardEvent({
             type: "paste",
             clipboardData: {
                 files: [],
                 types: ["text/html"],
                 getData: jest.fn().mockReturnValue(`<img src="blob:" />`),
             },
-        } as unknown as ClipboardEvent;
+        });
         const mockEventRelation = {} as unknown as IEventRelation;
-        const input = [originalEvent, mockRoomState, mockClient, mockEventRelation] as const;
-        const output = handleClipboardEvent(...input);
+        const output = handleClipboardEvent(originalEvent, mockRoomState, mockClient, mockEventRelation);
 
         await waitFor(() => {
             expect(logSpy).toHaveBeenCalledWith(mockErrorMessage);
