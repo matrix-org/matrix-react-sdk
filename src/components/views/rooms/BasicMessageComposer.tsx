22a58547df4a679ed3e408ec1a3961f822ccd5e5
Michael Telatynski <7t3chguy@gmail.com>
2023-06-14 09:41:04 +0100

    Fix slash commands not being enabled in certain cases

--- src/components/views/rooms/BasicMessageComposer.tsx
+++ src/components/views/rooms/BasicMessageComposer.tsx
@@ -50,7 +50,7 @@ import { getKeyBindingsManager } from "../../../KeyBindingsManager";
 import { ALTERNATE_KEY_NAME, KeyBindingAction } from "../../../accessibility/KeyboardShortcuts";
 import { _t } from "../../../languageHandler";
 import { linkify } from "../../../linkify-matrix";
-import { SdkContextClass } from "../../../contexts/SDKContext";
+import { SDKContext } from "../../../contexts/SDKContext";
 
 // matches emoticons which follow the start of a line or whitespace
 const REGEX_EMOTICON_WHITESPACE = new RegExp("(?:^|\\s)(" + EMOTICON_REGEX.source + ")\\s|:^$");
@@ -122,6 +122,9 @@ interface IState {
 }
 
 export default class BasicMessageEditor extends React.Component<IProps, IState> {
+    public static contextType = SDKContext;
+    public context!: React.ContextType<typeof SDKContext>;
+
     public readonly editorRef = createRef<HTMLDivElement>();
     private autocompleteRef = createRef<Autocomplete>();
     private formatBarRef = createRef<MessageComposerFormatBar>();
@@ -268,15 +271,11 @@ export default class BasicMessageEditor extends React.Component<IProps, IState> 
         if (isTyping && this.props.model.parts[0].type === "command") {
             const { cmd } = parseCommandString(this.props.model.parts[0].text);
             const command = CommandMap.get(cmd!);
-            if (!command?.isEnabled() || command.category !== CommandCategories.messages) {
+            if (!command?.isEnabled(this.context.client ?? null) || command.category !== CommandCategories.messages) {
                 isTyping = false;
             }
         }
-        SdkContextClass.instance.typingStore.setSelfTyping(
-            this.props.room.roomId,
-            this.props.threadId ?? null,
-            isTyping,
-        );
+        this.context.typingStore.setSelfTyping(this.props.room.roomId, this.props.threadId ?? null, isTyping);
 
         this.props.onChange?.(selection, inputType, diff);
     };
