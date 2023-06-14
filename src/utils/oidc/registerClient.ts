a76cde9d33c4636304d69c1b58398864648d812d
Kerry Archibald <kerrya@element.io>
2023-06-14 14:30:35 +1200

    OidcRegistrationClientMetadata type

--- src/utils/oidc/registerClient.ts
+++ src/utils/oidc/registerClient.ts
@@ -21,6 +21,12 @@ import { ValidatedIssuerConfig } from "matrix-js-sdk/src/oidc/validate";
 
 import { OidcClientError } from "./error";
 
+export type OidcRegistrationClientMetadata = {
+    clientName: string;
+    clientUri: string;
+    redirectUris: string[];
+};
+
 /**
  * Make the client registration request
  * @param registrationEndpoint URL as configured on ValidatedServerConfig
