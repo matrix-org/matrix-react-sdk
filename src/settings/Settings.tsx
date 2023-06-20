a814b86998567adc2083f6af72869aa72291339b
Janne Mareike Koschinski <janne.koschinski@matrix.org>
2023-06-20 15:16:38 +0200

    update beta card

--- src/settings/Settings.tsx
+++ src/settings/Settings.tsx
@@ -238,6 +238,7 @@ export const SETTINGS: { [setting: string]: ISetting } = {
                     </p>
                 </>
             ),
+            image: require("../../res/img/betas/notification_settings.png"),
         },
     },
     "feature_exploring_public_spaces": {
