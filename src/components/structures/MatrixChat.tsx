db78b5ce7ffc7ffef96ad2ce237a4c8fc7bc9097
Kerry Archibald <kerrya@element.io>
2023-06-22 18:41:31 +1200

    remove debug

--- src/components/structures/MatrixChat.tsx
+++ src/components/structures/MatrixChat.tsx
@@ -356,7 +356,6 @@ export default class MatrixChat extends React.PureComponent<IProps, IState> {
     }
 
     private async postLoginSetup(): Promise<void> {
-        console.log("YOOOO postLoginSetup");
         const cli = MatrixClientPeg.safeGet();
         const cryptoEnabled = cli.isCryptoEnabled();
         if (!cryptoEnabled) {
