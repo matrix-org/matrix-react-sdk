f5be14bb982767545cd15d5382045b870aefe63e
Kerry <kerrya@element.io>
2023-06-22 21:41:25 +1200

    Update src/utils/ValidatedServerConfig.ts
    Co-authored-by: Richard van der Hoff <1389908+richvdh@users.noreply.github.com>

--- src/utils/ValidatedServerConfig.ts
+++ src/utils/ValidatedServerConfig.ts
@@ -35,7 +35,7 @@ export interface ValidatedServerConfig {
     /**
      * Config related to delegated authentication
      * Included when delegated auth is configured and valid, otherwise undefined
-     * From homeserver .well-known m.authentication, and issuer's ./well-known/openid-configuration
+     * From homeserver .well-known m.authentication, and issuer's .well-known/openid-configuration
      * Used for OIDC native flow authentication
      */
     delegatedAuthentication?: ValidatedDelegatedAuthConfig;
