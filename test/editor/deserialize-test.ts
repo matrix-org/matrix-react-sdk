33ba3bec1c3cd7bd81cfe5b5c0e49934bdd41fba
Johannes Marbach <johannesm@element.io>
2023-07-04 19:55:16 +0200

    Fix copy/paste error

--- test/editor/deserialize-test.ts
+++ test/editor/deserialize-test.ts
@@ -183,8 +183,8 @@ describe("editor/deserialize", function () {
             expect(parts[1]).toStrictEqual({ type: "user-pill", text: "Alice]", resourceId: "@alice:hs.tld" });
             expect(parts[2]).toStrictEqual({ type: "plain", text: "!" });
         });
-        it("user pill with displayname containing linebreak square bracket", function () {
-            const html = 'Hi <a href="https://matrix.to/#/@alice:hs.tld">Alice<br/>123</a>!';
+        it("user pill with displayname containing linebreak", function () {
+            const html = 'Hi <a href="https://matrix.to/#/@alice:hs.tld">Alice<br>123</a>!';
             const parts = normalize(parseEvent(htmlMessage(html), createPartCreator()));
             expect(parts.length).toBe(3);
             expect(parts[0]).toStrictEqual({ type: "plain", text: "Hi " });
