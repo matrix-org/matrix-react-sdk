1c0fc4ced4cd5541f6e37204ae7183e53ae55d23
Alun Turner <alunt@element.io>
2023-07-11 13:12:18 +0100

    update comments

--- src/components/views/rooms/wysiwyg_composer/hooks/usePlainTextInitialization.ts
+++ src/components/views/rooms/wysiwyg_composer/hooks/usePlainTextInitialization.ts
@@ -18,7 +18,7 @@ import { RefObject, useEffect } from "react";
 
 export function usePlainTextInitialization(initialContent = "", ref: RefObject<HTMLElement>): void {
     useEffect(() => {
-        // we always read and write the ref.current using .innerHTML for consistency in linebreak handling
+        // always read and write the ref.current using .innerHTML for consistency in linebreak and HTML entity handling
         if (ref.current) {
             ref.current.innerHTML = initialContent;
         }
