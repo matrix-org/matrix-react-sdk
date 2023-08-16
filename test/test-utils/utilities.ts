4ce4796f3efdac99bf280b21dfc8501bbec70021
Richard van der Hoff <richard@matrix.org>
2023-08-16 08:58:09 +0100

    clarify ts-ignore

--- test/test-utils/utilities.ts
+++ test/test-utils/utilities.ts
@@ -228,7 +228,7 @@ export const clearAllModals = async (): Promise<void> => {
 
 /** Install a stub object at `navigator.mediaDevices` */
 export function useMockMediaDevices(): void {
-    // @ts-ignore
+    // @ts-ignore assignment of a thing that isn't a `MediaDevices` to read-only property
     navigator["mediaDevices"] = {
         enumerateDevices: jest.fn().mockResolvedValue([]),
         getUserMedia: jest.fn(),
