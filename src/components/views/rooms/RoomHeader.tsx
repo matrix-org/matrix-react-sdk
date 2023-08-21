59aab1da774e8e2602c531eaafe3d9de132ff1e3
Germain <germains@element.io>
2023-08-21 18:11:55 +0100

    Add room notification icon to room header

--- src/components/views/rooms/RoomHeader.tsx
+++ src/components/views/rooms/RoomHeader.tsx
@@ -19,6 +19,7 @@ import { Body as BodyText, IconButton } from "@vector-im/compound-web";
 import { Icon as VideoCallIcon } from "@vector-im/compound-design-tokens/icons/video-call.svg";
 import { Icon as VoiceCallIcon } from "@vector-im/compound-design-tokens/icons/voice-call.svg";
 import { Icon as ThreadsIcon } from "@vector-im/compound-design-tokens/icons/threads-solid.svg";
+import { Icon as NotificationsIcon } from "@vector-im/compound-design-tokens/icons/notifications-solid.svg";
 import { CallType } from "matrix-js-sdk/src/webrtc/call";
 
 import type { Room } from "matrix-js-sdk/src/matrix";
@@ -37,6 +38,7 @@ import { ViewRoomPayload } from "../../../dispatcher/payloads/ViewRoomPayload";
 import { Action } from "../../../dispatcher/actions";
 import { useRoomThreadNotifications } from "../../../hooks/room/useRoomThreadNotifications";
 import { NotificationColor } from "../../../stores/notifications/NotificationColor";
+import { useGlobalNotificationState } from "../../../hooks/useGlobalNotificationState";
 
 /**
  * A helper to transform a notification color to the what the Compound Icon Button
@@ -97,19 +99,18 @@ export default function RoomHeader({ room }: { room: Room }): JSX.Element {
     );
 
     const threadNotifications = useRoomThreadNotifications(room);
+    const globalNotificationState = useGlobalNotificationState();
 
     return (
-        <Flex
-            as="header"
-            align="center"
-            gap="var(--cpd-space-3x)"
-            className="mx_RoomHeader light-panel"
-            onClick={() => {
-                showOrHidePanel(RightPanelPhases.RoomSummary);
-            }}
-        >
+        <Flex as="header" align="center" gap="var(--cpd-space-3x)" className="mx_RoomHeader light-panel">
             <DecoratedRoomAvatar room={room} avatarSize={40} displayBadge={false} />
-            <Box flex="1" className="mx_RoomHeader_info">
+            <Box
+                flex="1"
+                className="mx_RoomHeader_info"
+                onClick={() => {
+                    showOrHidePanel(RightPanelPhases.RoomSummary);
+                }}
+            >
                 <BodyText
                     as="div"
                     size="lg"
@@ -131,8 +132,7 @@ export default function RoomHeader({ room }: { room: Room }): JSX.Element {
                 <IconButton
                     disabled={!!voiceCallDisabledReason}
                     title={!voiceCallDisabledReason ? _t("Voice call") : voiceCallDisabledReason!}
-                    onClick={async (ev) => {
-                        ev.stopPropagation();
+                    onClick={async () => {
                         placeCall(CallType.Voice, voiceCallType);
                     }}
                 >
@@ -141,8 +141,7 @@ export default function RoomHeader({ room }: { room: Room }): JSX.Element {
                 <IconButton
                     disabled={!!videoCallDisabledReason}
                     title={!videoCallDisabledReason ? _t("Video call") : videoCallDisabledReason!}
-                    onClick={(ev) => {
-                        ev.stopPropagation();
+                    onClick={() => {
                         placeCall(CallType.Video, videoCallType);
                     }}
                 >
@@ -150,12 +149,19 @@ export default function RoomHeader({ room }: { room: Room }): JSX.Element {
                 </IconButton>
                 <IconButton
                     indicator={notificationColorToIndicator(threadNotifications)}
-                    onClick={(ev) => {
-                        ev.stopPropagation();
+                    onClick={() => {
                         showOrHidePanel(RightPanelPhases.ThreadPanel);
                     }}
                 >
                     <ThreadsIcon />
+                </IconButton>
+                <IconButton
+                    indicator={notificationColorToIndicator(globalNotificationState.color)}
+                    onClick={() => {
+                        showOrHidePanel(RightPanelPhases.NotificationPanel);
+                    }}
+                >
+                    <NotificationsIcon />
                 </IconButton>
             </Flex>
         </Flex>
