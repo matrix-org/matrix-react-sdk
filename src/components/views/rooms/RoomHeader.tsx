1e37d3d6742f2209d58a3746f8e99356b18e7b9b
Germain <germains@element.io>
2023-08-21 16:46:29 +0100

    Add Voice and Video call in room header

--- src/components/views/rooms/RoomHeader.tsx
+++ src/components/views/rooms/RoomHeader.tsx
@@ -14,10 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import React from "react";
-import { Body as BodyText } from "@vector-im/compound-web";
+import React, { useCallback } from "react";
+import { Body as BodyText, IconButton } from "@vector-im/compound-web";
+import { Icon as VideoCallIcon } from "@vector-im/compound-design-tokens/icons/video-call.svg";
+import { Icon as VoiceCallIcon } from "@vector-im/compound-design-tokens/icons/voice-call.svg";
+import { CallType } from "matrix-js-sdk/src/webrtc/call";
 
 import type { Room } from "matrix-js-sdk/src/matrix";
+import { _t } from "../../../languageHandler";
 import { useRoomName } from "../../../hooks/useRoomName";
 import DecoratedRoomAvatar from "../avatars/DecoratedRoomAvatar";
 import { RightPanelPhases } from "../../../stores/right-panel/RightPanelStorePhases";
@@ -25,10 +29,45 @@ import RightPanelStore from "../../../stores/right-panel/RightPanelStore";
 import { useTopic } from "../../../hooks/room/useTopic";
 import { Flex } from "../../utils/Flex";
 import { Box } from "../../utils/Box";
+import { useRoomCallStatus } from "../../../hooks/room/useRoomCallStatus";
+import LegacyCallHandler from "../../../LegacyCallHandler";
+import defaultDispatcher from "../../../dispatcher/dispatcher";
+import { ViewRoomPayload } from "../../../dispatcher/payloads/ViewRoomPayload";
+import { Action } from "../../../dispatcher/actions";
 
 export default function RoomHeader({ room }: { room: Room }): JSX.Element {
     const roomName = useRoomName(room);
     const roomTopic = useTopic(room);
+
+    const { voiceCallDisabledReason, voiceCallType, videoCallDisabledReason, videoCallType } = useRoomCallStatus(room);
+
+    const placeCall = useCallback(
+        async (callType: CallType, platformCallType: typeof voiceCallType) => {
+            switch (platformCallType) {
+                case "legacy_or_jitsi":
+                    await LegacyCallHandler.instance.placeCall(room.roomId, callType);
+                    break;
+                // TODO: Remove the jitsi_or_element_call case and
+                // use the commented code below
+                case "element_call":
+                case "jitsi_or_element_call":
+                    defaultDispatcher.dispatch<ViewRoomPayload>({
+                        action: Action.ViewRoom,
+                        room_id: room.roomId,
+                        view_call: true,
+                        metricsTrigger: undefined,
+                    });
+                    break;
+
+                // case "jitsi_or_element_call":
+                // TODO: Open dropdown menu to choice between
+                // EC and Jitsi. Waiting on Compound's dropdown
+                // component
+                // break;
+            }
+        },
+        [room.roomId],
+    );
 
     return (
         <Flex
@@ -62,6 +101,28 @@ export default function RoomHeader({ room }: { room: Room }): JSX.Element {
                     </BodyText>
                 )}
             </Box>
+            <Flex as="nav" align="center" gap="var(--cpd-space-2x)">
+                <IconButton
+                    disabled={!!voiceCallDisabledReason}
+                    title={!voiceCallDisabledReason ? _t("Voice call") : voiceCallDisabledReason!}
+                    onClick={async (ev) => {
+                        ev.preventDefault();
+                        placeCall(CallType.Voice, voiceCallType);
+                    }}
+                >
+                    <VoiceCallIcon />
+                </IconButton>
+                <IconButton
+                    disabled={!!videoCallDisabledReason}
+                    title={!videoCallDisabledReason ? _t("Video call") : videoCallDisabledReason!}
+                    onClick={(ev) => {
+                        ev.preventDefault();
+                        placeCall(CallType.Video, videoCallType);
+                    }}
+                >
+                    <VideoCallIcon />
+                </IconButton>
+            </Flex>
         </Flex>
     );
 }
---
+++ src/hooks/room/useRoomCallStatus.ts
@@ -0,0 +1,142 @@
+/*
+Copyright 2023 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { Room, RoomStateEvent } from "matrix-js-sdk/src/matrix";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+import { useFeatureEnabled } from "../useSettings";
+import SdkConfig from "../../SdkConfig";
+import { useEventEmitterState, useTypedEventEmitterState } from "../useEventEmitter";
+import LegacyCallHandler, { LegacyCallHandlerEvent } from "../../LegacyCallHandler";
+import { useWidgets } from "../../components/views/right_panel/RoomSummaryCard";
+import { WidgetType } from "../../widgets/WidgetType";
+import { useCall } from "../useCall";
+import { _t } from "../../languageHandler";
+import { useRoomMembers } from "../useRoomMembers";
+import { ElementCall } from "../../models/Call";
+
+type CallType = "element_call" | "jitsi_or_element_call" | "legacy_or_jitsi";
+
+export const useRoomCallStatus = (room: Room): any => {
+    const [voiceCallDisabledReason, setVoiceCallDisabledReason] = useState<string | null>(null);
+    const [videoCallDisabledReason, setVideoCallDisabledReason] = useState<string | null>(null);
+    const [voiceCallType, setVoiceCallType] = useState<CallType>("jitsi_or_element_call");
+    const [videoCallType, setVideoCallType] = useState<CallType>("jitsi_or_element_call");
+
+    const groupCallsEnabled = useFeatureEnabled("feature_group_calls");
+    const useElementCallExclusively = useMemo(() => {
+        return SdkConfig.get("element_call").use_exclusively;
+    }, []);
+
+    const hasLegacyCall = useEventEmitterState(
+        LegacyCallHandler.instance,
+        LegacyCallHandlerEvent.CallsChanged,
+        useCallback(() => LegacyCallHandler.instance.getCallForRoom(room.roomId) !== null, [room]),
+    );
+
+    const widgets = useWidgets(room);
+    const hasJitsiWidget = useMemo(() => widgets.some((widget) => WidgetType.JITSI.matches(widget.type)), [widgets]);
+
+    const hasGroupCall = useCall(room.roomId) !== null;
+
+    const functionalMembers = useRoomMembers(room);
+
+    const [mayEditWidgets, mayCreateElementCalls] = useTypedEventEmitterState(
+        room,
+        RoomStateEvent.Update,
+        useCallback(
+            () => [
+                room.currentState.mayClientSendStateEvent("im.vector.modular.widgets", room.client),
+                room.currentState.mayClientSendStateEvent(ElementCall.CALL_EVENT_TYPE.name, room.client),
+            ],
+            [room],
+        ),
+    );
+
+    useEffect(() => {
+        if (groupCallsEnabled) {
+            if (useElementCallExclusively) {
+                if (hasGroupCall) {
+                    setVideoCallDisabledReason(_t("Ongoing call"));
+                } else if (mayCreateElementCalls) {
+                    setVideoCallType("element_call");
+                } else {
+                    setVoiceCallDisabledReason(_t("You do not have permission to start video calls"));
+                }
+            } else if (hasLegacyCall || hasJitsiWidget || hasGroupCall) {
+                setVoiceCallDisabledReason(_t("Ongoing call"));
+                setVideoCallDisabledReason(_t("Ongoing call"));
+            } else if (functionalMembers.length <= 1) {
+                setVoiceCallDisabledReason(_t("There's no one here to call"));
+                setVideoCallDisabledReason(_t("There's no one here to call"));
+            } else if (functionalMembers.length === 2) {
+                setVoiceCallType("legacy_or_jitsi");
+                setVideoCallType("legacy_or_jitsi");
+            } else if (mayEditWidgets) {
+                setVoiceCallType("legacy_or_jitsi");
+                setVideoCallType(mayCreateElementCalls ? "jitsi_or_element_call" : "legacy_or_jitsi");
+            } else {
+                setVoiceCallDisabledReason(_t("You do not have permission to start voice calls"));
+                if (mayCreateElementCalls) {
+                    setVideoCallType("element_call");
+                } else {
+                    setVideoCallDisabledReason(_t("You do not have permission to start video calls"));
+                }
+            }
+        } else if (hasLegacyCall || hasJitsiWidget) {
+            setVoiceCallDisabledReason(_t("Ongoing call"));
+            setVideoCallDisabledReason(_t("Ongoing call"));
+        } else if (functionalMembers.length <= 1) {
+            setVoiceCallDisabledReason(_t("There's no one here to call"));
+            setVideoCallDisabledReason(_t("There's no one here to call"));
+        } else if (functionalMembers.length === 2 || mayEditWidgets) {
+            setVoiceCallType("legacy_or_jitsi");
+            setVideoCallType("legacy_or_jitsi");
+        } else {
+            setVoiceCallDisabledReason(_t("You do not have permission to start voice calls"));
+            setVideoCallDisabledReason(_t("You do not have permission to start voice calls"));
+        }
+    }, [
+        functionalMembers.length,
+        groupCallsEnabled,
+        hasGroupCall,
+        hasJitsiWidget,
+        hasLegacyCall,
+        mayCreateElementCalls,
+        mayEditWidgets,
+        useElementCallExclusively,
+    ]);
+
+    /**
+     * We've gone through all the steps
+     */
+    return {
+        voiceCallDisabledReason,
+        voiceCallType,
+        videoCallDisabledReason,
+        videoCallType,
+        // voice: {
+        //     disabled: !!voiceCallDisabledReason,
+        //     reason: voiceCallDisabledReason,
+        //     type: voiceCallType,
+        // },
+        // video: {
+        //     disabled: !!videoCallDisabledReason,
+        //     reason: videoCallDisabledReason,
+        //     type: videoCallType,
+        // },
+    };
+};
