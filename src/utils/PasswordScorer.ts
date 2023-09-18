ac16a379a5846891a5cad16d174a4cd588178592
Michael Telatynski <7t3chguy@gmail.com>
2023-09-18 09:35:32 +0100

    Switch from `zxcvbn` to `@zxcvbn-ts`
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/utils/PasswordScorer.ts
+++ src/utils/PasswordScorer.ts
@@ -14,44 +14,62 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import zxcvbn, { ZXCVBNFeedbackWarning } from "zxcvbn";
+import { zxcvbn, zxcvbnOptions, ZxcvbnResult } from "@zxcvbn-ts/core";
+import * as zxcvbnCommonPackage from "@zxcvbn-ts/language-common";
+import * as zxcvbnEnPackage from "@zxcvbn-ts/language-en";
 import { MatrixClient } from "matrix-js-sdk/src/matrix";
 
-import { _t, _td, TranslationKey } from "../languageHandler";
+import { _t } from "../languageHandler";
 import { MatrixClientPeg } from "../MatrixClientPeg";
+import SdkConfig from "../SdkConfig";
 
-const ZXCVBN_USER_INPUTS = ["riot", "matrix"];
-
-// Translations for zxcvbn's suggestion strings
-_td("Use a few words, avoid common phrases");
-_td("No need for symbols, digits, or uppercase letters");
-_td("Use a longer keyboard pattern with more turns");
-_td("Avoid repeated words and characters");
-_td("Avoid sequences");
-_td("Avoid recent years");
-_td("Avoid years that are associated with you");
-_td("Avoid dates and years that are associated with you");
-_td("Capitalization doesn't help very much");
-_td("All-uppercase is almost as easy to guess as all-lowercase");
-_td("Reversed words aren't much harder to guess");
-_td("Predictable substitutions like '@' instead of 'a' don't help very much");
-_td("Add another word or two. Uncommon words are better.");
-
-// and warnings
-_td('Repeats like "aaa" are easy to guess');
-_td('Repeats like "abcabcabc" are only slightly harder to guess than "abc"');
-_td("Sequences like abc or 6543 are easy to guess");
-_td("Recent years are easy to guess");
-_td("Dates are often easy to guess");
-_td("This is a top-10 common password");
-_td("This is a top-100 common password");
-_td("This is a very common password");
-_td("This is similar to a commonly used password");
-_td("A word by itself is easy to guess");
-_td("Names and surnames by themselves are easy to guess");
-_td("Common names and surnames are easy to guess");
-_td("Straight rows of keys are easy to guess");
-_td("Short keyboard patterns are easy to guess");
+zxcvbnOptions.setOptions({
+    dictionary: {
+        ...zxcvbnCommonPackage.dictionary,
+        ...zxcvbnEnPackage.dictionary,
+        userInputs: ["riot", "matrix", "element", SdkConfig.get().brand],
+    },
+    graphs: zxcvbnCommonPackage.adjacencyGraphs,
+    useLevenshteinDistance: true,
+    translations: {
+        warnings: {
+            straightRow: _t("Straight rows of keys are easy to guess"),
+            keyPattern: _t("Short keyboard patterns are easy to guess"),
+            simpleRepeat: _t('Repeats like "aaa" are easy to guess'),
+            extendedRepeat: _t('Repeats like "abcabcabc" are only slightly harder to guess than "abc"'),
+            sequences: _t("Sequences like abc or 6543 are easy to guess"),
+            recentYears: _t("Recent years are easy to guess"),
+            dates: _t("Dates are often easy to guess"),
+            topTen: _t("This is a top-10 common password"),
+            topHundred: _t("This is a top-100 common password"),
+            common: _t("This is a very common password"),
+            similarToCommon: _t("This is similar to a commonly used password"),
+            wordByItself: _t("A word by itself is easy to guess"),
+            namesByThemselves: _t("Names and surnames by themselves are easy to guess"),
+            commonNames: _t("Common names and surnames are easy to guess"),
+            userInputs: _t("There should not be any personal or page related data."),
+            pwned: _t("Your password was exposed by a data breach on the Internet."),
+        },
+        suggestions: {
+            l33t: _t("Predictable substitutions like '@' instead of 'a' don't help very much"),
+            reverseWords: _t("Reversed words aren't much harder to guess"),
+            allUppercase: _t("All-uppercase is almost as easy to guess as all-lowercase"),
+            capitalization: _t("Capitalization doesn't help very much"),
+            dates: _t("Avoid dates and years that are associated with you"),
+            recentYears: _t("Avoid recent years"),
+            associatedYears: _t("Avoid years that are associated with you"),
+            sequences: _t("Avoid sequences"),
+            repeated: _t("Avoid repeated words and characters"),
+            longerKeyboardPattern: _t("Use a longer keyboard pattern with more turns"),
+            anotherWord: _t("Add another word or two. Uncommon words are better."),
+            useWords: _t("Use a few words, avoid common phrases"),
+            noNeed: _t("No need for symbols, digits, or uppercase letters"),
+            pwned: _t("If you use this password elsewhere, you should change it."),
+        },
+        // We don't utilise the time estimation at this time so just pass through the English translations here
+        timeEstimation: zxcvbnEnPackage.translations.timeEstimation,
+    },
+});
 
 /**
  * Wrapper around zxcvbn password strength estimation
@@ -67,36 +85,25 @@ export function scorePassword(
     matrixClient: MatrixClient | null,
     password: string,
     userInputs: string[] = [],
-): zxcvbn.ZXCVBNResult | null {
+): ZxcvbnResult | null {
     if (password.length === 0) return null;
 
-    const inputs = [...userInputs, ...ZXCVBN_USER_INPUTS];
     if (matrixClient) {
-        inputs.push(matrixClient.getUserIdLocalpart()!);
+        userInputs.push(matrixClient.getUserIdLocalpart()!);
     }
 
     try {
         const domain = MatrixClientPeg.getHomeserverName();
-        inputs.push(domain);
+        userInputs.push(domain);
     } catch {
         // This is fine
     }
 
-    let zxcvbnResult = zxcvbn(password, inputs);
+    let zxcvbnResult = zxcvbn(password, userInputs);
     // Work around https://github.com/dropbox/zxcvbn/issues/216
     if (password.includes(" ")) {
-        const resultNoSpaces = zxcvbn(password.replace(/ /g, ""), inputs);
+        const resultNoSpaces = zxcvbn(password.replace(/ /g, ""), userInputs);
         if (resultNoSpaces.score < zxcvbnResult.score) zxcvbnResult = resultNoSpaces;
-    }
-
-    for (let i = 0; i < zxcvbnResult.feedback.suggestions.length; ++i) {
-        // translate suggestions - we ensure we mark them as `_td` at the top of this file
-        // https://github.com/dropbox/zxcvbn/issues/284 will be a better approach when it lands
-        zxcvbnResult.feedback.suggestions[i] = _t(zxcvbnResult.feedback.suggestions[i] as TranslationKey);
-    }
-    // and warning, if any
-    if (zxcvbnResult.feedback.warning) {
-        zxcvbnResult.feedback.warning = _t(zxcvbnResult.feedback.warning) as ZXCVBNFeedbackWarning;
     }
 
     return zxcvbnResult;
