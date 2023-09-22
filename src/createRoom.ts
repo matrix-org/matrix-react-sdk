ec636a107fbca06bea4a7000eec35fa1057e69d4
Michael Telatynski <7t3chguy@gmail.com>
2023-09-22 09:44:24 +0100

    ``` replace "Unable to load! Check your network connectivity and try again." "failed_load_async_component" replace "The file '%(fileName)s' failed to upload." "upload_failed_generic" replace "The file '%(fileName)s' exceeds this homeserver's size limit for uploads" "upload_failed_size" replace "Upload Failed" "upload_failed_title"
    replace "Server may be unavailable, overloaded, or you hit a bug." "create_room|generic_error"
    replace "The server does not support the room version specified." "create_room|unsupported_version"
    replace "Failure to create room" "create_room|error_title"
    
    replace "User Busy" "voip|user_busy"
    replace "The user you called is busy." "voip|user_busy_description"
    replace "The call could not be established" "voip|call_failed_description"
    replace "Answered Elsewhere" "voip|answered_elsewhere"
    replace "The call was answered on another device." "voip|answered_elsewhere_description"
    replace "Call failed due to misconfigured server" "voip|misconfigured_server"
    replace "Please ask the administrator of your homeserver (<code>%(homeserverDomain)s</code>) to configure a TURN server in order for calls to work reliably." "voip|misconfigured_server_description"
    replace "Alternatively, you can try to use the public server at <server/>, but this will not be as reliable, and it will share your IP address with that server. You can also manage this in Settings." "voip|misconfigured_server_fallback"
    replace "Try using %(server)s" "voip|misconfigured_server_fallback_accept"
    replace "Connectivity to the server has been lost" "voip|connection_lost"
    replace "You cannot place calls without a connection to the server." "voip|connection_lost_description"
    replace "Too Many Calls" "voip|too_many_calls"
    replace "You've reached the maximum number of simultaneous calls." "voip|too_many_calls_description"
    replace "You cannot place a call with yourself." "voip|cannot_call_yourself_description"
    ```
    
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/createRoom.ts
+++ src/createRoom.ts
@@ -390,15 +390,15 @@ export default async function createRoom(client: MatrixClient, opts: IOpts): Pro
                     roomId,
                 });
                 logger.error("Failed to create room " + roomId + " " + err);
-                let description = _t("Server may be unavailable, overloaded, or you hit a bug.");
+                let description = _t("create_room|generic_error");
                 if (err.errcode === "M_UNSUPPORTED_ROOM_VERSION") {
                     // Technically not possible with the UI as of April 2019 because there's no
                     // options for the user to change this. However, it's not a bad thing to report
                     // the error to the user for if/when the UI is available.
-                    description = _t("The server does not support the room version specified.");
+                    description = _t("create_room|unsupported_version");
                 }
                 Modal.createDialog(ErrorDialog, {
-                    title: _t("Failure to create room"),
+                    title: _t("create_room|error_title"),
                     description,
                 });
                 return null;
