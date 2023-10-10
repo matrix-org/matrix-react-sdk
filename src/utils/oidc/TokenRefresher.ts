8a47e6e1069d7e09cadbbdf4695f767687b10a1d
Kerry Archibald <kerrya@element.io>
2023-10-10 15:08:17 +1300

    test token refresher

--- src/utils/oidc/TokenRefresher.ts
+++ src/utils/oidc/TokenRefresher.ts
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import { IDelegatedAuthConfig, OidcTokenRefresher } from "matrix-js-sdk/src/matrix";
+import { IDelegatedAuthConfig, OidcTokenRefresher, AccessTokens } from "matrix-js-sdk/src/matrix";
 import { IdTokenClaims } from "oidc-client-ts";
 
 import PlatformPeg from "../../PlatformPeg";
@@ -39,13 +39,7 @@ export class TokenRefresher extends OidcTokenRefresher {
         this.deviceId = deviceId;
     }
 
-    public async persistTokens({
-        accessToken,
-        refreshToken,
-    }: {
-        accessToken: string;
-        refreshToken?: string | undefined;
-    }): Promise<void> {
+    public async persistTokens({ accessToken, refreshToken }: AccessTokens): Promise<void> {
         const pickleKey = (await PlatformPeg.get()?.getPickleKey(this.userId, this.deviceId)) ?? undefined;
         await persistAccessTokenInStorage(accessToken, pickleKey);
         await persistRefreshTokenInStorage(refreshToken, pickleKey);
