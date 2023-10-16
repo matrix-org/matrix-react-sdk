46efae48fec0a3590aec99051db01101e411e319
Valere <bill.carson@valrsoft.com>
2023-10-16 17:42:03 +0200

    put stacktrace last

--- src/components/views/rooms/EventTile.tsx
+++ src/components/views/rooms/EventTile.tsx
@@ -592,7 +592,7 @@ export class UnwrappedEventTile extends React.Component<EventTileProps, IState> 
     private verifyEvent(): void {
         this.doVerifyEvent().catch((e) => {
             const event = this.props.mxEvent;
-            logger.error("Error getting encryption info on event", e, event.getRoomId(), event.getId());
+            logger.error("Error getting encryption info on event", event.getRoomId(), event.getId(), e);
         });
     }
 
