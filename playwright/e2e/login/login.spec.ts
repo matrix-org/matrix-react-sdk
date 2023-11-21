2d472e00886f08c01b6f1b792228686b9aa4e67b
Michael Telatynski <7t3chguy@gmail.com>
2023-11-21 14:03:48 +0000

    Add login.spec.ts
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

---
+++ playwright/e2e/login/login.spec.ts
@@ -0,0 +1,88 @@
+/*
+Copyright 2023 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { test, expect } from "@playwright/test";
+import { checkA11y, injectAxe } from "axe-playwright";
+
+import { HomeserverInstance } from "../../plugins/utils/homeserver";
+import { startHomeserver, stopHomeserver } from "../../plugins/homeserver";
+import { ElementAppPage } from "../../pages/ElementAppPage";
+
+test.describe("Consent", () => {
+    let homeserver: HomeserverInstance;
+
+    test.afterEach(() => {
+        stopHomeserver(homeserver);
+    });
+
+    test.describe("m.login.password", () => {
+        const username = "user1234";
+        const password = "p4s5W0rD";
+
+        test.beforeEach(async ({ page, request }) => {
+            homeserver = await startHomeserver("consent");
+
+            await new ElementAppPage(page, request).registerUser(homeserver, username, password);
+            await page.goto("/#/login");
+        });
+
+        test("logs in with an existing account and lands on the home screen", async ({ page }) => {
+            await injectAxe(page);
+
+            // first pick the homeserver, as otherwise the user picker won't be visible
+            await page.getByRole("button", { name: "Edit" }).click();
+            await page.getByRole("textbox", { name: "Other homeserver" }).fill(homeserver.baseUrl);
+            await page.getByRole("button", { name: "Continue", exact: true }).click();
+            // wait for the dialog to go away
+            await expect(page.locator(".mx_ServerPickerDialog")).toHaveCount(0);
+
+            await expect(page.locator(".mx_Spinner")).toHaveCount(0);
+            await expect(page.locator(".mx_ServerPicker_server")).toHaveText(homeserver.baseUrl);
+
+            await page.getByRole("button", { name: "Edit" }).click();
+
+            // select the default server again
+            await page.locator(".mx_StyledRadioButton").first().click();
+            await page.getByRole("button", { name: "Continue", exact: true }).click();
+            await expect(page.locator(".mx_ServerPickerDialog")).toHaveCount(0);
+            await expect(page.locator(".mx_Spinner")).toHaveCount(0);
+            // name of default server
+            // TODO don't use matrix.org config.json
+            // await expect(page.locator(".mx_ServerPicker_server")).toHaveText("server.invalid");
+
+            // switch back to the custom homeserver
+            await page.getByRole("button", { name: "Edit" }).click();
+            await page.getByRole("textbox", { name: "Other homeserver" }).fill(homeserver.baseUrl);
+            await page.getByRole("button", { name: "Continue", exact: true }).click();
+            // wait for the dialog to go away
+            await expect(page.locator(".mx_ServerPickerDialog")).toHaveCount(0);
+
+            await expect(page.locator(".mx_Spinner")).toHaveCount(0);
+            await expect(page.locator(".mx_ServerPicker_server")).toHaveText(homeserver.baseUrl);
+
+            await expect(page.getByRole("textbox", { name: "Username" })).toBeVisible();
+            // Disabled because flaky - see https://github.com/vector-im/element-web/issues/24688
+            // cy.percySnapshot("Login");
+            await checkA11y(page);
+
+            await page.getByRole("textbox", { name: "Username" }).fill(username);
+            await page.getByPlaceholder("Password").fill(password);
+            await page.getByRole("button", { name: "Sign in" }).click();
+
+            await expect(page).toHaveURL(/\/#\/home$/);
+        });
+    });
+});
