150478d6be4d5a5b870ce252481a92de31bf60db
Michael Telatynski <7t3chguy@gmail.com>
2023-11-22 10:33:32 +0000

    Add remaining login.spec.ts tests using oauth_server
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- playwright/element-web-test.ts
+++ playwright/element-web-test.ts
@@ -15,11 +15,12 @@ limitations under the License.
 */
 
 import { test as base } from "@playwright/test";
+import _ from "lodash";
 
-import { HomeserverInstance, StartHomeserverOpts } from "./plugins/utils/homeserver";
+import { Credentials, HomeserverInstance, StartHomeserverOpts } from "./plugins/utils/homeserver";
 import { Synapse } from "./plugins/synapse";
 
-const CONFIG_JSON = {
+const CONFIG_JSON: Record<string, any> = {
     // This is deliberately quite a minimal config.json, so that we can test that the default settings
     // actually work.
     //
@@ -36,14 +37,18 @@ const CONFIG_JSON = {
 };
 
 export const test = base.extend<{
+    config: typeof CONFIG_JSON;
     startHomeserverOpts: StartHomeserverOpts | string;
     homeserver: HomeserverInstance;
+    user: Credentials & {
+        displayName: string;
+    };
 }>({
-    page: async ({ context, page }, use) => {
+    config: CONFIG_JSON,
+    page: async ({ context, page, config }, use) => {
         await context.route(`http://localhost:8080/config.json*`, async (route) => {
-            await route.fulfill({ json: CONFIG_JSON });
+            await route.fulfill({ json: config });
         });
-
         await use(page);
     },
 
@@ -56,6 +61,46 @@ export const test = base.extend<{
         const server = new Synapse(request);
         await use(await server.start(opts));
         await server.stop();
+    },
+
+    user: async ({ page, homeserver }, use) => {
+        const names = ["Alice", "Bob", "Charlie", "Daniel", "Eve", "Frank", "Grace", "Hannah", "Isaac", "Judy"];
+        const username = _.uniqueId("user_");
+        const password = _.uniqueId("password_");
+        const displayName = _.sample(names)!;
+
+        const credentials = await homeserver.registerUser(username, password, displayName);
+        console.log(`Registered test user ${username} with displayname ${displayName}`);
+
+        await page.goto("/");
+        await page.evaluate(
+            ({ baseUrl, credentials }) => {
+                // Seed the localStorage with the required credentials
+                window.localStorage.setItem("mx_hs_url", baseUrl);
+                window.localStorage.setItem("mx_user_id", credentials.userId);
+                window.localStorage.setItem("mx_access_token", credentials.accessToken);
+                window.localStorage.setItem("mx_device_id", credentials.deviceId);
+                window.localStorage.setItem("mx_is_guest", "false");
+                window.localStorage.setItem("mx_has_pickle_key", "false");
+                window.localStorage.setItem("mx_has_access_token", "true");
+
+                // Ensure the language is set to a consistent value
+                window.localStorage.setItem("mx_local_settings", '{"language":"en"}');
+            },
+            { baseUrl: homeserver.config.baseUrl, credentials },
+        );
+        await page.addInitScript(() => {
+            // reset notification permissions, so we have predictable behaviour of notifications toast
+            Object.assign(window.Notification, { permission: "default" });
+        });
+        await page.reload(); // We have to reload to have the app read the credentials we injected
+
+        await page.waitForSelector(".mx_MatrixChat", { timeout: 30000 });
+
+        await use({
+            ...credentials,
+            displayName,
+        });
     },
 });
 
