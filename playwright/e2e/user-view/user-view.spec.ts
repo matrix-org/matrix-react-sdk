e5393d92588dfbf07d41de30d87eeb76f0ea61d1
Michael Telatynski <7t3chguy@gmail.com>
2023-11-27 12:37:49 +0000

    Migrate user-view.spec.ts from Cypress to Playwright
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

---
+++ playwright/e2e/user-view/user-view.spec.ts
@@ -0,0 +1,34 @@
+/*
+Copyright 2022 The Matrix.org Foundation C.I.C.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import { test, expect } from "../../element-web-test";
+
+test.describe("UserView", () => {
+    test.use({
+        displayName: "Violet",
+    });
+
+    test("should render the user view as expected", async ({ page, homeserver, user }) => {
+        const bot = await homeserver.registerUser("usman", "password", "Usman");
+        await page.goto(`/#/user/${bot.userId}`);
+
+        const rightPanel = page.getByRole("complementary");
+        await expect(rightPanel.getByRole("heading", { name: "Usman", exact: true })).toBeVisible();
+        // TODO this does not yet work, we can wire it back up after we get the `bot` fixture working
+        // await expect(rightPanel).toHaveText("1 session");
+        await expect(rightPanel).toHaveScreenshot();
+    });
+});
