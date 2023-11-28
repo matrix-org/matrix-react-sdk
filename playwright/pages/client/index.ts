44e501ec9e8af0d938363f807ea4e16923c6aa20
R Midhun Suresh <hi@midhun.dev>
2023-11-28 22:22:48 +0530

    Fix improper evaluates in client

--- playwright/pages/client/index.ts
+++ playwright/pages/client/index.ts
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import { JSHandle } from "@playwright/test";
+
 import type {
     IContent,
     ICreateRoomOpts,
@@ -39,13 +41,20 @@ interface UserCredentials {
 }
 
 export class ElementClientPage {
+    private clientHandle: JSHandle<MatrixClient>;
+
     constructor(private page: Page) {}
 
     /**
      * Returns the MatrixClient from the MatrixClientPeg
      */
-    async getClient(): Promise<MatrixClient> {
-        return await this.page.evaluate(() => (window as any).mxMatrixClientPeg.matrixClient);
+    private async getClientHandle(): Promise<JSHandle<MatrixClient>> {
+        if (!this.clientHandle) {
+            this.clientHandle = (await this.page.evaluateHandle(
+                () => (window as any).mxMatrixClientPeg.matrixClient,
+            )) as JSHandle<MatrixClient>;
+        }
+        return this.clientHandle;
     }
 
     /**
@@ -54,9 +63,11 @@ export class ElementClientPage {
      * @return the list of DMs with that user
      */
     async getDmRooms(userId: string): Promise<string[]> {
-        const client = await this.getClient();
-        const dmRoomMap = client.getAccountData("m.direct")?.getContent<Record<string, string[]>>();
-        return dmRoomMap[userId] ?? [];
+        const handle = await this.getClientHandle();
+        return await handle.evaluate(async (client, userId) => {
+            const dmRoomMap = client.getAccountData("m.direct")?.getContent<Record<string, string[]>>();
+            return dmRoomMap[userId] ?? [];
+        }, userId);
     }
 
     /**
@@ -65,7 +76,7 @@ export class ElementClientPage {
      * @return the ID of the newly created room
      */
     async createRoom(options: ICreateRoomOpts): Promise<string> {
-        return await this.page.evaluate(async () => {
+        return await this.page.evaluate(async (options) => {
             const cli = (window as any).mxMatrixClientPeg.matrixClient;
             const resp = await cli.createRoom(options);
             const roomId = resp.room_id;
@@ -81,7 +92,7 @@ export class ElementClientPage {
                 });
             }
             return roomId;
-        });
+        }, options);
     }
 
     /**
@@ -104,8 +115,10 @@ export class ElementClientPage {
      * @param userId the id of the user to invite
      */
     async inviteUser(roomId: string, userId: string): Promise<{}> {
-        const client = await this.getClient();
-        const res = await client.invite(roomId, userId);
+        const handle = await this.getClientHandle();
+        const res = await handle.evaluate(async (client, userId) => {
+            return await client.invite(roomId, userId);
+        }, userId);
         console.log(`sent invite in ${roomId} for ${userId}`);
         return res;
     }
@@ -116,8 +129,10 @@ export class ElementClientPage {
      * @param data The data to store.
      */
     async setAccountData(type: string, data: IContent): Promise<{}> {
-        const client = await this.getClient();
-        return await client.setAccountData(type, data);
+        const handle = await this.getClientHandle();
+        return await handle.evaluate(async (client, data) => {
+            return client.setAccountData(type, data);
+        }, data);
     }
 
     /**
@@ -133,8 +148,13 @@ export class ElementClientPage {
         eventType: string,
         content: IContent,
     ): Promise<ISendEventResponse> {
-        const client = await this.getClient();
-        return await client.sendEvent(roomId, threadId, eventType, content);
+        const handle = await this.getClientHandle();
+        return await handle.evaluate(
+            async (client, { roomId, threadId, eventType, content }) => {
+                return await client.sendEvent(roomId, threadId, eventType, content);
+            },
+            { roomId, threadId, eventType, content },
+        );
     }
 
     /**
@@ -144,8 +164,17 @@ export class ElementClientPage {
      * @return {module:http-api.MatrixError} Rejects: with an error response.
      */
     async sendReadReceipt(event: MatrixEvent, receiptType?: ReceiptType, unthreaded?: boolean): Promise<{}> {
-        const client = await this.getClient();
-        return await client.sendReadReceipt(event, receiptType, unthreaded);
+        const handle = await this.getClientHandle();
+        return await handle.evaluate(
+            async (client, { event, receiptType, unthreaded }) => {
+                return await client.sendReadReceipt(event, receiptType, unthreaded);
+            },
+            {
+                event,
+                receiptType,
+                unthreaded,
+            },
+        );
     }
 
     /**
@@ -155,8 +184,10 @@ export class ElementClientPage {
      * @return {module:http-api.MatrixError} Rejects: with an error response.
      */
     async setDisplayName(name: string): Promise<{}> {
-        const client = await this.getClient();
-        return await client.setDisplayName(name);
+        const handle = await this.getClientHandle();
+        return await handle.evaluate(async (client, name) => {
+            return await client.setDisplayName(name);
+        }, name);
     }
 
     /**
@@ -167,8 +198,13 @@ export class ElementClientPage {
      *   a a Buffer, String or ReadStream.
      */
     async uploadContent(file: FileType, opts?: UploadOpts): Promise<Awaited<Upload["promise"]>> {
-        const client = await this.getClient();
-        return await client.uploadContent(file, opts);
+        const handle = await this.getClientHandle();
+        return await handle.evaluate(
+            async (client, { file, opts }) => {
+                return await client.uploadContent(file, opts);
+            },
+            { file, opts },
+        );
     }
 
     /**
@@ -178,15 +214,17 @@ export class ElementClientPage {
      * @return {module:http-api.MatrixError} Rejects: with an error response.
      */
     async setAvatarUrl(url: string): Promise<{}> {
-        const client = await this.getClient();
-        return await client.setAvatarUrl(url);
+        const handle = await this.getClientHandle();
+        return await handle.evaluate(async (client, url) => {
+            return await client.setAvatarUrl(url);
+        }, url);
     }
 
     /**
      * Boostraps cross-signing.
      */
     async bootstrapCrossSigning(credentials: UserCredentials): Promise<void> {
-        await this.page.evaluate(async () => {
+        await this.page.evaluate(async (credentials) => {
             (window as any).mxMatrixClientPeg.matrixClient.bootstrapCrossSigning({
                 authUploadDeviceSigningKeys: async (func) => {
                     await func({
@@ -199,7 +237,7 @@ export class ElementClientPage {
                     });
                 },
             });
-        });
+        }, credentials);
     }
 
     /**
@@ -207,7 +245,9 @@ export class ElementClientPage {
      * @param roomIdOrAlias the id or alias of the room to join
      */
     async joinRoom(roomIdOrAlias: string): Promise<Room> {
-        const client = await this.getClient();
-        return await client.joinRoom(roomIdOrAlias);
+        const client = await this.getClientHandle();
+        return await client.evaluate(async (client, roomIdOrAlias) => {
+            return await client.joinRoom(roomIdOrAlias);
+        }, roomIdOrAlias);
     }
 }
