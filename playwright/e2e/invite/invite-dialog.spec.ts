d0612bb0b3c6dc431ab11d94f614ff7e3d3a98c6
Michael Telatynski <7t3chguy@gmail.com>
2023-12-06 15:34:22 +0000

    Iterate
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- playwright/e2e/invite/invite-dialog.spec.ts
+++ playwright/e2e/invite/invite-dialog.spec.ts
@@ -1,5 +1,6 @@
 /*
 Copyright 2023 Suguru Hirahara
+Copyright 2023 The Matrix.org Foundation C.I.C.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
