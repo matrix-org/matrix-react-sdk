1b09a8c9eab918ff8c0deb681d8e344901353670
Michael Telatynski <7t3chguy@gmail.com>
2023-12-14 12:33:49 +0000

    Update global.d.ts

--- cypress/global.d.ts
+++ cypress/global.d.ts
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import "../src/@types/react";
 // eslint-disable-next-line no-restricted-imports
 import "matrix-js-sdk/src/@types/global";
 import type {
