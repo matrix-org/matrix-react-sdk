6084ecf131cd46fffd7f908fb8c7dd920709fedf
R Midhun Suresh <hi@midhun.dev>
2023-12-17 22:16:36 +0530

    Remove reference to cypress types

--- playwright/e2e/polls/pollHistory.spec.ts
+++ playwright/e2e/polls/pollHistory.spec.ts
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 - 2023 The Matrix.org Foundation C.I.C.
+Copyright 2023 The Matrix.org Foundation C.I.C.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,9 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-/// <reference types="cypress" />
-
 import { test, expect } from "../../element-web-test";
 import type { Page } from "@playwright/test";
 import type { Bot } from "../../pages/bot";
