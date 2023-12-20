5f5e11a72d14b650a71a613cbd5fa67017e23c89
Michael Telatynski <7t3chguy@gmail.com>
2023-12-20 14:18:22 +0000

    Remove useless comment
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/components/views/audio_messages/PlayPauseButton.tsx
+++ src/components/views/audio_messages/PlayPauseButton.tsx
@@ -21,7 +21,6 @@ import AccessibleTooltipButton from "../elements/AccessibleTooltipButton";
 import { _t } from "../../../languageHandler";
 import { Playback, PlaybackState } from "../../../audio/Playback";
 
-// omitted props are handled by render function
 type Props = Omit<ComponentProps<typeof AccessibleTooltipButton>, "title" | "onClick" | "disabled" | "element"> & {
     // Playback instance to manipulate. Cannot change during the component lifecycle.
     playback: Playback;
