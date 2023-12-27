f01b155fac2d83c3f844e616edefb3583d7c3ca2
Timo K <toger5@hotmail.de>
2023-12-27 17:45:35 +0100

    fix rounded corners in pip
    Signed-off-by: Timo K <toger5@hotmail.de>

--- src/components/views/elements/AppTile.tsx
+++ src/components/views/elements/AppTile.tsx
@@ -608,11 +608,11 @@ export default class AppTile extends React.Component<IProps, IState> {
             "microphone; camera; encrypted-media; autoplay; display-capture; clipboard-write; " + "clipboard-read;";
 
         const appTileBodyClass = classNames({
-            // We don't want mx_AppTileBody (rounded corners) for call widgets
             "mx_AppTileBody": true,
             "mx_AppTileBody--large": !this.props.miniMode,
             "mx_AppTileBody--mini": this.props.miniMode,
             "mx_AppTileBody--loading": this.state.loading,
+            // We don't want mx_AppTileBody (rounded corners) for call widgets
             "mx_AppTileBody--call": this.props.app.type === WidgetType.CALL.preferred,
         });
         const appTileBodyStyles: CSSProperties = {};
