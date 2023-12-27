628ea015bd91d17c61c547d95769372790be0366
Timo K <toger5@hotmail.de>
2023-12-27 20:44:56 +0100

    add correct LiveContentSummary labels
    Signed-off-by: Timo K <toger5@hotmail.de>

--- src/components/views/rooms/RoomTileCallSummary.tsx
+++ src/components/views/rooms/RoomTileCallSummary.tsx
@@ -35,6 +35,10 @@ export const RoomTileCallSummary: FC<Props> = ({ call }) => {
             text = _t("common|video");
             active = false;
             break;
+        case ConnectionState.WidgetLoading:
+            text = _t("common|loading");
+            active = false;
+            break;
         case ConnectionState.Lobby:
             text = _t("common|lobby");
             active = false;
