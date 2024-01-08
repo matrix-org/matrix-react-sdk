bae92891f5ca71d296ff8e096523137a6d37d135
Michael Telatynski <7t3chguy@gmail.com>
2024-01-08 08:41:38 +0000

    Revert unrelated change
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- test/setupTests.ts
+++ test/setupTests.ts
@@ -38,11 +38,9 @@ beforeEach(() => {
 });
 
 // Retry to work around our flaky app & tests
-if (process.env.CI) {
-    jest.retryTimes(2, {
-        logErrorsBeforeRetry: true,
-    });
-}
+jest.retryTimes(2, {
+    logErrorsBeforeRetry: true,
+});
 
 // Very carefully enable the mocks for everything else in
 // a specific order. We use this order to ensure we properly
