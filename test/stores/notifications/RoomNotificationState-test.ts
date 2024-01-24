c9eccdcd1684056cd8ae075c5062f4ddbe97a6e1
David Baker <dbkr@users.noreply.github.com>
2024-01-24 18:33:52 +0000

    Fix tests

--- test/stores/notifications/RoomNotificationState-test.ts
+++ test/stores/notifications/RoomNotificationState-test.ts
@@ -81,7 +81,7 @@ describe("RoomNotificationState", () => {
     }
 
     it("Updates on event decryption", () => {
-        const roomNotifState = new RoomNotificationState(room, false);
+        const roomNotifState = new RoomNotificationState(room, true);
         const listener = jest.fn();
         roomNotifState.addListener(NotificationStateEvents.Update, listener);
         const testEvent = {
@@ -159,7 +159,7 @@ describe("RoomNotificationState", () => {
     });
 
     it("includes threads", async () => {
-        const roomNotifState = new RoomNotificationState(room, false);
+        const roomNotifState = new RoomNotificationState(room, true);
 
         room.timeline.push(
             new MatrixEvent({
