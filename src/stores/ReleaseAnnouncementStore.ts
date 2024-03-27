17b5ebe1e0a6e4869f60329c142c4df26c547a1a
Florian Duros <florian.duros@ormaz.fr>
2024-03-27 14:54:38 +0100

    WIP

---
+++ src/stores/ReleaseAnnouncementStore.ts
@@ -0,0 +1,57 @@
+/*
+ *
+ * Copyright 2024 The Matrix.org Foundation C.I.C.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * /
+ */
+
+import { TypedEventEmitter } from "matrix-js-sdk/src/matrix";
+
+const FEATURES = ["threadsActivityCentre"] as const;
+export type Features = (typeof FEATURES)[number];
+
+type ReleaseAnnouncementStoreEvents = "releaseAnnouncementChanged";
+
+type HandlerMap = {
+    releaseAnnouncementChanged: (newName: Features) => void;
+};
+
+export class ReleaseAnnouncementStore extends TypedEventEmitter<ReleaseAnnouncementStoreEvents, HandlerMap> {
+    private static internalInstance: ReleaseAnnouncementStore;
+    private currentReleaseAnnouncement = 0;
+
+    public static get instance(): ReleaseAnnouncementStore {
+        if (!ReleaseAnnouncementStore.internalInstance) {
+            ReleaseAnnouncementStore.internalInstance = new ReleaseAnnouncementStore();
+        }
+        return ReleaseAnnouncementStore.internalInstance;
+    }
+
+    private constructor() {
+        super();
+    }
+
+    public getReleaseAnnouncement(): Features {
+        if (this.currentReleaseAnnouncement >= FEATURES.length) {
+            this.currentReleaseAnnouncement = 0;
+        }
+
+        return FEATURES[this.currentReleaseAnnouncement];
+    }
+
+    public nextReleaseAnnouncement(): void {
+        this.currentReleaseAnnouncement++;
+        this.emit("releaseAnnouncementChanged", this.getReleaseAnnouncement());
+    }
+}
