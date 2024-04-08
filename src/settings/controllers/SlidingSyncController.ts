16522ae745bfad260d9b4b06eed0abdbc3e6016b
Ed Geraghty <ed@geraghty.family>
2024-04-08 18:06:33 +0100

    Add myself to the copyright assignation
    I wrote the majority of this file...
    
    Signed-off-by: Ed Geraghty <ed@geraghty.family>

--- src/settings/controllers/SlidingSyncController.ts
+++ src/settings/controllers/SlidingSyncController.ts
@@ -1,5 +1,6 @@
 /*
 Copyright 2022 The Matrix.org Foundation C.I.C.
+Copyright 2024 Ed Geraghty <ed@geraghty.family>
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
