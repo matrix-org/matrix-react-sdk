7baa85a047875feaa380f537e27a4a367445e439
Timo <toger5@hotmail.de>
2024-04-11 18:12:10 +0200

    Fix link modal not shown after access upgrade
    We dont show the modal since there was a mistake in the isRoomJoinable function.
    It used a state variable instead of reacomputing the join rule with room.getJoinRule()
    The state is a captured variable that from before the link share click -> does not update at that time.

--- src/hooks/room/useGuestAccessInformation.ts
+++ src/hooks/room/useGuestAccessInformation.ts
@@ -52,7 +52,10 @@ export const useGuestAccessInformation = (room: Room): GuestAccessInformation =>
         [canChangeJoinRule, isRoomJoinable, guestSpaUrl],
     );
 
-    const isRoomJoinableFunction = (): boolean =>
-        room.getJoinRule() === JoinRule.Public || (joinRule === JoinRule.Knock && room.canInvite(room.myUserId));
+    const isRoomJoinableFunction = (): boolean => {
+        const joinRule = room.getJoinRule();
+        return joinRule === JoinRule.Public || (joinRule === JoinRule.Knock && room.canInvite(room.myUserId));
+    };
+
     return { canInviteGuests, guestSpaUrl, isRoomJoinable: isRoomJoinableFunction, canInvite };
 };
