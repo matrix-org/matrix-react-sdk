d36f850dec0f5268ea73e5f287d9fb217920102b
David Baker <dbkr@users.noreply.github.com>
2024-07-05 09:35:35 +0100

    Add test that actually opens the dialog in question

--- playwright/e2e/forgot-password/forgot-password.spec.ts
+++ playwright/e2e/forgot-password/forgot-password.spec.ts
@@ -17,7 +17,22 @@ limitations under the License.
 import { expect, test } from "../../element-web-test";
 import { selectHomeserver } from "../utils";
 
+const username = "user1234";
+const password = "p4s5W0rD";
+const email = "user@nowhere.dummy";
+
 test.describe("Forgot Password", () => {
+    test.use({
+        startHomeserverOpts: ({ mailhog }, use) =>
+            use({
+                template: "email",
+                variables: {
+                    SMTP_HOST: "host.containers.internal",
+                    SMTP_PORT: mailhog.instance.smtpPort,
+                },
+            }),
+    });
+
     test("renders properly", async ({ page, homeserver }) => {
         await page.goto("/");
 
@@ -28,6 +43,32 @@ test.describe("Forgot Password", () => {
 
         await page.getByRole("button", { name: "Forgot password?" }).click();
 
-        await expect(page.getByRole("main")).toMatchScreenshot("server-picker.png");
+        await expect(page.getByRole("main")).toMatchScreenshot("forgot-password.png");
+    });
+
+    test("renders email verification dialog properly", async ({ page, homeserver }) => {
+        const user = await homeserver.registerUser(username, password);
+
+        await homeserver.setThreepid(user.userId, "email", email);
+
+        await page.goto("/");
+
+        await page.getByRole("link", { name: "Sign in" }).click();
+        await selectHomeserver(page, homeserver.config.baseUrl);
+
+        await page.getByRole("button", { name: "Forgot password?" }).click();
+
+        await page.getByRole("textbox", { name: "Email address" }).fill(email);
+
+        await page.getByRole("button", { name: "Send email" }).click();
+
+        await page.getByRole("button", { name: "Next" }).click();
+
+        await page.getByRole("textbox", { name: "New Password", exact: true }).fill(password);
+        await page.getByRole("textbox", { name: "Confirm new password", exact: true }).fill(password);
+
+        await page.getByRole("button", { name: "Reset password" }).click();
+
+        await expect(page.getByRole("main")).toMatchScreenshot("forgot-password-verify-email.png");
     });
 });
