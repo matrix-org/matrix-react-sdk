8e4b24bd2279f1222216f52d908ff83c2ef54cc8
Michael Telatynski <7t3chguy@gmail.com>
2024-07-13 10:51:26 +0100

    Cull the css again using css-tree
    Signed-off-by: Michael Telatynski <7t3chguy@gmail.com>

--- src/utils/exportUtils/exportCSS.ts
+++ src/utils/exportUtils/exportCSS.ts
@@ -14,7 +14,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import type { Rule, StyleSheet } from "css-tree";
+
 import customCSS from "!!raw-loader!./exportCustomCSS.css";
+
+const cssSelectorTextClassesRegex = /\.[\w-]+/g;
 
 function mutateCssText(css: string): string {
     // replace used fonts so that we don't have to bundle Inter & Inconsalata
@@ -29,11 +33,29 @@ function mutateCssText(css: string): string {
         );
 }
 
+function includeRule(rule: Rule, usedClasses: Set<string>): boolean {
+    if (rule.prelude.type === "Raw") {
+        // cull empty rules
+        if (rule.block.children.isEmpty) return false;
+
+        return rule.prelude.value.split(",").some((subselector) => {
+            const classes = subselector.trim().match(cssSelectorTextClassesRegex);
+            if (classes && !classes.every((c) => usedClasses.has(c.substring(1)))) {
+                return false;
+            }
+            return true;
+        });
+    }
+    return true;
+}
+
 // naively culls unused css rules based on which classes are present in the html,
 // doesn't cull rules which won't apply due to the full selector not matching but gets rid of a LOT of cruft anyway.
 // We cannot use document.styleSheets as it does not handle variables in shorthand properties sanely,
 // see https://github.com/element-hq/element-web/issues/26761
 const getExportCSS = async (usedClasses: Set<string>): Promise<string> => {
+    const csstree = await import("css-tree");
+
     // only include bundle.css and light theme styling
     const hrefs = ["bundle.css", "theme-light.css"].map((name) => {
         return document.querySelector<HTMLLinkElement>(`link[rel="stylesheet"][href$="${name}"]`)?.href;
@@ -45,7 +67,28 @@ const getExportCSS = async (usedClasses: Set<string>): Promise<string> => {
         if (!href) continue;
         const res = await fetch(href);
         const text = await res.text();
-        css += mutateCssText(text);
+
+        const ast = csstree.parse(text, {
+            context: "stylesheet",
+            parseAtrulePrelude: false,
+            parseRulePrelude: false,
+            parseValue: false,
+            parseCustomProperty: false,
+        }) as StyleSheet;
+
+        for (const rule of ast.children) {
+            if (rule.type === "Atrule") {
+                if (rule.name === "font-face") {
+                    continue;
+                }
+            }
+
+            if (rule.type === "Rule" && !includeRule(rule, usedClasses)) {
+                continue;
+            }
+
+            css += mutateCssText(csstree.generate(rule));
+        }
     }
 
     return css + customCSS;
