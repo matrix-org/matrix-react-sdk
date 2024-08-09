fc97936c1085e2c56cb846b77b14004b022911a1
Srinjoy Sen Chowdhury <116475469+AllMightLegend@users.noreply.github.com>
2024-08-10 05:00:04 +0530

    #23 issue : Updated .ts and .tsx files
    copyright-header is inserted as first comment

--- .history/playwright/e2e/audio-player/audio-player.spec_20240810045223.ts
+++ .history/playwright/e2e/audio-player/audio-player.spec_20240810045223.ts
@@ -1,6 +1,6 @@
 /*
-Copyright 2024 Suguru Hirahara
-Copyright 2024 The Matrix.org Foundation C.I.C.
+Copyright 2023 Suguru Hirahara
+Copyright 2023 The Matrix.org Foundation C.I.C.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
